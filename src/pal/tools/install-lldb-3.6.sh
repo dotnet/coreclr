9f799b66344d1f4e82eb0d1da12d47eff53501ea
Mike McLaughlin <mikem@microsoft.com>
2015-03-09 17:59:35 -0700

    Don't append to llvm.list.

--- src/pal/tools/install-lldb-3.6.sh
+++ src/pal/tools/install-lldb-3.6.sh
@@ -1,4 +1,4 @@
-echo "deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty-3.6 main" >> /etc/apt/sources.list.d/llvm.list
+echo "deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty-3.6 main" > /etc/apt/sources.list.d/llvm.list
 wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key|sudo apt-key add -
 apt-get update
 apt-get install lldb-3.6 lldb-3.6-dev
