ab0f84aab89f72b1d936ce604120506ab06b9633
Roman Shchekin <mrqtros@gmail.com>
2015-11-29 16:54:01 +0300

    Correct way of calculating 1.5 (without floating point calculation, thanks mikedn)

--- src/coreclr/hosts/coreconsole/coreconsole.cpp
+++ src/coreclr/hosts/coreconsole/coreconsole.cpp
@@ -45,7 +45,7 @@ public:
             m_capacity = m_defaultSize;
         }
         while (m_length + strLen + 1 > m_capacity) {
-            size_t newCapacity = m_capacity * 1.5f;
+            size_t newCapacity = m_capacity * 3 / 2;
             wchar_t* newBuffer = new wchar_t[newCapacity];
             wcsncpy_s(newBuffer, newCapacity, m_buffer, m_length);
             delete[] m_buffer;
