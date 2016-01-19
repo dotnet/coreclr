b281ee86d29356bb6b7d8aae0b659072f667b1b3
Jan Kotas <jkotas@microsoft.com>
2016-01-19 00:04:33 -0800

    Add generic detection of Stream.{Begin|End}{Read|Write} overrides

--- src/mscorlib/src/System/IO/FileStream.cs
+++ src/mscorlib/src/System/IO/FileStream.cs
@@ -1778,10 +1778,6 @@ namespace System.IO {
             return;
         }
 
-#if FEATURE_CORECLR
-        internal override bool OverridesBeginEnd { get { return true; } }
-#endif
-
         [System.Security.SecuritySafeCritical]  // auto-generated
         [HostProtection(ExternalThreading = true)]
         public override IAsyncResult BeginRead(byte[] array, int offset, int numBytes, AsyncCallback userCallback, Object stateObject)
