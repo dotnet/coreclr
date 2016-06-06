88f255fa966c5e9de1fa2c1c7d17db12fe7888d7
wtgodbe <wigodbe@microsoft.com>
2016-06-06 12:37:30 -0700

    Test branch - don't merge

--- netci.groovy
+++ netci.groovy
@@ -26,6 +26,8 @@ def static getOSGroup(def os) {
     return osGroupMap[os]
 }
 
+//Don't merge me
+
 // We use this class (vs variables) so that the static functions can access data here.
 class Constants {
     // Innerloop build OS's
