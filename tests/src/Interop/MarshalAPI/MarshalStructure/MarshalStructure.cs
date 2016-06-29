821254450f274d071822a872058774a075c4d79a
tijoytk <tijoytk@microsoft.com>
2016-06-29 16:44:32 -0700

    MarshalAs(UnManaged.ByValArray) buffer overrun   For non-blittable embedded array in structs we ignored the SizeConst and wrote past the   buffer when number of elementsin the arrayis greater than SizeConst.   Fix is to truncate the array at SizeConst.
    MarshalAs(UnManaged.ByValTStr)
     Very subtle case when the SizeConst == Number of bytes required to marshal , we
     write the null one past the buffer.This happens only on  machine with non-english
     (multi-byte) locale as default. Fix is to check the number of bytes required and
     truncate the correctly leaving space for the terminating null.

--- tests/src/Interop/MarshalAPI/MarshalStructure/MarshalStructure.cs
+++ tests/src/Interop/MarshalAPI/MarshalStructure/MarshalStructure.cs
@@ -5,12 +5,42 @@ class Program
 {
     static int Main(string[] args)
     {
-        MarshalLongerByValArrayInAStruct();
+        VerifyByValBoolArray();
+        VerifyByValArrayInStruct();
+        VerfiyByValDateArray();
         return 100;
     }
-
-    static void VerifyMarshalLongerByValArrayInAStruct()
+    
+    static void VerifyByValBoolArray()
     {
+        var structure1 = new StructWithBoolArray()
+        {
+            array = new bool[]
+            {
+                true,true,true,true
+            }
+        };
+
+        int size = Marshal.SizeOf(structure1);
+        IntPtr memory = Marshal.AllocHGlobal(size + sizeof(Int32));
+
+        try
+        {
+            Marshal.WriteInt32(memory, size, 0xFF);
+            Marshal.StructureToPtr(structure1, memory, false);
+
+            if (Marshal.ReadInt32(memory, size) != 0xFF)
+                throw new Exception("Marshal.StructureToPtr buffer overwritten...");
+        }
+        finally
+        {
+            Marshal.FreeHGlobal(memory);
+        }
+    }
+
+    static void VerifyByValArrayInStruct()
+    {
+        // equal
         var structure1 = new StructWithByValArray()
         {
             array = new StructWithIntField[]
@@ -44,18 +74,23 @@ class Program
                 new StructWithIntField { value = 4 }
          }
         };
-
+        bool expectedException = false;
         size = Marshal.SizeOf(structure2);
         memory = Marshal.AllocHGlobal(size);
         try
         {
             Marshal.StructureToPtr(structure2, memory, false);
         }
-        catch (ArgumentException){ }
+        catch (ArgumentException)
+        {
+            expectedException = true;
+        }
         finally
         {
             Marshal.FreeHGlobal(memory);
         }
+        if (!expectedException)
+            throw new Exception("Expected ArgumentException");
 
         // overflow
         var structure3 = new StructWithByValArray()
@@ -77,7 +112,28 @@ class Program
         {
             Marshal.StructureToPtr(structure3, memory, false);
         }
-        catch (ArgumentException) { }
+        finally
+        {
+            Marshal.FreeHGlobal(memory);
+        }
+    }
+    
+    static void VerfiyByValDateArray()
+    {
+        var structure1 = new StructWithDateArray()
+        {
+           array = new DateTime[]
+           {
+                DateTime.Now, DateTime.Now , DateTime.Now, DateTime.Now, DateTime.Now, DateTime.Now , DateTime.Now, DateTime.Now
+           }
+        };
+
+        int size = Marshal.SizeOf(structure1);
+        IntPtr memory = Marshal.AllocHGlobal(size);
+        try
+        {
+            Marshal.StructureToPtr(structure1, memory, false);
+        }
         finally
         {
             Marshal.FreeHGlobal(memory);
@@ -93,5 +149,17 @@ class Program
     {
         [MarshalAs(UnmanagedType.ByValArray, SizeConst = 5)]
         public StructWithIntField[] array;
+    }
+
+    public struct StructWithBoolArray
+    {
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)]
+        public bool[] array;
+    }
+
+    public struct StructWithDateArray
+    {
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)]
+        public DateTime[] array;
     }
 }
