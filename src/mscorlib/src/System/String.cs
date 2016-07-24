763d99bd1adbf922517dc3b48aefa9d74a6d6025
James Ko <jamesqko@gmail.com>
2016-07-24 00:20:21 -0400

    Readability improvements for the alignment code

--- src/mscorlib/src/System/String.cs
+++ src/mscorlib/src/System/String.cs
@@ -2354,14 +2354,17 @@ namespace System {
 
                     // This calculates the number of bytes until the next aligned address
                     // We have to use uint and 0u - to stop the jit from generating
-                    // some redundant code
-                    int alignment = (int)((0u - (uint)pCh) % 8); // 0 -> 0, 2 -> 6, 4 -> 4, 6 -> 2, 8 -> 0
-                    Contract.Assert(alignment % 2 == 0); // char* from strings should always have an even address
+                    // some redundant code while doing math
+                    uint ualign = (0u - (uint)pCh) % 8; // 0 -> 0, 2 -> 6, 4 -> 4, 6 -> 2, 8 -> 0
+                    Contract.Assert(ualign % 2 == 0); // char* from strings should always have an even address
 
-                    // We also cast to uint here since otherwise the jit generates inefficient
-                    // code to treat the sign bit specially, when we know it'll never be set
-                    alignment =  (int)((uint)alignment / 2u); // we're counting in chars, not bytes, so divide by sizeof(char) which is 2
-                    Contract.Assert(((int)pCh + alignment) % 8 == 0);
+                    ualign /= 2; // we're counting in chars, not bytes, so divide by sizeof(char) which is 2
+                    Contract.Assert(((uint)pCh + ualign) % 8 == 0);
+
+                    // Now we have to cast to int to prevent Roslyn
+                    // from converting everything to long when mixing
+                    // ints and uints
+                    int alignment = (int)ualign;
 
                     if (alignment + Chunk > count)
                     {
