3eb9905d8073b205e4bd9836f0d3fe02497f29fd
James Ko <jamesqko@gmail.com>
2016-08-30 14:23:51 -0400

    Build breaks

--- src/mscorlib/src/System/String.Searching.cs
+++ src/mscorlib/src/System/String.Searching.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Diagnostics.Contracts;
+using System.Globalization;
 using System.Runtime.CompilerServices;
 
 namespace System
