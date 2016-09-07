651ce9ba5add29df0e6752bccc81c23e0d3dbadd
choikwa <code.kchoi@gmail.com>
2016-09-07 10:44:23 -0700

    move define strnlen to all the other similar defines

--- src/pal/inc/pal.h
+++ src/pal/inc/pal.h
@@ -5802,6 +5802,7 @@ CoCreateGuid(OUT GUID * pguid);
 #define _wcstoui64    PAL__wcstoui64
 #define _flushall     PAL__flushall
 #define _vsnprintf    PAL__vsnprintf
+#define strnlen       PAL_strnlen
 
 #ifdef _AMD64_ 
 #define _mm_getcsr    PAL__mm_getcsr
@@ -6953,5 +6954,4 @@ public:
 }
 #endif
 
-#define strnlen PAL_strnlen
 #endif // __PAL_H__
