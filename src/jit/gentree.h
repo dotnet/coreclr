4bb94d831f01a363dc6502bdb0778345451eb9d0
Peter Kukol <peterku@microsoft.com>
2016-09-20 12:15:39 -0700

    Merge conflict fix.

--- src/jit/gentree.h
+++ src/jit/gentree.h
@@ -1578,8 +1578,8 @@ public:
     static const char* OpName(genTreeOps op);
 #endif
 
-#if MEASURE_NODE_SIZE && SMALL_TREE_NODES
-    static const char* OpStructName(genTreeOps op);
+#if defined(DEBUG) || NODEBASH_STATS
+    static const char* OpName(genTreeOps op);
 #endif
 
     //---------------------------------------------------------------------
@@ -1894,10 +1894,6 @@ public:
         assert(OperIsConst());
         gtFlags &= ~GTF_REUSE_REG_VAL;
     }
-
-#if MEASURE_NODE_SIZE
-    static void DumpNodeSizes(FILE* fp);
-#endif
 
 #ifdef DEBUG
 
@@ -2423,8 +2419,6 @@ struct GenTreeStrCon : public GenTree
 // This inherits from UnOp because lclvar stores are Unops
 struct GenTreeLclVarCommon : public GenTreeUnOp
 {
-    friend struct GenTree;
-
 private:
     unsigned _gtLclNum; // The local number. An index into the Compiler::lvaTable array.
     unsigned _gtSsaNum; // The SSA number.
@@ -2524,8 +2518,6 @@ struct GenTreeLclFld : public GenTreeLclVarCommon
 
 struct GenTreeRegVar : public GenTreeLclVarCommon
 {
-    friend struct GenTree;
-
     // TODO-Cleanup: Note that the base class GenTree already has a gtRegNum field.
     // It's not clear exactly why a GT_REG_VAR has a separate field. When
     // GT_REG_VAR is created, the two are identical. It appears that they may
@@ -4017,8 +4009,6 @@ protected:
 
 struct GenTreeObj : public GenTreeBlk
 {
-    friend struct GenTree;
-
     CORINFO_CLASS_HANDLE gtClass; // the class of the object
 
     // If non-null, this array represents the gc-layout of the class.
