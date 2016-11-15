6b5497e9c048eafcdc8ca7563a6b31bca393a8f9
Jeremy Kuhne <jeremy.kuhne@microsoft.com>
2016-11-15 13:18:13 -0800

    Use CoreFX Path code
    This is to address consistency and perf issues by moving the Path
    code down to CoreCLR. It is more of an issue with Windows as
    normalization requires complicated logic and costly platform calls.

---
+++ src/mscorlib/corefx/Interop/OSX/Interop.Libraries.cs
@@ -0,0 +1,17 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+internal static partial class Interop
+{
+    private static partial class Libraries
+    {
+        internal const string CoreFoundationLibrary = "/System/Library/Frameworks/CoreFoundation.framework/CoreFoundation";
+        internal const string CoreServicesLibrary   = "/System/Library/Frameworks/CoreServices.framework/CoreServices";
+        internal const string libproc = "libproc";
+        internal const string LibSystemCommonCrypto = "/usr/lib/system/libcommonCrypto";
+        internal const string LibSystemKernel = "/usr/lib/system/libsystem_kernel";
+        internal const string SystemConfigurationLibrary = "/System/Library/Frameworks/SystemConfiguration.framework/SystemConfiguration";
+        internal const string AppleCryptoNative = "System.Security.Cryptography.Native.Apple";
+    }
+}
