52bad654a942531a813f5a58e97fbb8c59dfa675
Jonghyun Park <parjong@gmail.com>
2016-12-06 09:08:32 +0900

    Revise comment-style

--- src/vm/exceptmacros.h
+++ src/vm/exceptmacros.h
@@ -334,14 +334,14 @@ VOID DECLSPEC_NORETURN DispatchManagedException(PAL_SEHException& ex, bool isHar
             UNREACHABLE();                                                                          \
         }
 
-#else // defined(FEATURE_PAL) && defined(WIN64EXCEPTIONS)
+#else // FEATURE_PAL && WIN64EXCEPTIONS
 
 #define INSTALL_MANAGED_EXCEPTION_DISPATCHER
 #define UNINSTALL_MANAGED_EXCEPTION_DISPATCHER
 #define INSTALL_UNHANDLED_MANAGED_EXCEPTION_TRAP
 #define UNINSTALL_UNHANDLED_MANAGED_EXCEPTION_TRAP
 
-#endif // defined(FEATURE_PAL) && defined(WIN64EXCEPTIONS)
+#endif // FEATURE_PAL && WIN64EXCEPTIONS
 
 #define INSTALL_UNWIND_AND_CONTINUE_HANDLER_NO_PROBE                                        \
     {                                                                                       \
