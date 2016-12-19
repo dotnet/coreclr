523554c61da105d689690caca0f5dafcd2060bdc
Alexander Radchenko <radchenkosasha@gmail.com>
2016-12-19 23:28:57 +0700

    fixed (byte* pBytes = &bytes[0])

--- src/mscorlib/src/System/Text/Encoding.cs
+++ src/mscorlib/src/System/Text/Encoding.cs
@@ -1035,7 +1035,7 @@ namespace System.Text
                         return Array.Empty<byte>();
 
                     byte[] bytes = new byte[byteCount];
-                    fixed (byte* pBytes = bytes)
+                    fixed (byte* pBytes = &bytes[0])
                     {
                         int bytesReceived = GetBytes(pChar + index, count, pBytes, byteCount);
                         Debug.Assert(byteCount == bytesReceived);
