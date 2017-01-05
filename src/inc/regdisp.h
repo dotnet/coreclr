1fd427c80ab658c148e2f68d5b0a107fd9d5a304
Jonghyun Park <parjong@gmail.com>
2017-01-05 11:26:38 +0900

    Fix incorrect comment

--- src/inc/regdisp.h
+++ src/inc/regdisp.h
@@ -434,14 +434,14 @@ inline void FillRegDisplay(const PREGDISPLAY pRD, PT_CONTEXT pctx, PT_CONTEXT pC
     }
 
     pRD->ctxPtrsOne.Lr = &pctx->Lr;
-#elif defined(_TARGET_X86_) // _TARGET_X86_
+#elif defined(_TARGET_X86_) // _TARGET_ARM_
     for (int i = 0; i < 4; ++i)
     {
         *(&pRD->ctxPtrsOne.Ebx + i) = (&pctx->Ebx + i);
     }
-#else
+#else // _TARGET_X86_
     PORTABILITY_ASSERT("FillRegDisplay");
-#endif // ELSE
+#endif // _TARGET_???_ (ELSE)
 
     // Setup the references
     pRD->pCurrentContextPointers = &pRD->ctxPtrsOne;
