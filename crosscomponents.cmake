8ff52dbfb74031a1847a088a265336c12708d018
Hyeongseok Oh <hseok82.oh@samsung.com>
2017-01-12 16:34:10 +0900

    [ARM32/Linx] cross-architecture build: restrict build project list
    Restrict build project for ARM32/Linux cross-architecture
    We can build these projects with PR #8866 and #8917. (release build)
    - crossgen
    - clrjit
    - protojit

--- crosscomponents.cmake
+++ crosscomponents.cmake
@@ -7,4 +7,13 @@ set (CLR_CROSS_COMPONENTS_LIST
   sos
   clrjit
   protojit
-)  
+)
+
+if(CLR_CMAKE_PLATFORM_LINUX AND CLR_CMAKE_TARGET_ARCH_ARM)
+    list(REMOVE_ITEM CLR_CROSS_COMPONENTS_LIST
+        mscordaccore
+        mscordbi
+        sos
+    )
+endif()
+
