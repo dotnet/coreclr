c5d2a2582a2dadbd0aeff67d99e7a5a67b946ad1
Hyeongseok Oh <hseok82.oh@samsung.com>
2017-01-13 10:25:03 +0900

    modify crosscomponents.cmake
    Add list items when not Linux, instead remove in Linux

--- crosscomponents.cmake
+++ crosscomponents.cmake
@@ -1,19 +1,15 @@
 add_definitions(-DCROSS_COMPILE)
 
-set (CLR_CROSS_COMPONENTS_LIST  
-  crossgen   
-  mscordaccore   
-  mscordbi   
-  sos
-  clrjit
-  protojit
+set (CLR_CROSS_COMPONENTS_LIST
+    crossgen   
+    clrjit
+    protojit
 )
 
-if(CLR_CMAKE_PLATFORM_LINUX AND CLR_CMAKE_TARGET_ARCH_ARM)
-    list(REMOVE_ITEM CLR_CROSS_COMPONENTS_LIST
+if(NOT CLR_CMAKE_PLATFORM_LINUX)
+    list (APPEND CLR_CROSS_COMPONENTS_LIST
         mscordaccore
         mscordbi
         sos
     )
 endif()
-
