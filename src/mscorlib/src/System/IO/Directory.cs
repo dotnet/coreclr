e98a01326cd2d60edbc62f72be370ae0e00b17cf
danmosemsft <danmose@microsoft.com>
2017-02-03 21:30:45 -0800

    Fix Unix build

--- src/mscorlib/src/System/IO/Directory.cs
+++ src/mscorlib/src/System/IO/Directory.cs
@@ -29,6 +29,62 @@ namespace System.IO
     [ComVisible(true)]
     internal static class Directory {
 
+        // Private class that holds search data that is passed around 
+        // in the heap based stack recursion
+        internal sealed class SearchData
+        {
+            public SearchData(String fullPath, String userPath, SearchOption searchOption)
+            {
+                Contract.Requires(fullPath != null && fullPath.Length > 0);
+                Contract.Requires(userPath != null && userPath.Length > 0);
+                Contract.Requires(searchOption == SearchOption.AllDirectories || searchOption == SearchOption.TopDirectoryOnly);
+
+                this.fullPath = fullPath;
+                this.userPath = userPath;
+                this.searchOption = searchOption;
+            }
+
+            public readonly string fullPath;     // Fully qualified search path excluding the search criteria in the end (ex, c:\temp\bar\foo)
+            public readonly string userPath;     // User specified path (ex, bar\foo)
+            public readonly SearchOption searchOption;
+        }
+
+        public static IEnumerable<String> EnumerateFiles(String path, String searchPattern, SearchOption searchOption)
+        {
+            if (path == null)
+                throw new ArgumentNullException(nameof(path));
+            if (searchPattern == null)
+                throw new ArgumentNullException(nameof(searchPattern));
+            if ((searchOption != SearchOption.TopDirectoryOnly) && (searchOption != SearchOption.AllDirectories))
+                throw new ArgumentOutOfRangeException(nameof(searchOption), Environment.GetResourceString("ArgumentOutOfRange_Enum"));
+            Contract.Ensures(Contract.Result<IEnumerable<String>>() != null);
+            Contract.EndContractBlock();
+
+            return InternalEnumerateFiles(path, searchPattern, searchOption);
+        }
+
+        private static IEnumerable<String> InternalEnumerateFiles(String path, String searchPattern, SearchOption searchOption)
+        {
+            Contract.Requires(path != null);
+            Contract.Requires(searchPattern != null);
+            Contract.Requires(searchOption == SearchOption.AllDirectories || searchOption == SearchOption.TopDirectoryOnly);
+            Contract.Ensures(Contract.Result<IEnumerable<String>>() != null);
+
+            return EnumerateFileSystemNames(path, searchPattern, searchOption, true, false);
+        }
+
+        private static IEnumerable<String> EnumerateFileSystemNames(String path, String searchPattern, SearchOption searchOption,
+                                                            bool includeFiles, bool includeDirs)
+        {
+            Contract.Requires(path != null);
+            Contract.Requires(searchPattern != null);
+            Contract.Requires(searchOption == SearchOption.AllDirectories || searchOption == SearchOption.TopDirectoryOnly);
+            Contract.Ensures(Contract.Result<IEnumerable<String>>() != null);
+
+            return FileSystemEnumerableFactory.CreateFileNameIterator(path, path, searchPattern,
+                                                                        includeFiles, includeDirs, searchOption, true);
+        }
+
         internal static String InternalGetDirectoryRoot(String path) {
               if (path == null) return null;
             return path.Substring(0, PathInternal.GetRootLength(path));
