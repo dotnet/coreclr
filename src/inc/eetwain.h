332e7adb85b7e746491dc759130ff51e5dde44d7
Jonghyun Park <parjong@gmail.com>
2017-02-09 08:55:30 +0900

    Clean up GetAddrOfSecurityObject

--- src/inc/eetwain.h
+++ src/inc/eetwain.h
@@ -245,11 +245,13 @@ virtual bool EnumGcRefs(PREGDISPLAY     pContext,
                         DWORD           relOffsetOverride = NO_OVERRIDE_OFFSET) = 0;
 #endif // !CROSSGEN_COMPILE
 
+#if !defined(DACCESS_COMPILE) && !defined(CROSSGEN_COMPILE)
 /*
     Return the address of the local security object reference
     (if available).
 */
 virtual OBJECTREF* GetAddrOfSecurityObject(CrawlFrame *pCF) = 0;
+#endif // !DACCESS_COMPILE && !CROSSGEN_COMPILE
 
 /*
     For a non-static method, "this" pointer is passed in as argument 0.
@@ -518,8 +520,10 @@ static OBJECTREF* GetAddrOfSecurityObjectFromCachedInfo(
         StackwalkCacheUnwindInfo * stackwalkCacheUnwindInfo);
 #endif // _TARGET_X86_
 
+#if !defined(DACCESS_COMPILE) && !defined(CROSSGEN_COMPILE)
 virtual
 OBJECTREF* GetAddrOfSecurityObject(CrawlFrame *pCF) DAC_UNEXPECTED();
+#endif // !DACCESS_COMPILE && !CROSSGEN_COMPILE
 
 virtual
 OBJECTREF GetInstance(
