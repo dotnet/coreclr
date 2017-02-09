80d9726913dd07c2a2b1ce1105a829c5dff6a792
Jonghyun Park <parjong@gmail.com>
2017-02-09 09:09:44 +0900

    Clean up EECodeManager::GetInstance

--- src/inc/eetwain.h
+++ src/inc/eetwain.h
@@ -251,6 +251,7 @@ virtual bool EnumGcRefs(PREGDISPLAY     pContext,
 */
 virtual OBJECTREF* GetAddrOfSecurityObject(CrawlFrame *pCF) = 0;
 
+#ifndef CROSSGEN_COMPILE
 /*
     For a non-static method, "this" pointer is passed in as argument 0.
     However, if there is a "ldarga 0" or "starg 0" in the IL, 
@@ -263,6 +264,7 @@ virtual OBJECTREF* GetAddrOfSecurityObject(CrawlFrame *pCF) = 0;
 */
 virtual OBJECTREF GetInstance(PREGDISPLAY     pContext,
                               EECodeInfo*     pCodeInfo) = 0;
+#endif // CROSSGEN_COMPILE
 
 /*
     Returns the extra argument passed to to shared generic code if it is still alive.
@@ -521,10 +523,12 @@ static OBJECTREF* GetAddrOfSecurityObjectFromCachedInfo(
 virtual
 OBJECTREF* GetAddrOfSecurityObject(CrawlFrame *pCF) DAC_UNEXPECTED();
 
+#ifndef CROSSGEN_COMPILE
 virtual
 OBJECTREF GetInstance(
                 PREGDISPLAY     pContext,
                 EECodeInfo *    pCodeInfo);
+#endif // CROSSGEN_COMPILE
 
 /*
     Returns the extra argument passed to to shared generic code if it is still alive.
