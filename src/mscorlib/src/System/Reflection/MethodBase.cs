e419f6c6b9e2c1b30c69f3b39b08fbca1b6eb630
Atsushi Kanamori <atsushik@microsoft.com>
2017-03-15 13:31:40 -0700

    Copy over CoreRt MethodBase.cs (this is now just a reordering)

--- src/mscorlib/src/System/Reflection/MethodBase.cs
+++ src/mscorlib/src/System/Reflection/MethodBase.cs
@@ -2,14 +2,65 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Diagnostics;
 using System.Globalization;
+using System.Diagnostics;
 
 namespace System.Reflection
 {
     public abstract partial class MethodBase : MemberInfo
     {
         protected MethodBase() { }
+
+        public abstract ParameterInfo[] GetParameters();
+        public abstract MethodAttributes Attributes { get; }
+        public virtual MethodImplAttributes MethodImplementationFlags => GetMethodImplementationFlags();
+        public abstract MethodImplAttributes GetMethodImplementationFlags();
+        public virtual MethodBody GetMethodBody() { throw new InvalidOperationException(); }
+        public virtual CallingConventions CallingConvention => CallingConventions.Standard;
+
+        public bool IsAbstract => (Attributes & MethodAttributes.Abstract) != 0;
+        public bool IsConstructor
+        {
+            get
+            {
+                // To be backward compatible we only return true for instance RTSpecialName ctors.
+                return (this is ConstructorInfo &&
+                        !IsStatic &&
+                        ((Attributes & MethodAttributes.RTSpecialName) == MethodAttributes.RTSpecialName));
+            }
+        }
+        public bool IsFinal => (Attributes & MethodAttributes.Final) != 0;
+        public bool IsHideBySig => (Attributes & MethodAttributes.HideBySig) != 0;
+        public bool IsSpecialName => (Attributes & MethodAttributes.SpecialName) != 0;
+        public bool IsStatic => (Attributes & MethodAttributes.Static) != 0;
+        public bool IsVirtual => (Attributes & MethodAttributes.Virtual) != 0;
+
+        public bool IsAssembly => (Attributes & MethodAttributes.MemberAccessMask) == MethodAttributes.Assembly;
+        public bool IsFamily => (Attributes & MethodAttributes.MemberAccessMask) == MethodAttributes.Family;
+        public bool IsFamilyAndAssembly => (Attributes & MethodAttributes.MemberAccessMask) == MethodAttributes.FamANDAssem;
+        public bool IsFamilyOrAssembly => (Attributes & MethodAttributes.MemberAccessMask) == MethodAttributes.FamORAssem;
+        public bool IsPrivate => (Attributes & MethodAttributes.MemberAccessMask) == MethodAttributes.Private;
+        public bool IsPublic => (Attributes & MethodAttributes.MemberAccessMask) == MethodAttributes.Public;
+
+        public virtual bool IsConstructedGenericMethod => IsGenericMethod && !IsGenericMethodDefinition;
+        public virtual bool IsGenericMethod => false;
+        public virtual bool IsGenericMethodDefinition => false;
+        public virtual Type[] GetGenericArguments() { throw new NotSupportedException(SR.NotSupported_SubclassOverride); }
+        public virtual bool ContainsGenericParameters => false;
+
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        public object Invoke(object obj, object[] parameters) => Invoke(obj, BindingFlags.Default, binder: null, parameters: parameters, culture: null);
+        public abstract object Invoke(object obj, BindingFlags invokeAttr, Binder binder, object[] parameters, CultureInfo culture);
+
+        public abstract RuntimeMethodHandle MethodHandle { get; }
+
+        public virtual bool IsSecurityCritical { get { throw NotImplemented.ByDesign; } }
+        public virtual bool IsSecuritySafeCritical { get { throw NotImplemented.ByDesign; } }
+        public virtual bool IsSecurityTransparent { get { throw NotImplemented.ByDesign; } }
+
+        public override bool Equals(object obj) => base.Equals(obj);
+        public override int GetHashCode() => GetHashCode();
 
         public static bool operator ==(MethodBase left, MethodBase right)
         {
@@ -31,81 +82,5 @@ namespace System.Reflection
         }
 
         public static bool operator !=(MethodBase left, MethodBase right) => !(left == right);
-
-        public override bool Equals(object obj) => base.Equals(obj);
-        public override int GetHashCode() => GetHashCode();
-
-
-        public abstract ParameterInfo[] GetParameters();
-
-        public virtual MethodImplAttributes MethodImplementationFlags => GetMethodImplementationFlags();
-
-        public abstract MethodImplAttributes GetMethodImplementationFlags();
-
-        public abstract RuntimeMethodHandle MethodHandle { get; }
-
-        public abstract MethodAttributes Attributes { get; }
-
-        public abstract object Invoke(object obj, BindingFlags invokeAttr, Binder binder, object[] parameters, CultureInfo culture);
-
-        public virtual CallingConventions CallingConvention => CallingConventions.Standard;
-
-        public virtual Type[] GetGenericArguments() { throw new NotSupportedException(SR.NotSupported_SubclassOverride); }
-
-        public virtual bool IsGenericMethodDefinition => false;
-
-        public virtual bool ContainsGenericParameters => false;
-
-        public virtual bool IsGenericMethod => false;
-
-        public virtual bool IsConstructedGenericMethod => IsGenericMethod && !IsGenericMethodDefinition;
-
-        public virtual bool IsSecurityCritical { get { throw NotImplemented.ByDesign; } }
-
-        public virtual bool IsSecuritySafeCritical { get { throw NotImplemented.ByDesign; } }
-
-        public virtual bool IsSecurityTransparent { get { throw NotImplemented.ByDesign; } }
-
-
-        [DebuggerHidden]
-        [DebuggerStepThrough]
-        public object Invoke(object obj, object[] parameters) => Invoke(obj, BindingFlags.Default, binder: null, parameters: parameters, culture: null);
-
-        public bool IsPublic => (Attributes & MethodAttributes.MemberAccessMask) == MethodAttributes.Public;
-
-        public bool IsPrivate => (Attributes & MethodAttributes.MemberAccessMask) == MethodAttributes.Private;
-
-        public bool IsFamily => (Attributes & MethodAttributes.MemberAccessMask) == MethodAttributes.Family;
-
-        public bool IsAssembly => (Attributes & MethodAttributes.MemberAccessMask) == MethodAttributes.Assembly;
-
-        public bool IsFamilyAndAssembly => (Attributes & MethodAttributes.MemberAccessMask) == MethodAttributes.FamANDAssem;
-
-        public bool IsFamilyOrAssembly => (Attributes & MethodAttributes.MemberAccessMask) == MethodAttributes.FamORAssem;
-
-        public bool IsStatic => (Attributes & MethodAttributes.Static) != 0;
-
-        public bool IsFinal => (Attributes & MethodAttributes.Final) != 0;
-
-        public bool IsVirtual => (Attributes & MethodAttributes.Virtual) != 0;
-
-        public bool IsHideBySig => (Attributes & MethodAttributes.HideBySig) != 0;
-
-        public bool IsAbstract => (Attributes & MethodAttributes.Abstract) != 0;
-
-        public bool IsSpecialName => (Attributes & MethodAttributes.SpecialName) != 0;
-
-        public bool IsConstructor
-        {
-            get
-            {
-                // To be backward compatible we only return true for instance RTSpecialName ctors.
-                return (this is ConstructorInfo &&
-                        !IsStatic &&
-                        ((Attributes & MethodAttributes.RTSpecialName) == MethodAttributes.RTSpecialName));
-            }
-        }
-
-        public virtual MethodBody GetMethodBody() { throw new InvalidOperationException(); }
     }
 }
