8b2934c7f8fe255101434592d65fcd815336d84c
Atsushi Kanamori <atsushik@microsoft.com>
2017-03-16 06:46:45 -0700

    Reduce each file to its namesake class.

--- src/mscorlib/src/System/Reflection/RuntimeMethodInfo.cs
+++ src/mscorlib/src/System/Reflection/RuntimeMethodInfo.cs
@@ -23,65 +23,6 @@ namespace System.Reflection
     using System.Runtime.CompilerServices;
 
     [Serializable]
-    public abstract class MethodInfo : MethodBase
-    {
-        #region Constructor
-        protected MethodInfo() { }
-        #endregion
-
-        public static bool operator ==(MethodInfo left, MethodInfo right)
-        {
-            if (ReferenceEquals(left, right))
-                return true;
-
-            if ((object)left == null || (object)right == null ||
-                left is RuntimeMethodInfo || right is RuntimeMethodInfo)
-            {
-                return false;
-            }
-            return left.Equals(right);
-        }
-
-        public static bool operator !=(MethodInfo left, MethodInfo right)
-        {
-            return !(left == right);
-        }
-
-        public override bool Equals(object obj)
-        {
-            return base.Equals(obj);
-        }
-
-        public override int GetHashCode()
-        {
-            return base.GetHashCode();
-        }
-
-        #region MemberInfo Overrides
-        public override MemberTypes MemberType { get { return System.Reflection.MemberTypes.Method; } }
-        #endregion
-
-        #region Public Abstract\Virtual Members
-        public virtual Type ReturnType { get { throw new NotImplementedException(); } }
-
-        public virtual ParameterInfo ReturnParameter { get { throw new NotImplementedException(); } }
-
-        public abstract ICustomAttributeProvider ReturnTypeCustomAttributes { get; }
-
-        public abstract MethodInfo GetBaseDefinition();
-
-        public override Type[] GetGenericArguments() { throw new NotSupportedException(Environment.GetResourceString("NotSupported_SubclassOverride")); }
-
-        public virtual MethodInfo GetGenericMethodDefinition() { throw new NotSupportedException(Environment.GetResourceString("NotSupported_SubclassOverride")); }
-
-        public virtual MethodInfo MakeGenericMethod(params Type[] typeArguments) { throw new NotSupportedException(Environment.GetResourceString("NotSupported_SubclassOverride")); }
-
-        public virtual Delegate CreateDelegate(Type delegateType) { throw new NotSupportedException(Environment.GetResourceString("NotSupported_SubclassOverride")); }
-        public virtual Delegate CreateDelegate(Type delegateType, Object target) { throw new NotSupportedException(Environment.GetResourceString("NotSupported_SubclassOverride")); }
-        #endregion
-    }
-
-    [Serializable]
     internal sealed class RuntimeMethodInfo : MethodInfo, ISerializable, IRuntimeMethodInfo
     {
         #region Private Data Members
