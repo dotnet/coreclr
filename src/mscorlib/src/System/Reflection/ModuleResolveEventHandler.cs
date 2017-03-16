b927ca6c5c4160e37eaf9c04a17eee7f27922737
Atsushi Kanamori <atsushik@microsoft.com>
2017-03-16 09:21:23 -0700

    Fix build break.

--- src/mscorlib/src/System/Reflection/ModuleResolveEventHandler.cs
+++ src/mscorlib/src/System/Reflection/ModuleResolveEventHandler.cs
@@ -2,40 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-/*=============================================================================
-**
-**
-** 
-** 
-**
-**
-** Purpose: For Assembly-related stuff.
-**
-**
-=============================================================================*/
-
 namespace System.Reflection
 {
-    using System;
-    using System.Collections;
-    using System.Collections.Generic;
-    using CultureInfo = System.Globalization.CultureInfo;
-    using System.Security;
-    using System.Security.Policy;
-    using System.IO;
-    using StringBuilder = System.Text.StringBuilder;
-    using System.Configuration.Assemblies;
-    using StackCrawlMark = System.Threading.StackCrawlMark;
-    using System.Runtime.InteropServices;
-    using System.Runtime.CompilerServices;
-    using System.Runtime.Serialization;
-    using Microsoft.Win32;
-    using System.Threading;
-    using __HResults = System.__HResults;
-    using System.Runtime.Versioning;
-    using System.Diagnostics.Contracts;
-    using System.Runtime.Loader;
-
     [Serializable]
     public delegate Module ModuleResolveEventHandler(Object sender, ResolveEventArgs e);
 }
