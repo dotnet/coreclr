7c8d8ce15058681082bff99427ff1fa60ca9a0d7
Jonghyun Park <parjong@gmail.com>
2017-03-30 13:14:38 +0900

    Clean up Windows-specific EH code

--- src/vm/ceemain.cpp
+++ src/vm/ceemain.cpp
@@ -1893,6 +1893,7 @@ part2:
 
                 TerminateStackProbes();
 
+#ifndef FEATURE_PAL
                 // Unregister our vectored exception and continue handlers from the OS.
                 // This will ensure that if any other DLL unload (after ours) has an exception,
                 // we wont attempt to process that exception (which could lead to various
@@ -1905,6 +1906,7 @@ part2:
                 //
                 // 2) Only when the runtime is processing DLL_PROCESS_DETACH. 
                 CLRRemoveVectoredHandlers();
+#endif // !FEATURE_PAL
 
 #if USE_DISASSEMBLER
                 Disassembler::StaticClose();
@@ -2580,10 +2582,12 @@ BOOL STDMETHODCALLTYPE EEDllMain( // TRUE on success, FALSE on error.
                     LOG((LF_STARTUP, INFO3, "EEShutDown invoked from EEDllMain"));
                     EEShutDown(TRUE); // shut down EE if it was started up
                 }
+#ifndef FEATURE_PAL
                 else
                 {
                     CLRRemoveVectoredHandlers();
                 }
+#endif // !FEATURE_PAL
                 break;
             }
 
