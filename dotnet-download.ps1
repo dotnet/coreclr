2cbd9ae597a199eab68451ccc5a1923ea4506c54
lgomez <lgomez@hexacta.com>
2017-03-30 12:55:45 -0300

    Code review feedback changes

--- dotnet-download.ps1
+++ dotnet-download.ps1
@@ -1,40 +1,37 @@
 param(
-[parameter(Mandatory = $true)]
-$DOTNET_REMOTE_PATH,
-[parameter(Mandatory = $true)]
-$DOTNET_LOCAL_PATH,
-[parameter(Mandatory = $true)]
-$DOTNET_PATH)
+    [parameter(Mandatory=$true)]$DotnetRemotePath,
+    [parameter(Mandatory=$true)]$DotnetLocalPath,
+    [parameter(Mandatory = $true)]$DotnetPath
+)
 
-$retryCount = 0;
-$success = $false;
+$retryCount = 0
+$success = $false
 
 do {
-     try {
-          Write-Output "Downloading from $DOTNET_REMOTE_PATH"
-          (New-Object Net.WebClient).DownloadFile($DOTNET_REMOTE_PATH, $DOTNET_LOCAL_PATH);
-          $success = $true;
-        } catch { 
-            if ($retryCount -ge 6) {
-                Write-Output "Maximum of 5 retries exceeded. Aborting"
-                throw;
-            } 
-            else { 
-                $retryCount++;
-                $retryTime = 5 * $retryCount;
-                Write-Output "Download failed. Retrying in $retryTime seconds"
-                Start-Sleep -Seconds (5 * $retryCount);
-            }
+    try {
+        Write-Output "Downloading from $DotnetRemotePath"
+        (New-Object Net.WebClient).DownloadFile($DotnetRemotePath, $DotnetLocalPath)
+        $success = $true
+    } catch { 
+        if ($retryCount -ge 6) {
+            Write-Output "Maximum of 5 retries exceeded. Aborting"
+            throw
+        } 
+        else { 
+            $retryCount++
+            $retryTime = 5 * $retryCount
+            Write-Output "Download failed. Retrying in $retryTime seconds"
+            Start-Sleep -Seconds (5 * $retryCount)
         }
     }
-while ($success -eq $false);
+} while ($success -eq $false)
 
 Write-Output "Download finished"
-Add-Type -Assembly 'System.IO.Compression.FileSystem' -ErrorVariable AddTypeErrors; 
+Add-Type -Assembly 'System.IO.Compression.FileSystem' -ErrorVariable AddTypeErrors
 
 if ($AddTypeErrors.Count -eq 0) { 
-    [System.IO.Compression.ZipFile]::ExtractToDirectory($DOTNET_LOCAL_PATH, $DOTNET_PATH) 
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($DotnetLocalPath, $DotnetPath) 
 }
 else { 
-    (New-Object -com shell.application).namespace($DOTNET_PATH).CopyHere((new-object -com shell.application).namespace($DOTNET_LOCAL_PATH).Items(), 16) 
+    (New-Object -com shell.application).namespace($DotnetPath).CopyHere((new-object -com shell.application).namespace($DotnetLocalPath).Items(), 16)
 }
