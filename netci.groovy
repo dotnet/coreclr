6c079c9561a11a705b39b88b75c2732a31b4ba01
Matt Mitchell <mmitche@microsoft.com>
2017-04-05 08:03:17 -0700

    Use setMachineAffinity for arm64 (#10559)

--- netci.groovy
+++ netci.groovy
@@ -84,10 +84,7 @@ class Constants {
 
 def static setMachineAffinity(def job, def os, def architecture) {
     if (architecture == 'arm64' && os == 'Windows_NT') {
-        // For cross compilation
-        job.with {
-            label('arm64')
-        }
+        Utilities.setMachineAffinity(job, os, 'latest-arm64');
     } else if ((architecture == 'arm' || architecture == 'arm64') && os == 'Ubuntu') {
         Utilities.setMachineAffinity(job, os, 'arm-cross-latest');
     } else {
