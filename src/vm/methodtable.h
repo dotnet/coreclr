c7ade772b17562d8d9027275ce971c912fc1abe4
Yi Zhang (CLR) <yizhang82@users.noreply.github.com>
2017-04-05 21:04:25 -0700

    Default Interface Method Prototype (#10505)
    * allow non-zero RVA on abstract interface method in ilasm
    
    * Revert "allow non-zero RVA on abstract interface method in ilasm"
    
    This reverts commit eecb8024e58f14a20e5e49359f38019f5768ac41.
    
    * add a test case and allow virtual non-abstract method in ilasm
    
    * allow non-abstract methods in the loader
    
    * fixup dispatch map
    
    * support for simple default interface method scenario
    
    * fix a bug with incorrect usage of MethodIterator skpping the first method. add a test case for overriding but it may not be what we want
    
    * add another simple test case for base class
    
    * allow private/internal methods in ilasm and add a explict impl test
    
    * update reference to mscorlib in il test
    
    * add shared generics and variance case
    
    * allow interface dispatch to return instantiating stubs with the right PARAM_TYPE calling conv
    
    * simple factoring and add a valuetype test case
    
    * add a test case for generic virtual methods
    
    * a bit more refactoring by moving the CALLCONV_PARAMTYPE logic inside getMethodSigInternal
    
    * support explicit methodimpl and remove implicit methodimpl test case
    
    * update test cases to give more clear output
    
    * Fix a bug where GetMethodDescForSlot chokes on interface MethodDesc without precode. This is accdentally discovered by methodimpl test case when iterating methods on a default interface method that has already been JITted
    
    * cleanup code after review and add a bit more comments
    
    * update comments
    
    * only use custom ILAsm for default interface methods tests - some tests are choking on CoreCLR ilasm for security related stuff
    
    * address comments and allow instance methods, and add a constraint value type call test scenario
    
    * disable the failing protected method scenario

--- src/vm/methodtable.h
+++ src/vm/methodtable.h
@@ -2483,6 +2483,14 @@ public:
         UINT32         slotNumber, 
         DispatchSlot * pImplSlot);
 
+
+#ifndef DACCESS_COMPILE
+    BOOL FindDefaultMethod(
+        MethodDesc *pInterfaceMD,
+        MethodTable *pObjectMT,
+        MethodDesc **ppDefaultMethod);
+#endif // DACCESS_COMPILE
+
     DispatchSlot FindDispatchSlot(UINT32 typeID, UINT32 slotNumber);
 
     DispatchSlot FindDispatchSlot(DispatchToken tok);
