66ee78591c3aa800c8863a40afe89fb3a54adf1b
Tarun <tarunpothulapati@outlook.com>
2017-05-04 22:14:09 +0530

    Replaced the Dictionary.GetValueOrDefault Methods

--- src/mscorlib/src/System/RtType.cs
+++ src/mscorlib/src/System/RtType.cs
@@ -1200,7 +1200,7 @@ namespace System
                         {
                             string name = eventInfo.Name;
 
-                            if (csEventInfos.ContainsKey(name) ==true)
+                            if (csEventInfos.ContainsKey(name))
                                 continue;
 
                             csEventInfos[name] = eventInfo;
@@ -1355,10 +1355,8 @@ namespace System
                             if (csPropertyInfos != null)
                             {
                                 string name = propertyInfo.Name;
-
                                 List<RuntimePropertyInfo> cache;
-                                csPropertyInfos.TryGetValue(name,out cache);
-                                if (cache == null)
+                                if (!csPropertyInfos.TryGetValue(name, out cache))
                                 {
                                     cache = new List<RuntimePropertyInfo>(1);
                                     csPropertyInfos[name] = cache;
