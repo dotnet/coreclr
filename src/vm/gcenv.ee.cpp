242ec8c917b9a104b0536690f28c267bfe3da646
Sean Gillespie <segilles@microsoft.com>
2017-05-17 16:09:39 -0700

    Fix an incorrect comment

--- src/vm/gcenv.ee.cpp
+++ src/vm/gcenv.ee.cpp
@@ -1021,15 +1021,6 @@ bool GCToEEInterface::GetBooleanConfigValue(const char* key, bool* value)
 
     if (strcmp(key, "GCRetainVM") == 0)
     {
-        // GCRetainVM is strange in that it is derived from startup flags
-        // on unixes, but not on windows. This call preserves the current
-        // behavior.
-        //
-        // This is probably not optimal. The problem is that users of ICLRRuntimeHost4
-        // (i.e. corerun) are free to specify their own startup flags as they see fit, while
-        // users of the Unix hosting API (coreclr_initialize and friends, i.e. unix corerun) are
-        // not free to do so - they must pass a list of key-value string pairs,
-        // from which the runtime constructs startup flags and hands to ICLRRuntimeHost4.
         *value = !!g_pConfig->GetGCRetainVM();
         return true;
     }
