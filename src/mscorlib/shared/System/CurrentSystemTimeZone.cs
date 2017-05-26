2e5e368dff7b3e5c37384368e9cce94dd327a224
Viktor Hofer <viktor.hofer@microsoft.com>
2017-05-26 15:41:45 +0200

    TimeZone serializable added

--- src/mscorlib/shared/System/CurrentSystemTimeZone.cs
+++ src/mscorlib/shared/System/CurrentSystemTimeZone.cs
@@ -28,6 +28,7 @@ using System.Runtime.Versioning;
 namespace System
 {
     [Obsolete("System.CurrentSystemTimeZone has been deprecated.  Please investigate the use of System.TimeZoneInfo.Local instead.")]
+    [Serializable]
     internal partial class CurrentSystemTimeZone : TimeZone
     {
         // Standard offset in ticks to the Universal time if
