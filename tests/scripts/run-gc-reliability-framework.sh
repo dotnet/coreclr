cba8dc0e250a847707d57bc1667b014fad6549ee
Sean Gillespie <segilles@microsoft.com>
2017-05-31 09:29:09 -0700

    Don't save standard out, Jenkins already does this

--- tests/scripts/run-gc-reliability-framework.sh
+++ tests/scripts/run-gc-reliability-framework.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-set -o pipefail
 
 export CORE_ROOT=`pwd`/bin/tests/Windows_NT.$1.$2/Tests/coreoverlay
 FRAMEWORK_DIR=`pwd`/bin/tests/Windows_NT.$1.$2/GC/Stress/Framework/ReliabilityFramework
-$CORE_ROOT/corerun $FRAMEWORK_DIR/ReliabilityFramework.exe $FRAMEWORK_DIR/testmix_gc.config | tee stdout.txt
+$CORE_ROOT/corerun $FRAMEWORK_DIR/ReliabilityFramework.exe $FRAMEWORK_DIR/testmix_gc.config
 EXIT_CODE=$?
 if [ $EXIT_CODE -eq 100 ]
 then
