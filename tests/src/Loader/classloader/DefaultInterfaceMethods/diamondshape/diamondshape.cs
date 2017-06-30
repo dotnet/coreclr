2e7eb177a193a87a48b7992240769c70cc5a5093
Yi Zhang (CLR) <yzha@microsoft.com>
2017-06-30 11:10:47 -0700

    Add more diamond shape pos/neg tests

--- tests/src/Loader/classloader/DefaultInterfaceMethods/diamondshape/diamondshape.cs
+++ tests/src/Loader/classloader/DefaultInterfaceMethods/diamondshape/diamondshape.cs
@@ -55,17 +55,17 @@ interface I1
 
 interface I2 : I1
 {
-    // int I1.Func(int a);
+    // int I1.Func(int a) { return a + 2; }
 }
 
 interface I3 : I1
 {
-    // int I1.Func(int a);
+    // int I1.Func(int a) { return a + 3; }
 }
 
 interface I4 : I2, I3
 {
-    // int I1.Func(int a);
+    // int I1.Func(int a) { return a + 4; }
 }
 
 class I4Class : I4
@@ -74,8 +74,49 @@ class I4Class : I4
     int I1.Func(int a)
     {
         Console.WriteLine("At I4Class.Func");
-        return a + 2;
+        return a + 4;
     }        
+}
+
+interface I5: I1 
+{
+    // int I1.Func(int a) { return a + 5; }
+}
+
+interface I6: I1 
+{
+    // int I1.Func(int a) { return a + 6; }
+}
+
+interface I7: I5, I6
+{
+    // int I1.Func(int a) { return a + 7; }
+}
+
+interface I8: I6, I7
+{
+    // int I1.Func(int a) { return a + 8; }
+}
+
+class I47Class: I4, I7
+{
+    // @REMOVE
+    int I1.Func(int a)
+    {
+        Console.WriteLine("At I4Class.Func");
+        return a + 8;
+    }            
+
+}
+
+class I8Class: I8
+{
+    // @REMOVE
+    int I1.Func(int a)
+    {
+        Console.WriteLine("At I4Class.Func");
+        return a + 8;
+    }            
 }
 
 class Program
@@ -89,7 +130,19 @@ class Program
         try
         {
              foo.Foo(10);
-             Test.Assert(false, "Expecting exception");
+             Test.Assert(false, "Expecting exception on Foo");
+        }
+        catch(Exception)
+        {
+        }
+
+        I47Class i47Class = new I47Class();
+        I1 i1 = (I1) i47Class;
+        Console.WriteLine("Calling I1.Func on I47Class - expecting exception");
+        try
+        {
+            i1.Func(10);
+            Test.Assert(false, "Expecting exception on I47Class");
         }
         catch(Exception)
         {
@@ -98,9 +151,17 @@ class Program
 
     public static void Positive()
     {
-        I4Class i4 = new I4Class();
-        I1 i1 = (I1) i4;
-        Test.Assert(i1.Func(10) == 12, "Expecting I1.Func to land on I4.Func");
+        Console.WriteLine("Calling I1.Func on I4Class - expecting I4.Func");
+
+        I4Class i4Class = new I4Class();
+        I1 i1 = (I1) i4Class;
+        Test.Assert(i1.Func(10) == 14, "Expecting I1.Func to land on I4.Func");
+        
+        Console.WriteLine("Calling I1.Func on I8Class - expecting I8.Func");
+
+        I8Class i8Class = new I8Class();
+        i1 = (I1) i8Class;
+        Test.Assert(i1.Func(10) == 18, "Expecting I1.Func to land on I8.Func");
     }
 
     public static int Main()
