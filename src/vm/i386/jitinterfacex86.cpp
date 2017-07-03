49f03287f0b03cff223578e4eeb8881d08bfd557
Jonghyun Park <parjong@gmail.com>
2017-07-03 11:34:21 +0900

    Preserve EDX across UnframedGetTemplateMethodTable call

--- src/vm/i386/jitinterfacex86.cpp
+++ src/vm/i386/jitinterfacex86.cpp
@@ -923,9 +923,9 @@ HCIMPL2_RAW(Object*, UnframedAllocatePrimitiveArray, CorElementType type, DWORD 
 }
 HCIMPLEND_RAW
 
-HCIMPL1_RAW(PTR_MethodTable, UnframedGetMethodTable, ArrayTypeDesc *arrayDesc)
+HCIMPL1_RAW(PTR_MethodTable, UnframedGetTemplateMethodTable, ArrayTypeDesc *arrayDesc)
 {
-    return arrayDesc->GetMethodTable();
+    return arrayDesc->GetTemplateMethodTable();
 }
 HCIMPLEND_RAW
 
@@ -968,7 +968,10 @@ void *JIT_TrialAlloc::GenAllocArray(Flags flags)
             // je noLock
             sl.X86EmitCondJump(noLock, X86CondCode::kJZ);
 
-            sl.X86EmitCall(sl.NewExternalCodeLabel((LPVOID)UnframedGetMethodTable), 0);
+            sl.X86EmitPushReg(kEDX);
+            sl.X86EmitCall(sl.NewExternalCodeLabel((LPVOID)UnframedGetTemplateMethodTable), 0);
+            sl.X86EmitPopReg(kEDX);
+
             sl.X86EmitMovRegReg(kECX, kEAX);
         }
     }
