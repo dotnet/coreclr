75d9e2e39af34a8b68042fbb828eb68572169b4d
Nate Graf <vigraf@microsoft.com>
2017-08-11 15:06:37 -0700

    Make names unambiguous

--- src/mscorlib/src/System/Diagnostics/Eventing/EventPipe.cs
+++ src/mscorlib/src/System/Diagnostics/Eventing/EventPipe.cs
@@ -174,6 +174,6 @@ namespace System.Diagnostics.Tracing
 
         [DllImport(JitHelpers.QCall, CharSet = CharSet.Unicode)]
         [SuppressUnmanagedCodeSecurity]
-        internal static extern unsafe void WriteEvent(IntPtr eventHandle, uint eventID, EventProvider.EventData** pBlobs, uint blobCount, Guid* activityId, Guid* relatedActivityId);
+        internal static extern unsafe void WriteEventBlob(IntPtr eventHandle, uint eventID, EventProvider.EventData** pBlobs, uint blobCount, Guid* activityId, Guid* relatedActivityId);
     }
 }
