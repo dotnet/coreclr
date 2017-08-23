22788bb3b5cb52b9a4d5d22753068de0d7be8fe8
Jonghyun Park <parjong@gmail.com>
2017-08-23 16:45:10 +0900

    Enforce Equality Check

--- tests/src/CoreMangLib/system/delegate/regressions/Github_13160/Github_13160.cs
+++ tests/src/CoreMangLib/system/delegate/regressions/Github_13160/Github_13160.cs
@@ -20,25 +20,31 @@ class Program
         Action d1 = p.VirtualMethod;
         Action d2 = p.VirtualMethod;
 
-        if (d1.Equals(d2))
+        if (!d1.Equals(d2))
         {
-            if (d1.GetHashCode() != d2.GetHashCode())
-            {
-                Console.WriteLine("d1.Equals(d2) is true, but d1.GetHashCode() != d2.GetHashCode()");
-                return 200;
-            }
+            Console.WriteLine("FAILED: d1.Equals(d2) is not true");
+            return 200;
+        }
+
+        if (d1.GetHashCode() != d2.GetHashCode())
+        {
+            Console.WriteLine("FAILED: d1.GetHashCode() != d2.GetHashCode()");
+            return 201;
         }
 
         Action d3 = p.NonVirtualMethod;
         Action d4 = p.NonVirtualMethod;
 
-        if (d3.Equals(d4))
+        if (!d3.Equals(d4))
         {
-            if (d3.GetHashCode() != d4.GetHashCode())
-            {
-                Console.WriteLine("d3.Equals(d4) is true, but d3.GetHashCode() != d4.GetHashCode()");
-                return 200;
-            }
+            Console.WriteLine("FAILED: d3.Equals(d4) is not true");
+            return 202;
+        }
+
+        if (d3.GetHashCode() != d4.GetHashCode())
+        {
+            Console.WriteLine("FAILED: d3.GetHashCode() != d4.GetHashCode()");
+            return 203;
         }
 
         return 100;
