08172dc11eb155c79e4dbef5e787e4a5a77c51d2
danmosemsft <danmose@microsoft.com>
2017-08-23 16:56:11 -0700

    RWE feedback

--- src/mscorlib/shared/System/Runtime/CompilerServices/RuntimeWrappedException.cs
+++ src/mscorlib/shared/System/Runtime/CompilerServices/RuntimeWrappedException.cs
@@ -2,41 +2,37 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-/*=============================================================================
-**
-**
-**
-** Purpose: The exception class uses to wrap all non-CLS compliant exceptions.
-**
-**
-=============================================================================*/
-
 using System;
 using System.Runtime.Serialization;
-using System.Diagnostics.Contracts;
 
 namespace System.Runtime.CompilerServices
 {
+    /// <summary>
+    /// Exception used to wrap all non-CLS compliant exceptions.
+    /// </summary>
     public sealed class RuntimeWrappedException : Exception
     {
+        private Object _wrappedException;
+
+#if CORECLR
         private RuntimeWrappedException(Object thrownObject)
+#else
+        // Not an api but has to be public as System.Linq.Expression invokes this through Reflection when an expression
+        // throws an object that doesn't derive from Exception.
+        public RuntimeWrappedException(Object thrownObject)
+#endif
             : base(SR.RuntimeWrappedException)
         {
             HResult = System.HResults.COR_E_RUNTIMEWRAPPED;
-            m_wrappedException = thrownObject;
-        }
-
-        public Object WrappedException
-        {
-            get { return m_wrappedException; }
-        }
-
-        private Object m_wrappedException;
 
         public override void GetObjectData(SerializationInfo info, StreamingContext context)
         {
             base.GetObjectData(info, context);
         }
+
+        public Object WrappedException
+        {
+            get { return _wrappedException; }
+        }
     }
 }
-
