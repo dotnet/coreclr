ab4d97d3403976106f1061e1bc4b7f735e3ea340
Justin Van Patten <jvp@justinvp.com>
2017-08-26 11:58:12 -0700

    Remove unused wincon.h defines

--- src/pal/inc/pal.h
+++ src/pal/inc/pal.h
@@ -625,34 +625,6 @@ MessageBoxW(
 #define MessageBox MessageBoxA
 #endif
 
-/***************** wincon.h Entrypoints **********************************/
-
-#define CTRL_C_EVENT        0
-#define CTRL_BREAK_EVENT    1
-#define CTRL_CLOSE_EVENT    2
-// 3 is reserved!
-// 4 is reserved!
-#define CTRL_LOGOFF_EVENT   5
-#define CTRL_SHUTDOWN_EVENT 6
-
-typedef
-BOOL
-(PALAPI *PHANDLER_ROUTINE)(
-    DWORD CtrlType
-    );
-
-#ifndef CORECLR
-PALIMPORT
-BOOL
-PALAPI
-GenerateConsoleCtrlEvent(
-    IN DWORD dwCtrlEvent,
-    IN DWORD dwProcessGroupId
-    );
-#endif // !CORECLR
-
-//end wincon.h Entrypoints
-
 // From win32.h
 #ifndef _CRTIMP
 #ifdef __llvm__
