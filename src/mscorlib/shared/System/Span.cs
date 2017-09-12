4659f785a2b6e7d24c40d916c1f838adf4d36243
Jan Kotas <jkotas@microsoft.com>
2017-09-11 19:33:21 -0700

    Upgrade to C# 7.2

--- src/mscorlib/shared/System/Span.cs
+++ src/mscorlib/shared/System/Span.cs
@@ -21,10 +21,8 @@ namespace System
     /// Span represents a contiguous region of arbitrary memory. Unlike arrays, it can point to either managed
     /// or native memory, or to memory allocated on the stack. It is type- and memory-safe.
     /// </summary>
-    [IsReadOnly]
-    [IsByRefLike]
     [NonVersionable]
-    public struct Span<T>
+    public readonly ref struct Span<T>
     {
         /// <summary>A byref or a native ptr.</summary>
         private readonly ByReference<T> _pointer;
