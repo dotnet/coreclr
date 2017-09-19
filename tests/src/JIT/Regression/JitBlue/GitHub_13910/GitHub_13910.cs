4d7b61221b064ebef1091bd8e49ba4eeecbc68e6
Sergey Andreenko <seandree@microsoft.com>
2017-09-19 16:27:51 -0700

    Fix the example.

--- tests/src/JIT/Regression/JitBlue/GitHub_13910/GitHub_13910.cs
+++ tests/src/JIT/Regression/JitBlue/GitHub_13910/GitHub_13910.cs
@@ -64,16 +64,17 @@ internal class TestApp
         // tmp0 is AA.a_init[num].q.
         // It needs stress (complus_JitStressModeNames=STRESS_NULL_OBJECT_CHECK, STRESS_MAKE_CSE)
         // to force the compiler to do implicit null checks and store values as local variables.
-// Bad IL exaple, t53 is set as contained, t143 is set as contained, it means they will be calculated in t9.
+// Bad IL example, t53 is set as contained, t143 is set as contained, it means they will be calculated as part of their parent t9.
 // But at that moment V02, that is read in t143 is already modified by [000056].
 // N035 (  1,  1) [000035] ------------       t35 =    LCL_VAR   ref    V02 tmp0         u:3 eax (last use) REG eax <l:$149, c:$182>
 //                                                 /--*  t35    ref
 // N037 (???,???) [000143] -c----------      t143 = *  LEA(b+12) byref  REG NA
 //                                                  /--*  t143   byref
-// N039 (  4,  4) [000054] Rc---O------       t54 = *  IND       ref    REG NA <l:$155, c:$184> // invalud contained.
+// N039 (  4,  4) [000054] Rc---O------       t54 = *  IND       ref    REG NA <l:$155, c:$184> // This contained flag is invalid because 
+//                                                                                              // the value will be read after the store 000056.
 // *********************************************************************************************
 //                                                  /--*  t117   ref
-// N073 ( 18, 22) [000056] DA-XG-------             *  STORE_LCL_VAR ref    V02 tmp0         d:4 eax REG eax
+// N073 ( 18, 22) [000056] DA-XG-------             *  STORE_LCL_VAR ref    V02 tmp0         d:4 eax REG eax // the store that corrupts t54 value.
 // N075 (  1,  1) [000057] ------------       t57 =    LCL_VAR   ref    V02 tmp0         u:4 eax REG eax <l:$160, c:$187>
 //                                                  /--*  t57    ref
 // N077 (  2,  2) [000058] ---X---N----             *  NULLCHECK byte   REG NA <l:$166, c:$165>
@@ -83,7 +84,7 @@ internal class TestApp
 //                                                  /--*  t146   byref
 // N083 (  4,  4) [000079] R----O------       t79 = *  IND       ref    REG ecx <l:$16b, c:$189>
 //                                                  /--*  t79    ref
-// N085 (  4,  4) [000121] DA---O------             *  STORE_LCL_VAR ref    V07 cse1          ecx REG ecx // invalid store.
+// N085 (  4,  4) [000121] DA---O------             *  STORE_LCL_VAR ref    V07 cse1          ecx REG ecx
 // N087 (  1,  1) [000122] ------------      t122 =    LCL_VAR   ref    V07 cse1          ecx REG ecx <l:$16b, c:$189>
 //                                                  /--*  t54    ref // reads V02.
 //                                                  +--*  t122   ref
