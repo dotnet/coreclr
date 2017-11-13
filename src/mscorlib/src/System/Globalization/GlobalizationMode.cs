0e1cb4c09b72213b6b69b6543826bd7b3cf244a4
Tarek Mahmoud Sayed <tarekms@microsoft.com>
2017-11-13 15:50:32 -0800

    Enable Globalization Invariant mode using Environment Variable

--- src/mscorlib/src/System/Globalization/GlobalizationMode.cs
+++ src/mscorlib/src/System/Globalization/GlobalizationMode.cs
@@ -7,6 +7,27 @@ namespace System.Globalization
     internal sealed partial class GlobalizationMode
     {
         private const string c_InvariantModeConfigSwitch = "System.Globalization.Invariant";
+        // Linux doesn't support environment variable names including dots
+        private const string c_InvariantModeEnvironmentVariable = "System_Globalization_Invariant";
         internal static bool Invariant { get; } = GetGlobalizationInvariantMode();
+
+        // GetInvariantSwitchValue calls CLRConfig first to detect if the switch is defined in the config file.
+        // if the switch is defined we just use the value of this switch. otherwise, we'll try to get the switch
+        // value from the environment variable if it is defined.
+        internal static bool GetInvariantSwitchValue()
+        {
+            bool exist;
+            bool ret = CLRConfig.GetBoolValue(c_InvariantModeConfigSwitch, out exist);
+            if (!exist)
+            {
+                string switchValue = Environment.GetEnvironmentVariable(c_InvariantModeEnvironmentVariable);
+                if (switchValue != null)
+                {
+                    ret = switchValue.Equals("true", StringComparison.OrdinalIgnoreCase);
+                }
+            }
+
+            return ret;
+        }
     }
 }
