5792f93de6aa5d89f58027b63a6931569229cf0e
Victor Nate Graf <vigraf@microsoft.com>
2017-11-15 15:25:18 -0800

    Change labels to use new Ubuntu machine pool

--- perf.groovy
+++ perf.groovy
@@ -261,7 +261,7 @@ def static getFullPerfJobName(def project, def os, def isPR) {
     perfOSList.each { os ->
         def newJob = job(getFullPerfJobName(project, os, isPR)) {
 
-            label('linux_clr_perf')
+            label('ubuntu_1604_clr_perf')
             wrappers {
                 credentialsBinding {
                     string('BV_UPLOAD_SAS_TOKEN', 'CoreCLR Perf BenchView Sas')
@@ -400,7 +400,7 @@ def static getFullThroughputJobName(def project, def os, def isPR) {
     throughputOSList.each { os ->
         def newJob = job(getFullThroughputJobName(project, os, isPR)) {
 
-            label('linux_clr_perf')
+            label('ubuntu_1604_clr_perf')
                 wrappers {
                     credentialsBinding {
                         string('BV_UPLOAD_SAS_TOKEN', 'CoreCLR Perf BenchView Sas')
