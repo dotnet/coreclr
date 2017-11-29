bbfb75c991c9d25f96468a53226b3c829b6f3741
jashook <jashoo@microsoft.com>
2017-11-28 22:51:11 -0500

    Small changes

--- netci.groovy
+++ netci.groovy
@@ -2344,7 +2344,7 @@ Constants.allScenarios.each { scenario ->
                             def isUnixArm64 = (windowsArmJob != true) && (architecture == 'arm64')
 
                             // pri1 jobs still need to copy windows_nt built tests
-                            if (isUnixArm64 || (configuration != "default")) {
+                            if (isUnixArm64 || (scenario != "default")) {
                                 copyArtifacts(inputWindowTestsBuildName) {
                                     excludePatterns('**/testResults.xml', '**/*.ni.dll')
                                     buildSelector {
@@ -2378,7 +2378,7 @@ Constants.allScenarios.each { scenario ->
                                     shell("chmod +x ./bin/Product/Linux.arm64.${configuration}/corerun")
                                 }
 
-                                if (isUnixArm64 || (configuration != "default")) {
+                                if (isUnixArm64 || (scenario != "default")) {
                                     // Unzip the tests first.  Exit with 0
                                     shell("tar xzvf ./bin/tests/${osGroup}.${architecture}.${configuration}.tar.gz || exit 0")
                                 } else {
@@ -2580,7 +2580,7 @@ Constants.allScenarios.each { scenario ->
                     def isUnixArm64 = ((osGroup == "Linux") && (architecture == "arm64"))
 
                     // For pri0 jobs we can build tests on unix
-                    if ((configuration == "default") && (isUnixArm64 == false)) {
+                    if ((scenario == "default") && (isUnixArm64 == false)) {
                         // For Windows arm jobs there is no reason to build a parallel test job.
                         // The product build supports building and archiving the tests.
 
