aee691dfb74a6b25835102dc0a3ad0cbfb900c8a
vitek-karas <vitek.karas@microsoft.com>
2017-12-01 15:00:20 -0800

    Add better coverage for Marshal.GetDelegateForFunctionPointer
    Also fixes the test Main to correctly collect result codes from all 3 subtests.

--- tests/src/Interop/MarshalAPI/FunctionPointer/GetDelForFcnPtr_Negative_Catchable.cs
+++ tests/src/Interop/MarshalAPI/FunctionPointer/GetDelForFcnPtr_Negative_Catchable.cs
@@ -54,7 +54,7 @@ public partial class FunctionPtr
         }
         catch (ArgumentException e)
         {
-             Console.WriteLine("Fail - passing a null type received the right exception type, wrong message, message was '{0}'", e.Message);
+             Console.WriteLine("Pass - passing a null type received the right exception type, wrong message, message was '{0}'", e.Message);
         }
         catch (Exception e)
         {
@@ -71,7 +71,7 @@ public partial class FunctionPtr
         }
         catch (ArgumentException e)
         {
-            Console.WriteLine("Faile - threw the right exception passing a non-delegate type, but a wrong message, message was '{0}'", e.Message);
+            Console.WriteLine("Pass - threw the right exception passing a non-delegate type, but a wrong message, message was '{0}'", e.Message);
         }
         catch (Exception e)
         {
@@ -79,6 +79,70 @@ public partial class FunctionPtr
             Console.WriteLine("Failure - receive an incorrect exception while passing a non-delegate type");
             Console.WriteLine(e);
         }
+
+        // Delegate -> FcnPtr -> Delegate
+        try
+        {
+            VoidDelegate del = (VoidDelegate)Marshal.GetDelegateForFunctionPointer(fcnptr, typeof(VoidDelegate));
+            if (del.Target != md.Target)
+            {
+                retVal = 0;
+                Console.WriteLine("Failure - the Target of the funcptr->delegate should be null since the original method is static.");
+                Console.WriteLine(del.Target);
+            }
+
+            if (del.Method != md.Method)
+            {
+                retVal = 0;
+                Console.WriteLine("Failure - The Method of the funcptr->delegate should be equal to the MethodInfo of the original method.");
+                Console.WriteLine(del.Method);
+            }
+
+            // Try to call it
+            del();
+
+            Console.WriteLine("Pass - got a delegate for the function pointer.");
+        }
+        catch (Exception e)
+        {
+            retVal = 0;
+            Console.WriteLine("Failure - received exception while converting funcptr to delegate.");
+            Console.WriteLine(e);
+        }
+
+        // Native FcnPtr -> Delegate
+        try
+        {
+            // Intentionally using the fcnptr but moved by 1 byte. We need a native pointer which is readable (since CLR assumes this is a function pointer which can be called
+            // and reads the first few instructions to check if it's something it knows about).
+            // But we need something which is NOT a reverse-pinvoke thunk so that CLR won't be able to match/find it.
+            // Note that it really doesn't matter where the address comes from as long as it's readable and not one of the thunks.
+            VoidDelegate del = (VoidDelegate)Marshal.GetDelegateForFunctionPointer(fcnptr + 1, typeof(VoidDelegate));
+            if (del.Target != null)
+            {
+                retVal = 0;
+                Console.WriteLine("Failure - the Target of the funcptr->delegate should be null since we provided native funcptr.");
+                Console.WriteLine(del.Target);
+            }
+
+            if (del.Method.Name != "Invoke")
+            {
+                retVal = 0;
+                Console.WriteLine("Failure - The Method of the native funcptr->delegate should be the Invoke method.");
+                Console.WriteLine(del.Method);
+            }
+
+            // Don't try to call it - it's a random address.
+
+            Console.WriteLine("Pass - got a delegate for the function pointer.");
+        }
+        catch (Exception e)
+        {
+            retVal = 0;
+            Console.WriteLine("Failure - received exception while converting funcptr to delegate.");
+            Console.WriteLine(e);
+        }
+
 
         Console.WriteLine(retVal == 100 ? "Done - PASSED" : "Done - FAILED");
         return retVal;
