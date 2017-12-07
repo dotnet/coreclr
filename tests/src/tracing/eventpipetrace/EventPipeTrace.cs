cc215150bf91543b27df0ab9644ab640f331d006
Victor Nate Graf <vigraf@microsoft.com>
2017-12-06 16:16:49 -0800

    Incomplete improvements to EventPipeTrace test

--- tests/src/tracing/eventpipetrace/EventPipeTrace.cs
+++ tests/src/tracing/eventpipetrace/EventPipeTrace.cs
@@ -2,17 +2,22 @@ using System;
 using System.IO;
 using Tracing.Tests.Common;
 using Microsoft.Diagnostics.Tracing;
+using Microsoft.Diagnostics.Tracing.Parsers;
+using Microsoft.Diagnostics.Tracing.Parsers.Clr;
 
 namespace Tracing.Tests
 {
-    class EventPipeSmoke
+    class EventPipeTrace
     {
         private static int allocIterations = 10000;
+        private static int gcIterations = 10;
         private static int trivialSize = 0x100000;
 
         static int Main(string[] args)
         {
+            bool pass = false;
             bool keepOutput = false;
+
             // Use the first arg as an output filename if there is one
             string outputFilename = null;
             if (args.Length >= 1) {
@@ -23,56 +28,65 @@ namespace Tracing.Tests
                 outputFilename = System.IO.Path.GetTempPath() + Guid.NewGuid().ToString() + ".netperf";
             }
 
-            Console.WriteLine("\tStart: Enable tracing.");
-            TraceControl.EnableDefault(outputFilename);
-            Console.WriteLine("\tEnd: Enable tracing.\n");
-
-            Console.WriteLine("\tStart: Allocation.");
-            // Allocate for allocIterations iterations.
-            for(int i=0; i<allocIterations; i++)
+            try
             {
-                GC.KeepAlive(new object());
-            }
-            Console.WriteLine("\tEnd: Allocation.\n");
+                Console.WriteLine("\tStart: Enable tracing.");
+                TraceControl.EnableDefault(outputFilename);
+                Console.WriteLine("\tEnd: Enable tracing.\n");
 
-            Console.WriteLine("\tStart: Disable tracing.");
-            TraceControl.Disable();
-            Console.WriteLine("\tEnd: Disable tracing.\n");
-
-            FileInfo outputMeta = new FileInfo(outputFilename);
-            Console.WriteLine("\tCreated {0} bytes of data", outputMeta.Length);
-
-            Console.WriteLine("\tStart: Processing events from file.");
-            bool pass = false;
-            int eventCount = 0;
-            using (var trace = TraceEventDispatcher.GetDispatcherFromFileName(outputFilename))
-            {
-                trace.Dynamic.All += delegate(TraceEvent data)
+                Console.WriteLine("\tStart: Generating CLR events");
+                // Allocate for allocIterations iterations.
+                for(int i=0; i<allocIterations; i++)
                 {
-                    eventCount += 1;
-                    //Console.WriteLine("GOT EVENT: " + data.ToString());
-                };
-                // Handle everything for dev
-                trace.UnhandledEvents += delegate(TraceEvent data)
+                    GC.KeepAlive(new object());
+                }
+                // GC gcIternation times
+                for(int i=0; i<gcIterations; i++)
                 {
-                    eventCount += 1;
-                    //if ((int)data.ID != 0xFFFE)
-                    //    Console.WriteLine("GOT UNHANDLED EVENT: " + data.Dump());
-                };
+                    GC.Collect();
+                }
+                Console.WriteLine("\tEnd: Generating CLR Events\n");
 
-                trace.Process();
-            }
-            Console.WriteLine("\tEnd: Processing events from file.\n");
-            
-            Console.WriteLine("\tProcessed {0} events", eventCount);
+                Console.WriteLine("\tStart: Disable tracing.");
+                TraceControl.Disable();
+                Console.WriteLine("\tEnd: Disable tracing.\n");
 
-            if (keepOutput)
-            {
-                Console.WriteLine("\tOutput file: {0}", outputFilename);
+                FileInfo outputMeta = new FileInfo(outputFilename);
+                Console.WriteLine("\tCreated {0} bytes of data", outputMeta.Length);
+
+                Console.WriteLine("\tStart: Processing events from file.");
+                int allocTickCount = 0;
+                int gcTriggerCount = 0;
+                using (var trace = TraceEventDispatcher.GetDispatcherFromFileName(outputFilename))
+                {
+                    trace.Clr.GCAllocationTick += delegate(GCAllocationTickTraceData data)
+                    {
+                        allocTickCount += 1;
+                    };
+                    trace.Clr.GCTriggered += delegate(GCTriggeredTraceData data)
+                    {
+                        gcTriggerCount += 1;
+                    };
+
+                    trace.Process();
+                }
+                Console.WriteLine("\tEnd: Processing events from file.\n");
+
+                Console.WriteLine("\tProcessed {0} GCAllocationTick events", allocTickCount);
+                Console.WriteLine("\tProcessed {0} GCTriggered events", gcTriggerCount);
+
+                pass = allocTickCount > 0;
+                pass &= gcTriggerCount == gcIterations;
             }
-            else
-            {
-                System.IO.File.Delete(outputFilename);
+            finally {
+                if (keepOutput)
+                {
+                    Console.WriteLine("\tOutput file: {0}", outputFilename);
+                }
+                else
+                {
+                    System.IO.File.Delete(outputFilename);
+                }
             }
 
             return pass ? 100 : 0;
