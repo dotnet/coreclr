61433bcfcbbefac1224f5bd9c2e272d58a17feed
Andy Ayers <andya@microsoft.com>
2017-12-12 18:09:17 -0800

    Value number identities for compares and subs
    Improve value number support for various subtracts and unsigned compares.
    For instance for non-float values, x - x == 0.

--- src/jit/valuenum.cpp
+++ src/jit/valuenum.cpp
@@ -1154,10 +1154,18 @@ ValueNum ValueNumStore::VNForFunc(var_types typ, VNFunc func, ValueNum arg0VN, V
 
                     case GT_SUB:
                         // (x - 0) => x
-                        ZeroVN = VNZeroForType(typ);
-                        if (arg1VN == ZeroVN)
+                        // (x - x) => 0
+                        if (!varTypeIsFloating(typ))
                         {
-                            resultVN = arg0VN;
+                            ZeroVN = VNZeroForType(typ);
+                            if (arg1VN == ZeroVN)
+                            {
+                                resultVN = arg0VN;
+                            }
+                            else if ((arg0VN != NoVN) && (arg1VN == arg0VN))
+                            {
+                                resultVN = ZeroVN;
+                            }
                         }
                         break;
 
@@ -1249,7 +1257,11 @@ ValueNum ValueNumStore::VNForFunc(var_types typ, VNFunc func, ValueNum arg0VN, V
                         break;
 
                     case GT_EQ:
+                    case GT_GE:
+                    case GT_LE:
                         // (x == x) => true (unless x is NaN)
+                        // (x <= x) => true (unless x is NaN)
+                        // (x >= x) => true (unless x is NaN)
                         if (!varTypeIsFloating(TypeOfVN(arg0VN)) && (arg0VN != NoVN) && (arg0VN == arg1VN))
                         {
                             resultVN = VNOneForType(typ);
@@ -1260,8 +1272,13 @@ ValueNum ValueNumStore::VNForFunc(var_types typ, VNFunc func, ValueNum arg0VN, V
                             resultVN = VNZeroForType(typ);
                         }
                         break;
+
                     case GT_NE:
+                    case GT_GT:
+                    case GT_LT:
                         // (x != x) => false (unless x is NaN)
+                        // (x > x) => false (unless x is NaN)
+                        // (x < x) => false (unless x is NaN)
                         if (!varTypeIsFloating(TypeOfVN(arg0VN)) && (arg0VN != NoVN) && (arg0VN == arg1VN))
                         {
                             resultVN = VNZeroForType(typ);
@@ -1285,6 +1302,24 @@ ValueNum ValueNumStore::VNForFunc(var_types typ, VNFunc func, ValueNum arg0VN, V
         }
         else // must be a VNF_ function
         {
+            if ((arg0VN != NoVN) && (arg0VN == arg1VN))
+            {
+                assert(!varTypeIsFloating(TypeOfVN(arg0VN)));
+                // x <= x ==> true
+                // x >= x ==> true
+                if ((func == VNF_LE_UN) || (func == VNF_GE_UN))
+                {
+                    return VNOneForType(typ);
+                }
+                // x < x ==> false
+                // x > x ==> false
+                // x - x ==> 0
+                else if ((func == VNF_LT_UN) || (func == VNF_GT_UN) || (func == VNF_SUB_UN))
+                {
+                    return VNZeroForType(typ);
+                }
+            }
+
             if (func == VNF_CastClass)
             {
                 // In terms of values, a castclass always returns its second argument, the object being cast.
