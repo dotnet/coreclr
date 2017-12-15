05cb7002ee3df6e457569d487a7522bde46d7505
Anirudh Agnihotry <anirudhagnihotry098@gmail.com>
2017-12-14 16:56:55 -0800

    Removed Extra Comment

--- src/vm/comsynchronizable.cpp
+++ src/vm/comsynchronizable.cpp
@@ -1055,7 +1055,6 @@ FCIMPL1(INT32, ThreadNative::GetApartmentState, ThreadBaseObject* pThisUNSAFE)
         // ApartmentState.Unknown to our callers.
         if (!g_fComStarted)
         {
-            //EnsureComStarted();
             state = thread->GetApartment();
         }
     }
