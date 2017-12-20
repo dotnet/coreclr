aec344348d1c0fbbb8398d0621bc8465863de931
Victor "Nate" Graf <vigraf@microsoft.com>
2017-12-20 11:48:09 -0800

    remove stdlib.h

--- src/scripts/genDummyProvider.py
+++ src/scripts/genDummyProvider.py
@@ -163,7 +163,6 @@ install(TARGETS eventprovider DESTINATION lib)
             impl.write(stdprolog_cpp + "\n")
 
             impl.write("""
-#include "stdlib.h"
 #ifdef PLATFORM_UNIX
 #include "pal_mstypes.h"
 #include "pal_error.h"
