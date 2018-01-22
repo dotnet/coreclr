019b9f04b3d100cee3eba66457d782d3d0ac2af6
Sean Gillespie <segilles@microsoft.com>
2018-01-22 15:09:22 -0800

    Code review feedback: remove `descriptor` system for known events and instead use the gcevents xmacro to generate calls to `GCEventStatus::IsEnabled` with known constants

--- src/gc/gceventstatus.h
+++ src/gc/gceventstatus.h
@@ -187,50 +187,17 @@ private:
     GCEventStatus() = delete;
 };
 
-class GCEventBase
-{
-private:
-    const char *m_name;
-    GCEventProvider m_provider;
-    GCEventLevel m_level;
-    GCEventKeyword m_keyword;
-
-public:
-    GCEventBase(const char *name, GCEventProvider provider, GCEventLevel level, GCEventKeyword keyword)
-        : m_name(name), m_provider(provider), m_level(level), m_keyword(keyword)
-    {}
-
-    GCEventProvider Provider() const { return m_provider; }
-
-    GCEventLevel Level() const { return m_level; }
-
-    GCEventKeyword Keyword() const { return m_keyword; }
-
-    __forceinline bool IsEnabled() const
-    {
-        return GCEventStatus::IsEnabled(m_provider, m_keyword, m_level);
-    }
-};
-
-class GCKnownEvent : public GCEventBase
-{
-public:
-    GCKnownEvent(const char *name, GCEventProvider provider, GCEventLevel level, GCEventKeyword keyword)
-        : GCEventBase(name, provider, level, keyword)
-    {}
-};
-
-class GCDynamicEvent : public GCEventBase
+class GCDynamicEvent
 {
     /* TODO(segilles) - Not Yet Implemented */
 };
 
 #if FEATURE_EVENT_TRACE
 #define KNOWN_EVENT(name, _provider, _level, _keyword)   \
-  extern gc_events::GCKnownEvent name##EventDescriptor;
+  inline bool GCEventEnabled##name() { return GCEventStatus::IsEnabled(_provider, _level, _keyword); }
 #include "gcevents.h"
 
-#define EVENT_ENABLED(name) name##EventDescriptor.IsEnabled()
+#define EVENT_ENABLED(name) GCEventEnabled##name()
 #define FIRE_EVENT(name, ...) \
   do {                                                      \
     IGCToCLREventSink* sink = GCToEEInterface::EventSink(); \
