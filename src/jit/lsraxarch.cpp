de31b08132d468aa40043ec140f474738aee4720
Sergey Andreenko <seandree@microsoft.com>
2018-01-22 18:32:42 -0800

    delete fgArgTabEntryPtr

--- src/jit/lsraxarch.cpp
+++ src/jit/lsraxarch.cpp
@@ -1305,7 +1305,7 @@ void LinearScan::TreeNodeInfoInitCall(GenTreeCall* call, TreeNodeInfo* info)
 #ifdef DEBUG
         // In DEBUG only, check validity with respect to the arg table entry.
 
-        fgArgTabEntryPtr curArgTabEntry = compiler->gtArgEntryByNode(call, argNode);
+        fgArgTabEntry* curArgTabEntry = compiler->gtArgEntryByNode(call, argNode);
         assert(curArgTabEntry);
 
         if (curArgTabEntry->regNum == REG_STK)
