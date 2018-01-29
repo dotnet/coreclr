3ac42bd041606ae1f8b89b10c18958b8a2cb8229
Brian Robbins <brianrob@microsoft.com>
2018-01-29 14:28:56 -0800

    Fix assert methods to handle null values.

--- tests/src/tracing/common/Assert.cs
+++ tests/src/tracing/common/Assert.cs
@@ -6,7 +6,17 @@ namespace Tracing.Tests.Common
     {
         public static void Equal<T>(T left, T right) where T : IEquatable<T>
         {
-            if (!left.Equals(right))
+            if (left == null && right != null)
+            {
+                throw new Exception(
+                    string.Format("Values are not equal!  Left=NULL Right='{0}'", right));
+            }
+            else if (left != null && right == null)
+            {
+                throw new Exception(
+                    string.Format("Values are not equal!  Left='{0}' Right=NULL", left));
+            }
+            else (!left.Equals(right))
             {
                 throw new Exception(
                     string.Format("Values are not equal! Left='{0}' Right='{1}'", left, right));
@@ -15,7 +25,12 @@ namespace Tracing.Tests.Common
 
         public static void NotEqual<T>(T left, T right) where T : IEquatable<T>
         {
-            if (left.Equals(right))
+            if (left == right && left == NULL)
+            {
+                throw new Exception(
+                    "Values are equal! Left=NULL Right=NULL");
+            }
+            else if (left != null && left.Equals(right))
             {
                 throw new Exception(
                     string.Format("Values are equal! Left='{0}' Right='{1}'", left, right));
