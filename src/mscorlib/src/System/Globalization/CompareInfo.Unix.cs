a935900a71d1ee083cfc367e882355df16b35038
ahsonkhan <ahson_ahmedk@yahoo.com>
2018-02-08 12:08:22 -0800

    Updating checks for Unix to mimic what IsFastSort does.

--- src/mscorlib/src/System/Globalization/CompareInfo.Unix.cs
+++ src/mscorlib/src/System/Globalization/CompareInfo.Unix.cs
@@ -27,14 +27,14 @@ namespace System.Globalization
             }
             else
             {
-                Interop.Globalization.ResultCode resultCode = Interop.Globalization.GetSortHandle(GetNullTerminatedUtf8String(_sortName), out _sortHandle); 
+                Interop.Globalization.ResultCode resultCode = Interop.Globalization.GetSortHandle(GetNullTerminatedUtf8String(_sortName), out _sortHandle);
                 if (resultCode != Interop.Globalization.ResultCode.Success)
                 {
                     _sortHandle.Dispose();
-                    
+
                     if (resultCode == Interop.Globalization.ResultCode.OutOfMemory)
                         throw new OutOfMemoryException();
-                    
+
                     throw new ExternalException(SR.Arg_ExternalException);
                 }
                 _isAsciiEqualityOrdinal = (_sortName == "en-US" || _sortName == "");
@@ -44,7 +44,7 @@ namespace System.Globalization
         internal static unsafe int IndexOfOrdinalCore(string source, string value, int startIndex, int count, bool ignoreCase)
         {
             Debug.Assert(!GlobalizationMode.Invariant);
-            
+
             Debug.Assert(source != null);
             Debug.Assert(value != null);
 
@@ -90,7 +90,7 @@ namespace System.Globalization
         internal static unsafe int LastIndexOfOrdinalCore(string source, string value, int startIndex, int count, bool ignoreCase)
         {
             Debug.Assert(!GlobalizationMode.Invariant);
-            
+
             Debug.Assert(source != null);
             Debug.Assert(value != null);
 
@@ -174,7 +174,7 @@ namespace System.Globalization
         internal unsafe int IndexOfCore(string source, string target, int startIndex, int count, CompareOptions options, int* matchLengthPtr)
         {
             Debug.Assert(!_invariantMode);
-            
+
             Debug.Assert(!string.IsNullOrEmpty(source));
             Debug.Assert(target != null);
             Debug.Assert((options & CompareOptions.OrdinalIgnoreCase) == 0);
@@ -183,7 +183,7 @@ namespace System.Globalization
 
             if (target.Length == 0)
             {
-                if(matchLengthPtr != null)
+                if (matchLengthPtr != null)
                     *matchLengthPtr = 0;
                 return startIndex;
             }
@@ -191,9 +191,9 @@ namespace System.Globalization
             if (options == CompareOptions.Ordinal)
             {
                 index = IndexOfOrdinal(source, target, startIndex, count, ignoreCase: false);
-                if(index != -1)
+                if (index != -1)
                 {
-                    if(matchLengthPtr != null)
+                    if (matchLengthPtr != null)
                         *matchLengthPtr = target.Length;
                 }
                 return index;
@@ -202,14 +202,14 @@ namespace System.Globalization
             if (_isAsciiEqualityOrdinal && CanUseAsciiOrdinalForOptions(options) && source.IsFastSort() && target.IsFastSort())
             {
                 index = IndexOf(source, target, startIndex, count, GetOrdinalCompareOptions(options));
-                if(index != -1)
+                if (index != -1)
                 {
-                    if(matchLengthPtr != null)
+                    if (matchLengthPtr != null)
                         *matchLengthPtr = target.Length;
                 }
                 return index;
             }
-            
+
             fixed (char* pSource = source)
             {
                 index = Interop.Globalization.IndexOf(_sortHandle, target, target.Length, pSource + startIndex, count, options, matchLengthPtr);
@@ -343,6 +343,20 @@ namespace System.Globalization
             }
         }
 
+        private unsafe bool IsFastSort(char* alreadyFixedPointer, int length)
+        {
+            char* ptr = alreadyFixedPointer;
+            for (int i = 0; i < length; i++)
+            {
+                char c = *ptr;
+                if (c >= 0x80 || HighCharTable[c])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
         private unsafe bool EndsWithOrdinalIgnoreCaseHelper(ReadOnlySpan<char> source, ReadOnlySpan<char> suffix, CompareOptions options)
         {
             return StartsWithOrdinalIgnoreCaseHelper(source.Slice(source.Length - suffix.Length), suffix, options);
@@ -370,7 +384,9 @@ namespace System.Globalization
                 char* a = ap;
                 char* b = bp;
 
-                while (length != 0 && (*a <= 0x80) && (*b <= 0x80))
+                if (!IsFastSort(ap) || !IsFastSort(bp)) goto NonOrdinal;
+
+                while (length != 0 && (*a < 0x80) && (*b < 0x80) && (!HighCharTable[*a]) && (!HighCharTable[*b]))
                 {
                     int charA = *a;
                     int charB = *b;
@@ -396,6 +412,8 @@ namespace System.Globalization
                 }
 
                 if (length == 0) return true;
+
+            NonOrdinal:
                 return Interop.Globalization.StartsWith(_sortHandle, b, prefix.Length - length, a, source.Length - length, options);
             }
         }
@@ -414,31 +432,61 @@ namespace System.Globalization
             fixed (char* ap = &MemoryMarshal.GetReference(source))
             fixed (char* bp = &MemoryMarshal.GetReference(prefix))
             {
+                if (!IsFastSort(ap) || !IsFastSort(bp)) goto NonOrdinal;
+
                 char* a = ap;
                 char* b = bp;
 
-                while (length != 0 && (*a <= 0x80) && (*b <= 0x80))
+#if BIT64
+                // Single int read aligns pointers for the following long reads
+                if (length >= 2)
                 {
-                    int charA = *a;
-                    int charB = *b;
-
-                    if (charA == charB)
-                    {
-                        a++; b++;
-                        length--;
-                        continue;
-                    }
-
-                    //Return the (case-insensitive) difference between them.
-                    if (charA != charB)
+                    if (*(int*)a != *(int*)b)
                         return false;
-
-                    // Next char
-                    a++; b++;
-                    length--;
+                    length -= 2;
+                    a += 2;
+                    b += 2;
                 }
 
-                if (length == 0) return true;
+                while (length >= 12)
+                {
+                    if (*(long*)a != *(long*)b)
+                        return false;
+                    if (*(long*)(a + 4) != *(long*)(b + 4))
+                        return false;
+                    if (*(long*)(a + 8) != *(long*)(b + 8))
+                        return false;
+                    length -= 12;
+                    a += 12;
+                    b += 12;
+                }
+#else
+                while (length >= 10)
+                {
+                    if (*(int*)a != *(int*)b) return false;
+                    if (*(int*)(a + 2) != *(int*)(b + 2)) return false;
+                    if (*(int*)(a + 4) != *(int*)(b + 4)) return false;
+                    if (*(int*)(a + 6) != *(int*)(b + 6)) return false;
+                    if (*(int*)(a + 8) != *(int*)(b + 8)) return false;
+                    length -= 10; a += 10; b += 10;
+                }
+#endif
+
+                while (length >= 2)
+                {
+                    if (*(int*)a != *(int*)b)
+                        return false;
+                    length -= 2;
+                    a += 2;
+                    b += 2;
+                }
+
+                // PERF: This depends on the fact that the String objects are always zero terminated 
+                // and that the terminating zero is not included in the length. For even string sizes
+                // this compare can include the zero terminator. Bitwise OR avoids a branch.
+                if (length == 0) return *a == *b;
+
+            NonOrdinal:
                 return Interop.Globalization.StartsWith(_sortHandle, b, prefix.Length - length, a, source.Length - length, options);
             }
         }
@@ -593,5 +641,138 @@ namespace System.Globalization
                                                              (byte) ((LCID  & 0x0000FF00) >> 8),
                                                              (byte) (LCID  & 0xFF)));
         }
+
+        // See https://github.com/dotnet/coreclr/blob/master/src/utilcode/util_nodependencies.cpp#L970
+        private static readonly bool[] HighCharTable = new bool[0x80]
+        {
+            true, /* 0x0, 0x0 */
+            true, /* 0x1, .*/
+            true, /* 0x2, .*/
+            true, /* 0x3, .*/
+            true, /* 0x4, .*/
+            true, /* 0x5, .*/
+            true, /* 0x6, .*/
+            true, /* 0x7, .*/
+            true, /* 0x8, .*/
+            false, /* 0x9,   */
+            true, /* 0xA,  */
+            false, /* 0xB, .*/
+            false, /* 0xC, .*/
+            true, /* 0xD,  */
+            true, /* 0xE, .*/
+            true, /* 0xF, .*/
+            true, /* 0x10, .*/
+            true, /* 0x11, .*/
+            true, /* 0x12, .*/
+            true, /* 0x13, .*/
+            true, /* 0x14, .*/
+            true, /* 0x15, .*/
+            true, /* 0x16, .*/
+            true, /* 0x17, .*/
+            true, /* 0x18, .*/
+            true, /* 0x19, .*/
+            true, /* 0x1A, */
+            true, /* 0x1B, .*/
+            true, /* 0x1C, .*/
+            true, /* 0x1D, .*/
+            true, /* 0x1E, .*/
+            true, /* 0x1F, .*/
+            false, /*0x20,  */
+            false, /*0x21, !*/
+            false, /*0x22, "*/
+            false, /*0x23,  #*/
+            false, /*0x24,  $*/
+            false, /*0x25,  %*/
+            false, /*0x26,  &*/
+            true,  /*0x27, '*/
+            false, /*0x28, (*/
+            false, /*0x29, )*/
+            false, /*0x2A **/
+            false, /*0x2B, +*/
+            false, /*0x2C, ,*/
+            true,  /*0x2D, -*/
+            false, /*0x2E, .*/
+            false, /*0x2F, /*/
+            false, /*0x30, 0*/
+            false, /*0x31, 1*/
+            false, /*0x32, 2*/
+            false, /*0x33, 3*/
+            false, /*0x34, 4*/
+            false, /*0x35, 5*/
+            false, /*0x36, 6*/
+            false, /*0x37, 7*/
+            false, /*0x38, 8*/
+            false, /*0x39, 9*/
+            false, /*0x3A, :*/
+            false, /*0x3B, ;*/
+            false, /*0x3C, <*/
+            false, /*0x3D, =*/
+            false, /*0x3E, >*/
+            false, /*0x3F, ?*/
+            false, /*0x40, @*/
+            false, /*0x41, A*/
+            false, /*0x42, B*/
+            false, /*0x43, C*/
+            false, /*0x44, D*/
+            false, /*0x45, E*/
+            false, /*0x46, F*/
+            false, /*0x47, G*/
+            false, /*0x48, H*/
+            false, /*0x49, I*/
+            false, /*0x4A, J*/
+            false, /*0x4B, K*/
+            false, /*0x4C, L*/
+            false, /*0x4D, M*/
+            false, /*0x4E, N*/
+            false, /*0x4F, O*/
+            false, /*0x50, P*/
+            false, /*0x51, Q*/
+            false, /*0x52, R*/
+            false, /*0x53, S*/
+            false, /*0x54, T*/
+            false, /*0x55, U*/
+            false, /*0x56, V*/
+            false, /*0x57, W*/
+            false, /*0x58, X*/
+            false, /*0x59, Y*/
+            false, /*0x5A, Z*/
+            false, /*0x5B, [*/
+            false, /*0x5C, \*/
+            false, /*0x5D, ]*/
+            false, /*0x5E, ^*/
+            false, /*0x5F, _*/
+            false, /*0x60, `*/
+            false, /*0x61, a*/
+            false, /*0x62, b*/
+            false, /*0x63, c*/
+            false, /*0x64, d*/
+            false, /*0x65, e*/
+            false, /*0x66, f*/
+            false, /*0x67, g*/
+            false, /*0x68, h*/
+            false, /*0x69, i*/
+            false, /*0x6A, j*/
+            false, /*0x6B, k*/
+            false, /*0x6C, l*/
+            false, /*0x6D, m*/
+            false, /*0x6E, n*/
+            false, /*0x6F, o*/
+            false, /*0x70, p*/
+            false, /*0x71, q*/
+            false, /*0x72, r*/
+            false, /*0x73, s*/
+            false, /*0x74, t*/
+            false, /*0x75, u*/
+            false, /*0x76, v*/
+            false, /*0x77, w*/
+            false, /*0x78, x*/
+            false, /*0x79, y*/
+            false, /*0x7A, z*/
+            false, /*0x7B, {*/
+            false, /*0x7C, |*/
+            false, /*0x7D, }*/
+            false, /*0x7E, ~*/
+            true, /*0x7F, */
+        };
     }
 }
