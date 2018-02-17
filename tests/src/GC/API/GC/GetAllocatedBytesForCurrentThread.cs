1012493c132a1bdf99bbe5e54c965359985e18a8
Sung Yoon Whang <suwhang@microsoft.com>
2018-02-16 18:25:51 -0800

    modified test to fit coreclr test runner

--- tests/src/GC/API/GC/GetAllocatedBytesForCurrentThread.cs
+++ tests/src/GC/API/GC/GetAllocatedBytesForCurrentThread.cs
@@ -6,12 +6,13 @@
 using System;
 using System.Diagnostics;
 using System.Collections.Generic;
+using System.Reflection;
 
 public class Test 
 {
     static Random Rand = new Random();
 
-    public static void GetAllocatedBytesForCurrentThread(int size)
+    public static bool GetAllocatedBytesForCurrentThread(int size)
     {
         int startCount = GC.CollectionCount(0);
         long start = GC.GetAllocatedBytesForCurrentThread();
@@ -23,10 +24,10 @@ public class Test
 
         if (start == end)
         {
-            Console.WriteLine("start: {0} same as end??! attach debugger, GC {1}->{2}",
-                start, startCount, endCount);
-            Console.ReadLine();
+            Console.WriteLine("GetAllocatedBytesForCurrentThread: start and end same!");
+            return false;
         }
+        return true;
     }
 
     static int Alloc(List<object> list, int size)
@@ -45,7 +46,7 @@ public class Test
         return allocated;
     }
 
-    static void TestWithAlloc()
+    static bool TestWithAlloc()
     {
         int allocatedBytes = 0;
         for (int i = 0; i < 100; i++)
@@ -53,14 +54,18 @@ public class Test
             List<object> list = new List<object>();
             allocatedBytes = Alloc(list, 80*1024*1024);
 
-            GetAllocatedBytesForCurrentThread (100000);
+            if (!GetAllocatedBytesForCurrentThread (100000)) 
+            {
+                return false;
+            }
             Console.WriteLine("iter {0} allocated {1} bytes", i, allocatedBytes);
         }
+        return true;
     }
 
     // In core 1.0 we didn't have the API exposed so needed to use reflection to get it.
     // This should be split into 2 tests, with and without GC.Collect.
-    static void TestCore1()
+    static bool TestCore1()
     {
         const string name = "GetAllocatedBytesForCurrentThread";
         var typeInfo = typeof(GC).GetTypeInfo();
@@ -82,17 +87,28 @@ public class Test
             {
                 int countAfter = GC.CollectionCount(0);
                 Console.WriteLine("b: {0}, a: {1}, iter {2}, {3}->{4}", nBytesBefore, nBytesAfter, i, countBefore, countAfter);
-                Debug.Assert(false);
+                return false;
             }
         }
+        return true;
     }
 
     public static int Main() 
     {
-        TestCore1();
+        if (!TestCore1())
+        {
+            Console.WriteLine("Test for GetAllocatedBytesForCurrentThread() failed!");
+            return 1;
+        }
 
-        TestWithAlloc();
+        if (!TestWithAlloc())
+        {
+            Console.WriteLine("Test for GetAllocatedBytesForCurrentThread() failed!");
+            return 1;
+        }
 
-        return 0;
+
+        Console.WriteLine("Test for GetAllocatedBytesForCurrentThread() passed!");
+        return 100;
     }
 }
