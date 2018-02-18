0a0a63287e60aab7ccbd449a6c27b60732ee6467
noahfalk <noahfalk@microsoft.com>
2018-02-17 18:52:29 -0800

    Add more JitBench Benchmarks
    a) JitBench includes several more benchmarks and a refactored runner to make it easier to add more
    b) A new JitBench/unofficial_dotnet SDK style build project offers simpler developer workflow at the command line and in VS when working with these Benchmarks
    c) Misc build issues with local dotnet build in the test tree have workarounds now, but official CI builds still don't support it
    d) run-xunit-perf.py has support to run a specific assembly from the testBinLoc folder intended to handle selecting a binary from the folder of binaries produced in the .Net Core SDK project build process. However until CI build can support building projects that way the xunit-perf support is unused.

---
+++ tests/src/performance/Scenario/JitBench/Runner/Statistics.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JitBench
+{
+    public static class Statistics
+    {
+        public static double SampleStandardDeviation(this IEnumerable<double> data)
+        {
+            int n = data.Count();
+            double sampleMean = data.Average();
+            return Math.Sqrt(data.Select(x => (x - sampleMean) * (x - sampleMean)).Sum() / (n - 1));
+        }
+
+        public static double StandardError(this IEnumerable<double> data)
+        {
+            int n = data.Count();
+            return SampleStandardDeviation(data) / Math.Sqrt(n);
+        }
+
+        public static double MarginOfError95(this IEnumerable<double> data)
+        {
+            return StandardError(data) * 1.96;
+        }
+    }
+}
