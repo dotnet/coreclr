a3dcc28b6c44c76504e98513f20ceedfc51a4564
Anipik <anirudhagnihotry098@gmail.com>
2018-02-26 21:47:38 -0800

    using span

--- src/mscorlib/shared/System/IO/Path.Windows.cs
+++ src/mscorlib/shared/System/IO/Path.Windows.cs
@@ -98,8 +98,8 @@ namespace System.IO
                 {
                     // No matching root, root to specified drive
                     // "D:Foo" and "C:\Bar" => "D:Foo"
-                    // "D:\Foo" and "\\?\C:\Bar" => "\\?\D:\Foo"
-                    combinedPath = PathInternal.IsDevice(basePath) ? basePath.Substring(0,4) + path.Insert(2, "\\") : path.Insert(2, "\\");
+                    // "D:Foo" and "\\?\C:\Bar" => "\\?\D:\Foo"
+                    combinedPath = PathInternal.IsDevice(basePath) ? CombineNoChecksInternal(basePath.AsSpan().Slice(0, 4), path.AsSpan().Slice(0, 2), "\\", path.AsSpan().Slice(2)) : path.Insert(2, "\\");
                 }
             }
             else
