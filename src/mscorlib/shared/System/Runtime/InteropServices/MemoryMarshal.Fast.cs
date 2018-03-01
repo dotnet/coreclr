cf3a40418640b7c9636d1b12df09fe735755014a
ahsonkhan <ahson_ahmedk@yahoo.com>
2018-03-01 11:52:33 -0800

    Move TryGetString to common MemoryMarshal.cs

--- src/mscorlib/shared/System/Runtime/InteropServices/MemoryMarshal.Fast.cs
+++ src/mscorlib/shared/System/Runtime/InteropServices/MemoryMarshal.Fast.cs
@@ -168,29 +168,5 @@ namespace System.Runtime.InteropServices
         /// <param name="length">The number of <typeparamref name="T"/> elements the memory contains.</param>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static ReadOnlySpan<T> CreateReadOnlySpan<T>(ref T reference, int length) => new ReadOnlySpan<T>(ref reference, length);
-
-        /// <summary>Attempts to get the underlying <see cref="string"/> from a <see cref="ReadOnlyMemory{T}"/>.</summary>
-        /// <param name="readOnlyMemory">The memory that may be wrapping a <see cref="string"/> object.</param>
-        /// <param name="text">The string.</param>
-        /// <param name="start">The starting location in <paramref name="text"/>.</param>
-        /// <param name="length">The number of items in <paramref name="text"/>.</param>
-        /// <returns></returns>
-        public static bool TryGetString(this ReadOnlyMemory<char> readOnlyMemory, out string text, out int start, out int length)
-        {
-            if (readOnlyMemory.GetObjectStartLength(out int offset, out int count) is string s)
-            {
-                text = s;
-                start = offset;
-                length = count;
-                return true;
-            }
-            else
-            {
-                text = null;
-                start = 0;
-                length = 0;
-                return false;
-            }
-        }
     }
 }
