dotnet/runtime

commit 31b5ee0bc7df328fd9187d25dbadebe7c1623d38

Direct mapping of IL-assembly images that don't contain writeable sections.

src/utilcode/pedecoder.cpp
@@ -440,6 +440,37 @@ CHECK PEDecoder::CheckSection(COUNT_T previousAddressEnd, COUNT_T addressStart, 
     CHECK_OK;
 }
 
+BOOL PEDecoder::HasWriteableSections() const
+{
+    CONTRACT_CHECK
+    {
+        INSTANCE_CHECK;
+        PRECONDITION(CheckFormat());
+        NOTHROW;
+        GC_NOTRIGGER;
+        SUPPORTS_DAC;
+        SO_TOLERANT;
+    }
+    CONTRACT_CHECK_END;
+
+    PTR_IMAGE_SECTION_HEADER pSection = FindFirstSection(FindNTHeaders());
+    _ASSERTE(pSection != NULL);
+
+    PTR_IMAGE_SECTION_HEADER pSectionEnd = pSection + VAL16(FindNTHeaders()->FileHeader.NumberOfSections);
+
+    while (pSection < pSectionEnd)
+    {
+        if ((pSection->Characteristics & VAL32(IMAGE_SCN_MEM_WRITE)) != 0)
+        {
+            return TRUE;
+        }
+
+        pSection++;
+    }
+
+    return FALSE;
+}
+
 CHECK PEDecoder::CheckDirectoryEntry(int entry, int forbiddenFlags, IsNullOK ok) const
 {
     CONTRACT_CHECK

src/vm/peimage.cpp
@@ -909,13 +909,36 @@ PTR_PEImageLayout PEImage::GetLayoutInternal(DWORD imageLayoutMask,DWORD flags)
     {
         _ASSERTE(HasID());
 
-        if (imageLayoutMask&PEImageLayout::LAYOUT_MAPPED)
+        BOOL bIsMappedLayoutSuitable = ((imageLayoutMask & PEImageLayout::LAYOUT_MAPPED) != 0);
+        BOOL bIsFlatLayoutSuitable = ((imageLayoutMask & PEImageLayout::LAYOUT_FLAT) != 0);
+
+#if !defined(PLATFORM_UNIX)
+        if (bIsMappedLayoutSuitable)
         {
-          pRetVal = PEImage::CreateLayoutMapped();
+            bIsFlatLayoutSuitable = FALSE;
         }
-        else
+#endif // !PLATFORM_UNIX
+
+        _ASSERTE(bIsMappedLayoutSuitable || bIsFlatLayoutSuitable);
+
+        BOOL bIsMappedLayoutRequired = !bIsFlatLayoutSuitable;
+        BOOL bIsFlatLayoutRequired = !bIsMappedLayoutSuitable;
+
+        if (bIsFlatLayoutRequired
+            || (bIsFlatLayoutSuitable && !m_bIsTrustedNativeImage))
         {
-          pRetVal = PEImage::CreateLayoutFlat();
+          _ASSERTE(bIsFlatLayoutSuitable);
+
+          BOOL bPermitWriteableSections = bIsFlatLayoutRequired;
+
+          pRetVal = PEImage::CreateLayoutFlat(bPermitWriteableSections);
+        }
+
+        if (pRetVal == NULL)
+        {
+          _ASSERTE(bIsMappedLayoutSuitable);
+
+          pRetVal = PEImage::CreateLayoutMapped();
         }
     }
 
@@ -992,7 +1015,7 @@ PTR_PEImageLayout PEImage::CreateLayoutMapped()
     return pRetVal;
 }
 
-PTR_PEImageLayout PEImage::CreateLayoutFlat()
+PTR_PEImageLayout PEImage::CreateLayoutFlat(BOOL bPermitWriteableSections)
 {
     CONTRACTL
     {
@@ -1002,12 +1025,22 @@ PTR_PEImageLayout PEImage::CreateLayoutFlat()
     }
     CONTRACTL_END;
 
-    PTR_PEImageLayout pRetVal;
+    _ASSERTE(m_pLayouts[IMAGE_FLAT] == NULL);
 
-    pRetVal = PEImageLayout::LoadFlat(GetFileHandle(),this);
-    m_pLayouts[IMAGE_FLAT] = pRetVal;
+    PTR_PEImageLayout pFlatLayout = PEImageLayout::LoadFlat(GetFileHandle(),this);
 
-    return pRetVal;
+    if (!bPermitWriteableSections && pFlatLayout->HasWriteableSections())
+    {
+        pFlatLayout->Release();
+
+        return NULL;
+    }
+    else
+    {
+        m_pLayouts[IMAGE_FLAT] = pFlatLayout;
+
+        return pFlatLayout;
+    }
 }
 
 /* static */
@@ -1070,17 +1103,44 @@ void PEImage::Load()
     }
 
     SimpleWriteLockHolder lock(m_pLayoutLock);
-    if(!IsFile())
+
+    _ASSERTE(m_pLayouts[IMAGE_LOADED] == NULL);
+
+#ifdef PLATFORM_UNIX
+    if (m_pLayouts[IMAGE_FLAT] != NULL
+        && m_pLayouts[IMAGE_FLAT]->CheckFormat()
+        && m_pLayouts[IMAGE_FLAT]->IsILOnly()
+        && !m_pLayouts[IMAGE_FLAT]->HasWriteableSections())
     {
-        if (!m_pLayouts[IMAGE_FLAT]->CheckILOnly())
-            ThrowHR(COR_E_BADIMAGEFORMAT);
-        if(m_pLayouts[IMAGE_LOADED]==NULL)
-            SetLayout(IMAGE_LOADED,PEImageLayout::LoadFromFlat(m_pLayouts[IMAGE_FLAT]));
+        // IL-only images with writeable sections are mapped in general way,
+        // because the writeable sections should always be page-aligned
+        // to make possible setting another protection bits exactly for these sections
+        _ASSERTE(!m_pLayouts[IMAGE_FLAT]->HasWriteableSections());
+
+        // As the image is IL-only, there should no be native code to execute
+        _ASSERTE(!m_pLayouts[IMAGE_FLAT]->HasNativeEntryPoint());
+
+        m_pLayouts[IMAGE_FLAT]->AddRef();
+
+        SetLayout(IMAGE_LOADED, m_pLayouts[IMAGE_FLAT]);
     }
     else
+#endif // PLATFORM_UNIX
     {
-        if(m_pLayouts[IMAGE_LOADED]==NULL)
-            SetLayout(IMAGE_LOADED,PEImageLayout::Load(this,TRUE));
+        if(!IsFile())
+        {
+            _ASSERTE(m_pLayouts[IMAGE_FLAT] != NULL);
+
+            if (!m_pLayouts[IMAGE_FLAT]->CheckILOnly())
+                ThrowHR(COR_E_BADIMAGEFORMAT);
+            if(m_pLayouts[IMAGE_LOADED]==NULL)
+                SetLayout(IMAGE_LOADED,PEImageLayout::LoadFromFlat(m_pLayouts[IMAGE_FLAT]));
+        }
+        else
+        {
+            if(m_pLayouts[IMAGE_LOADED]==NULL)
+                SetLayout(IMAGE_LOADED,PEImageLayout::Load(this,TRUE));
+        }
     }
 }
 

src/inc/pedecoder.h
@@ -187,6 +187,8 @@ class PEDecoder
 
     DWORD GetImageIdentity() const;
 
+    BOOL HasWriteableSections() const;
+
     // Directory entry access
 
     BOOL HasDirectoryEntry(int entry) const;

src/vm/pefile.cpp
@@ -354,9 +354,22 @@ void PEFile::LoadLibrary(BOOL allowNativeSkip/*=TRUE*/) // if allowNativeSkip==F
 #endif
         {
             if (GetILimage()->IsFile())
-                GetILimage()->LoadFromMapped();
+            {
+#ifdef PLATFORM_UNIX
+                if (GetILimage()->IsILOnly())
+                {
+                    GetILimage()->Load();
+                }
+                else
+#endif // PLATFORM_UNIX
+                {
+                    GetILimage()->LoadFromMapped();
+                }
+            }
             else
+            {
                 GetILimage()->LoadNoFile();
+            }
         }
     }
 

src/vm/peimage.h
@@ -262,7 +262,7 @@ private:
     PTR_PEImageLayout CreateLayoutMapped();
 
     // Create the flat layout
-    PTR_PEImageLayout CreateLayoutFlat();
+    PTR_PEImageLayout CreateLayoutFlat(BOOL bPermitWriteableSections);
 #endif
     // Get an existing layout corresponding to the mask, no AddRef
     PTR_PEImageLayout GetExistingLayoutInternal(DWORD imageLayoutMask);

dotnet/coreclr

commit d0080098c0c9174dbbcdbc39ecd5906eba257223

Fix handling of incorrect assemblies on Unix
This fixes the regression that was introduced by #10772 and is
caused by a missing check for validity of loaded assembly file.

Related issue: #15544

src/utilcode/pedecoder.cpp
@@ -445,6 +445,7 @@ BOOL PEDecoder::HasWriteableSections() const
     CONTRACT_CHECK
     {
         INSTANCE_CHECK;
+        PRECONDITION(CheckNTHeaders());
         PRECONDITION(CheckFormat());
         NOTHROW;
         GC_NOTRIGGER;
@@ -453,7 +454,7 @@ BOOL PEDecoder::HasWriteableSections() const
     }
     CONTRACT_CHECK_END;
 
-    PTR_IMAGE_SECTION_HEADER pSection = FindFirstSection(FindNTHeaders());
+    PTR_IMAGE_SECTION_HEADER pSection = FindFirstSection();
     _ASSERTE(pSection != NULL);
 
     PTR_IMAGE_SECTION_HEADER pSectionEnd = pSection + VAL16(FindNTHeaders()->FileHeader.NumberOfSections);

src/vm/peimage.cpp
@@ -1029,7 +1029,9 @@ PTR_PEImageLayout PEImage::CreateLayoutFlat(BOOL bPermitWriteableSections)
 
     PTR_PEImageLayout pFlatLayout = PEImageLayout::LoadFlat(GetFileHandle(),this);
 
-    if (!bPermitWriteableSections && pFlatLayout->HasWriteableSections())
+    if (!bPermitWriteableSections
+        && pFlatLayout->CheckNTHeaders()
+        && pFlatLayout->HasWriteableSections())
     {
         pFlatLayout->Release();
 
@@ -1114,8 +1116,7 @@ void PEImage::Load()
 
 #ifdef PLATFORM_UNIX
     if (m_pLayouts[IMAGE_FLAT] != NULL
-        && m_pLayouts[IMAGE_FLAT]->CheckFormat()
-        && m_pLayouts[IMAGE_FLAT]->IsILOnly()
+        && m_pLayouts[IMAGE_FLAT]->CheckILOnlyFormat()
         && !m_pLayouts[IMAGE_FLAT]->HasWriteableSections())
     {
         // IL-only images with writeable sections are mapped in general way,

tests/src/Loader/regressions/GitHub_15544/main.cs
@@ -1,0 +1,37 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+//
+
+using System;
+using System.IO;
+using System.Reflection;
+
+public class CMain{
+    public static int Main(String[] args) {
+        string tempFileName = Path.GetTempFileName();
+
+        bool isThrown = false;
+
+        try
+        {
+            AssemblyName.GetAssemblyName(tempFileName);
+        }
+        catch (BadImageFormatException)
+        {
+            isThrown = true;
+        }
+
+        File.Delete(tempFileName);
+
+        if (isThrown) {
+            Console.WriteLine("PASS");
+
+            return 100;
+        } else {
+            Console.WriteLine("FAIL");
+
+            return 101;
+        }
+    }
+}

tests/src/Loader/regressions/GitHub_15544/main.csproj
@@ -1,0 +1,31 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<Project ToolsVersion="12.0" DefaultTargets="Build" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+  <Import Project="$([MSBuild]::GetDirectoryNameOfFileAbove($(MSBuildThisFileDirectory), dir.props))\dir.props" />
+  <PropertyGroup>
+    <Configuration Condition=" '$(Configuration)' == '' ">Debug</Configuration>
+    <Platform Condition=" '$(Platform)' == '' ">AnyCPU</Platform>
+    <SchemaVersion>2.0</SchemaVersion>
+    <ProjectGuid>{AC75380E-F196-4F32-9BCF-F0589AF864E6}</ProjectGuid>
+    <OutputType>Exe</OutputType>
+    <ProjectTypeGuids>{786C830F-07A1-408B-BD7F-6EE04809D6DB};{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}</ProjectTypeGuids>
+    <SolutionDir Condition="$(SolutionDir) == '' Or $(SolutionDir) == '*Undefined*'">..\..\</SolutionDir>
+    <AllowUnsafeBlocks>true</AllowUnsafeBlocks>
+  </PropertyGroup>
+  <!-- Default configurations to help VS understand the configurations -->
+  <PropertyGroup Condition=" '$(Configuration)|$(Platform)' == 'Debug|AnyCPU' ">
+  </PropertyGroup>
+  <PropertyGroup Condition=" '$(Configuration)|$(Platform)' == 'Release|AnyCPU' ">
+  </PropertyGroup>
+  <ItemGroup>
+    <CodeAnalysisDependentAssemblyPaths Condition=" '$(VS100COMNTOOLS)' != '' " Include="$(VS100COMNTOOLS)..\IDE\PrivateAssemblies">
+      <Visible>False</Visible>
+    </CodeAnalysisDependentAssemblyPaths>
+  </ItemGroup>
+  <ItemGroup>
+    <Compile Include="main.cs" />
+  </ItemGroup>
+  <ItemGroup>
+    <Service Include="{82A7F48D-3B50-4B1E-B82E-3ADA8210C358}" />
+  </ItemGroup>
+  <Import Project="$([MSBuild]::GetDirectoryNameOfFileAbove($(MSBuildThisFileDirectory), dir.targets))\dir.targets" />
+</Project>