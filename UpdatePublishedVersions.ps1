7c099272e5c139f6f66124da5e74557b15d2f997
William Godbe <wigodbe@microsoft.com>
2018-03-09 12:40:41 -0800

    Fix UpdatePublishedVersions.ps1 (#16865)
    Remove the "Tools" string accidentally left in the run.cmd invocation

--- UpdatePublishedVersions.ps1
+++ UpdatePublishedVersions.ps1
@@ -16,7 +16,7 @@ param(
     # A pattern matching all packages in the set that the versions repository should be set to.
     [Parameter(Mandatory=$true)][string]$nupkgPath)
 
-& "$PSScriptRoot\Tools\run.cmd" build -- tests\build.proj /t:UpdatePublishedVersions `
+& "$PSScriptRoot\run.cmd" build -- tests\build.proj /t:UpdatePublishedVersions `
     /p:GitHubUser="$gitHubUser" `
     /p:GitHubEmail="$gitHubEmail" `
     /p:GitHubAuthToken="$gitHubAuthToken" `
