2aa71267362c5a9a167483772e07cf43cb203e45
Ben Adams <thundercat@illyriad.co.uk>
2018-03-24 17:21:10 +0000

    Feedback

--- src/mscorlib/shared/System/Collections/Generic/EmptyEnumerator.cs
+++ src/mscorlib/shared/System/Collections/Generic/EmptyEnumerator.cs
@@ -2,27 +2,47 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Runtime.CompilerServices;
+using System.Threading;
+
 namespace System.Collections.Generic
 {
     internal class EmptyEnumerator<T> : IEnumerator<T>, IDictionaryEnumerator
     {
-        public static EmptyEnumerator<T> Shared { get; } = new EmptyEnumerator<T>();
+        private static EmptyEnumerator<T> s_shared;
+
+        public static EmptyEnumerator<T> Shared
+        {
+            [MethodImpl(MethodImplOptions.NoInlining)]
+            get
+            {
+                return s_shared ?? InitalizeShared();
+            }
+        }
 
         private EmptyEnumerator() { }
 
-        public T Current { get; } = default;
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static EmptyEnumerator<T> InitalizeShared()
+        {
+            var newEnumerator = new EmptyEnumerator<T>();
+            Interlocked.CompareExchange(ref s_shared, newEnumerator, null); // failure is benign. Someone else set the value.
+            return s_shared;
+        }
+
+        public T Current => default(T);
 
         public bool MoveNext() => false;
 
         public void Dispose() { }
 
-        DictionaryEntry IDictionaryEnumerator.Entry { get; } = default;
+        DictionaryEntry IDictionaryEnumerator.Entry => default(DictionaryEntry);
 
-        object IDictionaryEnumerator.Key { get; } = null;
+        object IDictionaryEnumerator.Key => null;
 
-        object IDictionaryEnumerator.Value { get; } = null;
+        object IDictionaryEnumerator.Value => null;
 
-        object IEnumerator.Current { get; } = null;
+        object IEnumerator.Current => null;
 
         void IEnumerator.Reset() { }
     }
