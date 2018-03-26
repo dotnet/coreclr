aa80194b45ca4f8d284fed92208e13daa14773d4
Sung Yoon Whang <suwhang@microsoft.com>
2018-03-26 12:20:17 -0700

    Fix typo

--- src/vm/excep.cpp
+++ src/vm/excep.cpp
@@ -5241,7 +5241,7 @@ LONG InternalUnhandledExceptionFilter_Worker(
         if (tore.GetType() == TypeOfReportedError::NativeThreadUnhandledException)
         {
             pParam->retval = EXCEPTION_CONTINUE_SEARCH;
-#ifdef defined(FEATURE_EVENT_TRACE) && !defined(FEATURE_PAL)
+#if defined(FEATURE_EVENT_TRACE) && !defined(FEATURE_PAL)
             DoReportForIgnoredUnhandledException(pParam->pExceptionInfo);
 #endif
             goto lDone;
@@ -5251,7 +5251,7 @@ LONG InternalUnhandledExceptionFilter_Worker(
         {
             LOG((LF_EH, LL_INFO100, "InternalUnhandledExceptionFilter_Worker, ignoring the exception\n"));
             pParam->retval = EXCEPTION_CONTINUE_SEARCH;
-#ifdef defined(FEATURE_EVENT_TRACE) && !defined(FEATURE_PAL)
+#if defined(FEATURE_EVENT_TRACE) && !defined(FEATURE_PAL)
             DoReportForIgnoredUnhandledException(pParam->pExceptionInfo);
 #endif
             goto lDone;
