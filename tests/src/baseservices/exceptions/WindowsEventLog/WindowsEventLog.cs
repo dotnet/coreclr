798d23a3612c14796c9d27f0ab15727cceed594f
Sung Yoon Whang <suwhang@microsoft.com>
2018-04-03 16:59:17 -0700

    Make it run under one executable

--- tests/src/baseservices/exceptions/WindowsEventLog/WindowsEventLog.cs
+++ tests/src/baseservices/exceptions/WindowsEventLog/WindowsEventLog.cs
@@ -1,33 +1,119 @@
 using System;
 using System.Diagnostics;
+using System.IO;
 
 class Program
 {
-    static void Main()
+    private static void UnhandledException()
     {
-        Process unhandledException = new Process();
-        unhandledException.StartInfo.FileName = "..\\UnhandledException.exe";
-        unhandledException.Start();
+        A();
+    }
 
-        unhandledException.WaitForExit();
+    private static void FailFast()
+    {
+        try 
+        {
+            A();
+        }
+        catch (ArgumentException ex)
+        {
+            Environment.FailFast("failing fast", ex);
+        }
 
-        EventLog log = new
-        EventLog("Application");
+    }
 
-        DateTime dt = DateTime.Now.AddHours(-5.00);
-        Console.WriteLine(dt.ToString());
+    private static void A()
+    {
+        B();
+    }
+
+    private static void B()
+    {
+        throw new ArgumentException("my ae");
+    }
+
+    private static bool LaunchTest(string testName, string[] logEntriesToCheck)
+    {
+        DateTime dt = DateTime.Now;
+
+        Process testProcess = new Process();
+        string currentPath = Directory.GetCurrentDirectory();
+        string corerunPath = Environment.GetEnvironmentVariable("CORE_ROOT") + "\\corerun.exe";
+
+        testProcess.StartInfo.FileName = corerunPath;
+        testProcess.StartInfo.Arguments = currentPath + "\\WindowsEventLog.exe " + testName;
+        testProcess.StartInfo.EnvironmentVariables["CORE_ROOT"] =  Environment.GetEnvironmentVariable("CORE_ROOT");
+        testProcess.StartInfo.UseShellExecute = false;
+
+        testProcess.Start();
+        testProcess.WaitForExit();
+
+        EventLog log = new EventLog("Application");
+        int checkCount = 0;
 
         foreach (EventLogEntry entry in log.Entries)
         {
             if (entry.TimeGenerated > dt) 
-            //if (entry.Source.Equals("Application Hang") && (entry.TimeGenerated > dt))
-            //if (entry.Source.Equals(".NET Runtime 2.0 Error Reporting") && (entry.TimeGenerated > dt))
             {
-                Console.WriteLine(entry.Source);
-                Console.WriteLine(entry.EntryType);
-                Console.WriteLine(entry.Message);
-                Console.WriteLine("--------");
+                String source = entry.Source;
+                String message = entry.Message;
+
+                foreach (string logEntry in logEntriesToCheck)
+                {
+                    if (message.Contains(logEntry))
+                        checkCount += 1;
+                }
+
+                if (source.Contains(".NET Runtime") && checkCount == logEntriesToCheck.Length)
+                    return true;
             }
         }
+        return false;
+    }
+    
+    private static bool RunUnhandledExceptionTest()
+    {
+        string[] logEntriesToCheck = {"unhandled exception", "ArgumentException"};
+        return LaunchTest("UnhandledException", logEntriesToCheck);
+    }
+
+    private static bool RunFailFastTest()
+    {
+        string[] logEntriesToCheck = {"The application requested process termination through System.Environment.FailFast", "ArgumentException"};
+        return LaunchTest("FailFast", logEntriesToCheck);
+    }
+
+
+    public static int Main(string[] args)
+    {
+        if (args.Length == 0) // When invoked with no args, launch itself with appropriate args to cause various exceptions
+        {
+            if (!RunUnhandledExceptionTest())
+            {
+                Console.WriteLine("WindowsEventLog Test: UnhandledExceptionTest failed.");
+                return 1;
+            }
+        
+            if (!RunFailFastTest())
+            {
+                Console.WriteLine("WindowsEventLog Test: FailFastTest failed.");
+                return 1;
+            }
+
+            return 100;
+        }
+
+        Debug.Assert(args.Length == 1);
+
+        if (args[0] == "UnhandledException")
+        {
+            UnhandledException();
+        }
+        else if (args[0] == "FailFast")
+        {
+            FailFast();
+        }
+
+        return 100; // Should never reach here 
     }
 }
