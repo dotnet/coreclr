24e4cbe0d00c0af305ffb751538a7cfa025cea51
Koundinya Veluri <kouvel@microsoft.com>
2018-04-09 10:31:18 -0700

    Small fix

--- src/vm/prestub.cpp
+++ src/vm/prestub.cpp
@@ -734,12 +734,10 @@ PCODE MethodDesc::JitCompileCodeLockedEventWrapper(PrepareCodeConfig* pConfig, J
     }
 
 #ifdef FEATURE_TIERED_COMPILATION
-    if (flags.IsSet(CORJIT_FLAGS::CORJIT_FLAG_TIER0))
+    if (g_pConfig->TieredCompilation() && flags.IsSet(CORJIT_FLAGS::CORJIT_FLAG_TIER0))
     {
-        // The flag above is only set (in TieredCompilationManager::GetJitFlags()) when tiered compilation is enabled, this
-        // method was eligible for tiered compilation at the time when it was checked, and a tier 0 JIT was requested for this
-        // method
-        _ASSERTE(g_pConfig->TieredCompilation());
+        // The flag above is only set (in TieredCompilationManager::GetJitFlags()) when this method was eligible for tiered
+        // compilation at the time when it was checked, and a tier 0 JIT was requested for this method
         GetAppDomain()->GetTieredCompilationManager()->OnTier0JitInvoked();
     }
 #endif // FEATURE_TIERED_COMPILATION
