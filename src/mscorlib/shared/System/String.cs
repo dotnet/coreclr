84848f6aa2e326e81a8ce97798d3a925e107aa46
Ben Adams <thundercat@illyriad.co.uk>
2018-04-11 19:53:36 +0100

    Add comment for ternary operator

--- src/mscorlib/shared/System/String.cs
+++ src/mscorlib/shared/System/String.cs
@@ -442,6 +442,8 @@ namespace System
             // value.Length == 0 as it will elide the bounds check to
             // the first char: value[0] if that is performed following the test
             // for the same test cost.
+            // Ternary operator returning true/false prevents redundant asm generation:
+            // https://github.com/dotnet/coreclr/issues/914
             return (value == null || 0u >= (uint)value.Length) ? true : false;
         }
 
