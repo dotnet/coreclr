bf30bfde33a2fa87bc4bc4f99d583bf11a562241
Tarek Mahmoud Sayed <tarekms@microsoft.com>
2018-04-21 03:57:43 +0430

    Update the comment

--- src/mscorlib/shared/System/TimeZoneInfo.Unix.cs
+++ src/mscorlib/shared/System/TimeZoneInfo.Unix.cs
@@ -384,7 +384,7 @@ namespace System
             string symlinkPath = Interop.Sys.ReadLink(tzFilePath);
             if (symlinkPath != null)
             {
-                // Use Path.Combine to resolve links that contain a relative path (e.g. /etc/localtime).
+                // symlinkPath can be relative path, use Path to get the full absolute path.
                 symlinkPath = Path.GetFullPath(symlinkPath, Path.GetDirectoryName(tzFilePath));
 
                 string timeZoneDirectory = GetTimeZoneDirectory();
