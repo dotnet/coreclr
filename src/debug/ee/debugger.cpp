fa730cc736630208e64be01dce285e6ddfeb09a1
David Mason <davmason@microsoft.com>
2018-05-15 15:50:15 -0700

    change profiler rejit to be enabled by default, and also change the debugger to only give up on setting a breakpoint if a method has been rejitted, rather than just whenever rejit is on

--- src/debug/ee/debugger.cpp
+++ src/debug/ee/debugger.cpp
@@ -3616,7 +3616,9 @@ HRESULT Debugger::SetIP( bool fCanSetIPOnly, Thread *thread,Module *module,
 
     LOG((LF_CORDB, LL_INFO1000, "D::SIP: In SetIP ==> fCanSetIPOnly:0x%x <==!\n", fCanSetIPOnly));
 
-    if (ReJitManager::IsReJITEnabled())
+    CodeVersionManager *pCodeVersionManager = module->GetCodeVersionManager();
+    ILCodeVersion ilCodeVersion = pCodeVersionManager->GetActiveILCodeVersion(module, mdMeth);
+    if (!ilCodeVersion.IsDefaultVersion())
     {
         return CORDBG_E_SET_IP_IMPOSSIBLE;
     }
