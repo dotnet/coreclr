26c504f4950fb3b660b076240785add4e717f3cf
Marco Rossignoli <marco.rossignoli@gmail.com>
2018-05-31 23:19:18 +0200

    update BCryptGenRandom

--- src/System.Private.CoreLib/shared/Interop/Windows/BCrypt/Interop.BCryptGenRandom.cs
+++ src/System.Private.CoreLib/shared/Interop/Windows/BCrypt/Interop.BCryptGenRandom.cs
@@ -10,7 +10,7 @@ internal partial class Interop
 {
     internal partial class BCrypt
     {
-        internal static unsafe int BCryptGenRandom(byte* pbBuffer, int count)
+        internal static unsafe NTSTATUS BCryptGenRandom(byte* pbBuffer, int count)
         {
             Debug.Assert(pbBuffer != null);
             Debug.Assert(count >= 0);
@@ -19,19 +19,25 @@ internal partial class Interop
         }
 
         private const int BCRYPT_USE_SYSTEM_PREFERRED_RNG = 0x00000002;
-        internal const int STATUS_SUCCESS = 0x0;
-        internal const int STATUS_NO_MEMORY = unchecked((int)0xC0000017);
+
+        internal enum NTSTATUS : uint
+        {
+            STATUS_SUCCESS = 0x0,
+            STATUS_NOT_FOUND = 0xc0000225,
+            STATUS_INVALID_PARAMETER = 0xc000000d,
+            STATUS_NO_MEMORY = 0xc0000017,
+        }
 
         [DllImport(Libraries.BCrypt, CharSet = CharSet.Unicode)]
-        private static extern unsafe int BCryptGenRandom(IntPtr hAlgorithm, byte* pbBuffer, int cbBuffer, int dwFlags);
+        private static extern unsafe NTSTATUS BCryptGenRandom(IntPtr hAlgorithm, byte* pbBuffer, int cbBuffer, int dwFlags);
     }
 
     internal static unsafe void GetRandomBytes(byte* buffer, int length)
     {
-        int status = BCrypt.BCryptGenRandom(buffer, length);
-        if (status != BCrypt.STATUS_SUCCESS)
+        BCrypt.NTSTATUS status = BCrypt.BCryptGenRandom(buffer, length);
+        if (status != BCrypt.NTSTATUS.STATUS_SUCCESS)
         {
-            if (status == BCrypt.STATUS_NO_MEMORY)
+            if (status == BCrypt.NTSTATUS.STATUS_NO_MEMORY)
             {
                 throw new OutOfMemoryException();
             }
