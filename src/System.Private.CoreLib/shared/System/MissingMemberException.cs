06cffe00f2c0bba808d8683248c152bd7788779e
Maryam Ariyan <maariyan@microsoft.com>
2018-05-30 17:33:44 -0700

    Removing useless comments

--- src/System.Private.CoreLib/shared/System/MissingMemberException.cs
+++ src/System.Private.CoreLib/shared/System/MissingMemberException.cs
@@ -2,15 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-/*=============================================================================
-**
-**
-**
-** Purpose: The exception class for versioning problems with DLLS.
-**
-**
-=============================================================================*/
-
 using System.Runtime.Serialization;
 
 namespace System
--- src/System.Private.CoreLib/src/System/MissingMemberException.CoreCLR.cs
+++ src/System.Private.CoreLib/src/System/MissingMemberException.CoreCLR.cs
@@ -2,15 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-/*=============================================================================
-**
-**
-**
-** Purpose: The exception class for versioning problems with DLLS.
-**
-**
-=============================================================================*/
-
 using System.Runtime.Serialization;
 using System.Runtime.CompilerServices;
 
@@ -18,7 +9,6 @@ namespace System
 {
     public partial class MissingMemberException : MemberAccessException, ISerializable
     {
-        // Called to format signature
         [MethodImplAttribute(MethodImplOptions.InternalCall)]
         internal static extern string FormatSignature(byte[] signature);
     }
