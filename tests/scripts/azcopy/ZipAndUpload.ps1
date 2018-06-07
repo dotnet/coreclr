62d9acb1053b41e423b36c3ea63ee1b826d520c6
Adam Sitnik <adam.sitnik@gmail.com>
2018-06-07 14:00:24 +0200

    "withCredentials" step exposes the credentials as Shell Environment Variables, not as groovy variables, read the sas from env var

--- tests/scripts/azcopy/ZipAndUpload.ps1
+++ tests/scripts/azcopy/ZipAndUpload.ps1
@@ -4,6 +4,8 @@ Param(
   [string] $blobStorageUrl = "https://dotnetperfciblobs.blob.core.windows.net",
   [string] $container,
   [string] $fileName,
+  
+  [Parameter(Mandatory=$false)]
   [string] $sas
 )
 
@@ -19,6 +21,16 @@ $zipped = "$temp\$fileName.zip"
 
 Write-Output "Zipping the input files."
 Compress-Archive -Path $inputFiles -DestinationPath $zipped
+
+if (!$sas)
+{
+    $sas = $env:AZ_BLOB_LOGS_SAS_TOKEN
+}
+
+if (!$sas)
+{
+    throw [System.ArgumentException ] "SAS token not provided and not present in env var."
+}
 
 Write-Output "Uploading the input files to Azure Blob Storage."
 Start-Process $azCopyPath -Argument "/Source:$zipped /Dest:$blobStorageUrl/$container/$fileName /DestSAS:$sas /V:.\bin\Logs\AzCopyLog.txt" -Wait
