f5238bb27b4a886eb71580fe816c336f9cb4ce59
Anipik <anirudhagnihotry098@gmail.com>
2018-06-09 19:20:37 -0700

    Removed Path Length check

--- src/System.Private.CoreLib/shared/System/Threading/Mutex.Windows.cs
+++ src/System.Private.CoreLib/shared/System/Threading/Mutex.Windows.cs
@@ -21,17 +21,9 @@ namespace System.Threading
 #if PLATFORM_UNIX
         // Maximum file name length on tmpfs file system.
         private const int WaitHandleNameMax = 255;
+#else
+        private const int WaitHandleNameMax = Interop.Kernel32.MAX_PATH;
 #endif
-
-        private static void VerifyNameForCreate(string name)
-        {
-#if PLATFORM_WINDOWS
-            if (name != null && (Interop.Kernel32.MAX_PATH < name.Length))
-            {
-                throw new ArgumentException(SR.Format(SR.Argument_WaitHandleNameTooLong, name, Interop.Kernel32.MAX_PATH), nameof(name));
-            }
-#endif
-        }
 
         private void CreateMutexCore(bool initiallyOwned, string name, out bool createdNew)
         {
@@ -42,11 +34,11 @@ namespace System.Threading
             if (mutexHandle.IsInvalid)
             {
                 mutexHandle.SetHandleAsInvalid();
-#if PLATFORM_UNIX
+
                 if (errorCode == Interop.Errors.ERROR_FILENAME_EXCED_RANGE)
                     // On Unix, length validation is done by CoreCLR's PAL after converting to utf-8
                     throw new ArgumentException(SR.Format(SR.Argument_WaitHandleNameTooLong, name, WaitHandleNameMax), nameof(name));
-#endif
+
                 if (errorCode == Interop.Errors.ERROR_INVALID_HANDLE)
                     throw new WaitHandleCannotBeOpenedException(SR.Format(SR.Threading_WaitHandleCannotBeOpenedException_InvalidHandle, name));
 
@@ -69,7 +61,6 @@ namespace System.Threading
                 throw new ArgumentException(SR.Argument_EmptyName, nameof(name));
             }
 
-            VerifyNameForCreate(name);
             result = null;
             // To allow users to view & edit the ACL's, call OpenMutex
             // with parameters to allow us to view & edit the ACL.  This will
@@ -80,13 +71,12 @@ namespace System.Threading
             if (myHandle.IsInvalid)
             {
                 int errorCode = Marshal.GetLastWin32Error();
-#if PLATFORM_UNIX
                 if (errorCode == Interop.Errors.ERROR_FILENAME_EXCED_RANGE)
                 {
                     // On Unix, length validation is done by CoreCLR's PAL after converting to utf-8
                     throw new ArgumentException(SR.Format(SR.Argument_WaitHandleNameTooLong, name, WaitHandleNameMax), nameof(name));
                 }
-#endif
+
                 if (Interop.Errors.ERROR_FILE_NOT_FOUND == errorCode || Interop.Errors.ERROR_INVALID_NAME == errorCode)
                     return OpenExistingResult.NameNotFound;
                 if (Interop.Errors.ERROR_PATH_NOT_FOUND == errorCode)
