55477aba3c86b6f0a366afc8fa4a6a8dc59b1036
jswolf19 <jswolf19@hotmail.com>
2018-06-10 13:03:10 +0900

    Added CallerArgumentExpressionAttribute

---
+++ src/System.Private.CoreLib/shared/System/Runtime/CompilerServices/CallerArgumentExpressionAttribute.cs
@@ -0,0 +1,22 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace System.Runtime.CompilerServices
+{
+    [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
+    public sealed class CallerArgumentExpressionAttribute : Attribute
+    {
+        public string ParameterName { get; }
+
+        public CallerArgumentExpressionAttribute(string parameterName)
+        {
+            if (parameterName == null)
+            {
+                throw new ArgumentNullException(nameof(parameterName));
+            }
+
+            ParameterName = parameterName;
+        }
+    }
+}
