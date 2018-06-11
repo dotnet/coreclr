eb48b139dfb72c9e835a83377491e081dac5ce2e
luqunl <luqunl@LUQUN03>
2018-06-11 14:41:55 -0700

    Add public implementation FrameworkEventSourceSupport

---
+++ src/System.Private.CoreLib/src/Internal/Diagnostics/Tracing/FrameworkEventSourceSupport.cs
@@ -0,0 +1,44 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Diagnostics.Tracing;
+using System.Runtime.CompilerServices;
+
+namespace Internal.Diagnostics.Tracing
+{
+    //
+    // An internal contract that exposes just enough async debugger support needed by the AsTask() extension methods in the WindowsRuntimeSystemExtensions class.
+    //
+    public static class FrameworkEventSourceSupport
+    {
+        public static bool IsEnabled
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get
+            {
+                return FrameworkEventSource.Log.IsEnabled(EventLevel.Informational, FrameworkEventSource.Keywords.ThreadTransfer);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void ThreadTransferSendObj(object id, int kind, string info, bool multiDequeues)
+        {
+            FrameworkEventSource.Log.ThreadTransferSendObj(id, kind, info, multiDequeues);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void ThreadTransferReceiveObj(object id, int kind, string info)
+        {
+            FrameworkEventSource.Log.ThreadTransferReceiveObj(id, kind, info);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void ThreadTransferReceiveHandledObj(object id, int kind, string info)
+        {
+            FrameworkEventSource.Log.ThreadTransferReceiveHandledObj(id, kind, info);
+        }
+    }
+}
+
