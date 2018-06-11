4d6171a55933402da43e3af149854ab3f928a98e
luqunl <luqunl@LUQUN03>
2018-06-11 15:02:16 -0700

    update comments

--- src/System.Private.CoreLib/src/Internal/Resources/WindowsRuntimeResourceManagerBase.cs
+++ src/System.Private.CoreLib/src/Internal/Resources/WindowsRuntimeResourceManagerBase.cs
@@ -7,13 +7,8 @@ using System.Globalization;
 
 namespace Internal.Resources
 {
-    //
     // This is implemented in System.Runtime.WindowsRuntime as function System.Resources.WindowsRuntimeResourceManager,
     // allowing us to ask for a WinRT-specific ResourceManager.
-    // It is important to have WindowsRuntimeResourceManagerBase as regular class with virtual methods and default implementations. 
-    // Defining WindowsRuntimeResourceManagerBase as abstract class or interface will cause issues when adding more methods to it 
-    // because it'll create dependency between mscorlib and System.Runtime.WindowsRuntime which will require always shipping both DLLs together. 
-    //
     public abstract class WindowsRuntimeResourceManagerBase
     {
         public abstract bool Initialize(string libpath, string reswFilename, out PRIExceptionInfo exceptionInfo);
