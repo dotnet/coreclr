7910a0fb31e5a1b5f5d171433ca27739e541ec95
Brian Robbins <brianrob@microsoft.com>
2018-06-19 16:47:17 -0700

    Add a very basic crash test for RuntimeEventSource.

---
+++ tests/src/tracing/runtimeeventsource/RuntimeEventSourceTest.cs
@@ -0,0 +1,68 @@
+using System;
+using System.IO;
+using System.Diagnostics.Tracing;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using Tracing.Tests.Common;
+
+namespace Tracing.Tests
+{
+    public sealed class RuntimeEventSourceTest
+    {
+        static int Main(string[] args)
+        {
+            // Get the RuntimeEventSource.
+            EventSource eventSource = RuntimeEventSource.Log;
+
+            // Create of EventListener.
+            using (SimpleEventListener listener = new SimpleEventListener())
+            {
+                // Trigger the allocator task.
+                System.Threading.Tasks.Task.Run(new Action(Allocator));
+
+                // Enable events.
+                listener.EnableEvents(eventSource, EventLevel.Verbose, (EventKeywords)(-1));
+
+                // Wait for events.
+                Thread.Sleep(5000);
+
+                GC.Collect(2, GCCollectionMode.Forced);
+
+                // Wait for more events.
+                Thread.Sleep(5000);
+            }
+
+            return 100;
+        }
+
+        private static void Allocator()
+        {
+            while (true)
+            {
+                for(int i=0; i<1000; i++)
+                    GC.KeepAlive(new object());
+
+                Thread.Sleep(10);
+            }
+        }
+    }
+
+    internal sealed class SimpleEventListener : EventListener
+    {
+        public int EventCount { get; private set; } = 0;
+
+        protected override void OnEventWritten(EventWrittenEventArgs eventData)
+        {
+            Console.WriteLine($"ID = {eventData.EventId} Name = {eventData.EventName}");
+            for (int i = 0; i < eventData.Payload.Count; i++)
+            {
+                string payloadString = eventData.Payload[i] != null ? eventData.Payload[i].ToString() : string.Empty;
+                Console.WriteLine($"\tName = \"{eventData.PayloadNames[i]}\" Value = \"{payloadString}\"");
+            }
+            Console.WriteLine("\n");
+
+
+            EventCount++;
+        }
+    }
+}
