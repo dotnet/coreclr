f7e626d8caee418c966739f1f6ed38e1b0e8a72c
Anipik <anirudhagnihotry098@gmail.com>
2018-06-26 19:43:33 -0700

    splitting safeWaitHandle to .windows and .unix

--- src/System.Private.CoreLib/shared/Microsoft/Win32/SafeHandles/SafeWaitHandle.Windows.cs
+++ src/System.Private.CoreLib/shared/Microsoft/Win32/SafeHandles/SafeWaitHandle.Windows.cs
@@ -2,20 +2,10 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
-
 namespace Microsoft.Win32.SafeHandles
 {
     public sealed partial class SafeWaitHandle : SafeHandleZeroOrMinusOneIsInvalid
     {
-        // Called by P/Invoke marshaler
-        private SafeWaitHandle() : base(true)
-        {
-        }
-
-        public SafeWaitHandle(IntPtr existingHandle, bool ownsHandle) : base(ownsHandle)
-        {
-            SetHandle(existingHandle);
-        }
+        protected override bool ReleaseHandle() => Interop.Kernel32.CloseHandle(handle);
     }
 }
