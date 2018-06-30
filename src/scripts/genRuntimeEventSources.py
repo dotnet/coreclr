5658e23f16df95559eca01575d8a0aa6ad6610b2
Brian Robbins <brianrob@microsoft.com>
2018-06-29 19:18:03 -0700

    Remove EventSource GUID to avoid enabling the EventSource when it is turned on via ETW.

--- src/scripts/genRuntimeEventSources.py
+++ src/scripts/genRuntimeEventSources.py
@@ -374,7 +374,7 @@ namespace System.Diagnostics.Tracing
             increaseTabLevel()
 
             className = providerNameToClassNameMap[providerName]
-            writeOutput(outputFile, "[EventSource(Name = \"" + providerName + "\", Guid = \"" + providerNode.getAttribute("guid") + "\")]\n")
+            writeOutput(outputFile, "[EventSource(Name = \"" + providerName + "\")]\n")
             writeOutput(outputFile, "internal sealed partial class " + className + " : EventSource\n")
             writeOutput(outputFile, "{\n")
             increaseTabLevel()
