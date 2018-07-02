897e33daa42cdf33b9c4bd89f558fe5ed69a8ec8
Amy Yu <amycmyu@gmail.com>
2018-07-02 10:40:56 -0700

    Test infrastructure for R2RDump, compare xml and output diff

---
+++ tests/src/r2rdump/BasicTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Xunit;
+using R2RDump;
+using System.Xml;
+using System.IO;
+using System.Xml.Linq;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit.Abstractions;
+
+namespace R2RDump.Test
+{
+    public class BasicTest
+    {
+        private readonly ITestOutputHelper _output;
+        public BasicTest(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void NoSections()
+        {
+            R2RReader r2r = new R2RReader("c:/Code/r2rdump/peFiles/NoSections.ni.dll");
+            Assert.Equal(0, r2r.R2RHeader.Sections.Count);
+            Assert.Equal(0, r2r.R2RMethods.Count);
+        }
+
+        [Fact]
+        public void Simple()
+        {
+            List<XmlNode> testXmlNodes = TestHelpers.GetTestXmlNodes("c:/Code/r2rdump/peFiles/HelloWorld.ni.dll", true, true, true, true, true, true).Cast<XmlNode>().ToList();
+            List<XmlNode> expectedXmlNodes = TestHelpers.GetExpectedXmlNodes("c:/Code/r2rdump/out/HelloWorld.xml").Cast<XmlNode>().ToList();
+            bool identical = TestHelpers.XmlDiff(_output, testXmlNodes, expectedXmlNodes);
+            Assert.True(identical);
+        }
+    }
+}
---
+++ tests/src/r2rdump/TestHelpers.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+using Xunit.Abstractions;
+
+namespace R2RDump.Test
+{
+    class TestHelpers
+    {
+        public static bool XmlDiff(ITestOutputHelper output, List<XmlNode> testXmlNodes, List<XmlNode> expectedXmlNodes)
+        {
+            testXmlNodes.RemoveAll(node => !IsLeaf(node));
+            expectedXmlNodes.RemoveAll(node => !IsLeaf(node));
+            Dictionary<string, XmlNode> diffExpected = testXmlNodes.Except(expectedXmlNodes, new XElementEqualityComparer()).ToDictionary(node => XmlNodeFullName(node));
+            Dictionary<string, XmlNode> diffTest = expectedXmlNodes.Except(testXmlNodes, new XElementEqualityComparer()).ToDictionary(node => XmlNodeFullName(node));
+
+            foreach (KeyValuePair<string, XmlNode> diff in diffTest)
+            {
+                XmlNode testNode = diff.Value;
+                output.WriteLine("Test:");
+                output.WriteLine("\t" + XmlNodeFullName(testNode) + ": " + testNode.InnerText);
+                if (diffExpected.ContainsKey(diff.Key))
+                {
+                    XmlNode expectedNode = diffExpected[diff.Key];
+                    output.WriteLine("Expected:");
+                    output.WriteLine("\t" + XmlNodeFullName(expectedNode) + ": " + expectedNode.InnerText);
+                }
+                else
+                {
+                    output.WriteLine("Expected:");
+                    output.WriteLine("\tnone");
+                }
+                output.WriteLine("");
+            }
+            foreach (KeyValuePair<string, XmlNode> diff in diffExpected)
+            {
+                if (!diffTest.ContainsKey(diff.Key))
+                {
+                    output.WriteLine("Test:");
+                    output.WriteLine("\tnone");
+                    output.WriteLine("Expected:");
+                    output.WriteLine("\t" + XmlNodeFullName(diff.Value) + ": " + diff.Value.InnerText);
+                }
+                output.WriteLine("");
+            }
+
+            return diffExpected.Count == 0 && diffTest.Count == 0;
+        }
+
+        private class XElementEqualityComparer : IEqualityComparer<XmlNode>
+        {
+            public bool Equals(XmlNode x, XmlNode y)
+            {
+                return x.OuterXml.Equals(y.OuterXml);
+            }
+            public int GetHashCode(XmlNode obj)
+            {
+                return 0;
+            }
+        }
+
+        private static bool IsLeaf(XmlNode node)
+        {
+            return !node.HasChildNodes || node.FirstChild.NodeType == XmlNodeType.Text;
+        }
+
+        private static string XmlNodeFullName(XmlNode node)
+        {
+            string fullName = "";
+            XmlNode n = node;
+            while (node != null && node.NodeType != XmlNodeType.Document)
+            {
+                fullName = node.Name + "." + fullName;
+                node = node.ParentNode;
+            }
+            return fullName;
+        }
+
+        public static XmlNodeList GetTestXmlNodes(string filename, bool raw, bool header, bool disasm, bool unwind, bool gc, bool sc)
+        {
+            R2RReader r2r = new R2RReader(filename);
+            IntPtr disassembler = CoreDisTools.GetDisasm(r2r.Machine);
+            XmlDumper dumper = new XmlDumper(r2r, null, raw, header, disasm, disassembler, unwind, gc, sc);
+            return dumper.GetXmlDocument().SelectNodes("//*");
+        }
+
+        public static XmlNodeList GetExpectedXmlNodes(string filename)
+        {
+            XmlDocument expectedXml = new XmlDocument();
+            expectedXml.Load(filename);
+            return expectedXml.SelectNodes("//*");
+        }
+    }
+}
