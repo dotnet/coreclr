24865aebd0dae46ccc6c2c0fdecc8a42d450605f
Amy Yu <amycmyu@gmail.com>
2018-07-06 17:27:04 -0700

    Generate readytorun binaries from source code, copy expected xml output files to executable's directory

--- tests/src/readytorun/r2rdump/BasicTests.cs
+++ tests/src/readytorun/r2rdump/BasicTests.cs
@@ -10,30 +10,30 @@ using Xunit.Abstractions;
 
 namespace R2RDumpTest
 {
-    public class BasicTest
+    public class BasicTests
     {
 		static int Main(string[] args)
 		{
 			Console.WriteLine("Starting the test");
 			
-			NoSections();
-			Simple();
+			TestNoSections();
+			TestHelloWorld();
 			
 			Console.WriteLine("PASSED");
 			return 100;
 		}
 
-        static void NoSections()
+		static void TestNoSections()
         {
-            R2RReader r2r = new R2RReader("c:/Code/r2rdump/peFiles/NoSections.ni.dll");
+            /*R2RReader r2r = new R2RReader("c:/Code/r2rdump/peFiles/NoSections.ni.dll");
             Assert.Equal(0, r2r.R2RHeader.Sections.Count);
-            Assert.Equal(0, r2r.R2RMethods.Count);
+            Assert.Equal(0, r2r.R2RMethods.Count);*/
         }
-
-        static void Simple()
+		
+        static void TestHelloWorld()
         {
-            List<XmlNode> testXmlNodes = TestHelpers.GetTestXmlNodes("c:/Code/r2rdump/peFiles/HelloWorld.ni.dll", true, true, true, true, true, true).Cast<XmlNode>().ToList();
-            List<XmlNode> expectedXmlNodes = TestHelpers.GetExpectedXmlNodes("c:/Code/r2rdump/out/HelloWorld.xml").Cast<XmlNode>().ToList();
+            List<XmlNode> testXmlNodes = TestHelpers.GetTestXmlNodes("HelloWorld.ni.dll", true, true, true, true, true, true).Cast<XmlNode>().ToList();
+            List<XmlNode> expectedXmlNodes = TestHelpers.GetExpectedXmlNodes("HelloWorld.xml").Cast<XmlNode>().ToList();
             bool identical = TestHelpers.XmlDiff(testXmlNodes, expectedXmlNodes);
             Assert.True(identical);
         }
---
+++ tests/src/readytorun/r2rdump/files/HelloWorld.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace HelloWorld
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
