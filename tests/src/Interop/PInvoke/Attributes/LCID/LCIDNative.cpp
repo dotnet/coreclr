2f040adfef2953e0184469fc0519d2cebb0e2714
Jiang Zeng (Pactera Technologies Inc) <v-jiazen@microsoft.com>
2018-07-24 01:53:52 -0700

    Add Interop/PInvoke/Attributes tests

---
+++ tests/src/Interop/PInvoke/Attributes/LCID/LCIDNative.cpp
@@ -0,0 +1,151 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+#include <stdio.h>
+#include <windows.h>
+#include <tchar.h>
+#include <xplatform.h>
+
+char* strManaged = "Managed\0String\0";
+int   lenstrManaged = 7; // the length of strManaged
+
+char* strReturn = "a\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0";
+char* strFalseReturn = "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0";
+
+char* strNative = " Native\0String\0";
+int lenstrNative = 7; //the len of strNative
+
+//Test Method1
+
+extern "C" LPSTR ReturnString()
+{
+    size_t lenstrReturn = strlen(strReturn);
+    LPSTR ret = (LPSTR)(CoTaskMemAlloc(sizeof(char)*(lenstrReturn+1)));
+    memset(ret,'\0',lenstrReturn+1);
+    strncpy_s(ret,lenstrReturn + 1,strReturn,1);
+    return ret;
+}
+
+extern "C" LPSTR ReturnFalseString()
+{
+    size_t lenstrFalseReturn = strlen(strFalseReturn);
+    LPSTR ret = (LPSTR)(CoTaskMemAlloc(sizeof(char)*(lenstrFalseReturn+1)));
+    memset(ret,'\0',lenstrFalseReturn+1);
+    strncpy_s(ret,lenstrFalseReturn + 1,strFalseReturn,1);
+    return ret;
+}
+
+//Test Method2
+extern "C" DLL_EXPORT LPSTR Marshal_InOut1(int lcid, LPSTR s)
+{
+    printf("LCID:%d\n\n",lcid);
+
+    //Check the Input
+    size_t len = strlen(s);
+    if((len != lenstrManaged)||(memcmp(s,strManaged,len)!=0))
+    {
+        printf("Error in Function Marshal_InOut(Native Client)\n");
+
+        //Expected
+        printf("Expected:");
+        for(int i = 0; i< lenstrManaged;++i)
+            putchar(*(((char *)strManaged)+i));
+        printf("\tThe length of Expected:%d\n",lenstrManaged);
+
+        //Actual
+        printf("Actual:");
+        for( int j = 0; j < len; ++j )
+            putchar(*(((char *)s) + j));
+        printf("\tThe length of Actual:%d\n",len);
+        return ReturnFalseString();
+    }
+
+    //In-Place Change
+    strncpy_s(s,len + 1,strNative,lenstrNative);
+
+    //Return
+    return ReturnString();
+}
+
+extern "C" DLL_EXPORT LPSTR Marshal_InOut2(LPSTR s,int lcid)
+{	
+    //Check the Input
+    size_t len = strlen(s);
+    if((len != lenstrManaged)||(memcmp(s,strManaged,len)!=0))
+    {
+        printf("Error in Function Marshal_InOut(Native Client)\n");
+
+        //Expected
+        printf("Expected:");
+        for(int i = 0; i< lenstrManaged;++i)
+            putchar(*(((char *)strManaged)+i));
+        printf("\tThe length of Expected:%d\n",lenstrManaged);
+
+        //Actual
+        printf("Actual:");
+        for( int j = 0; j < len; ++j )
+            putchar(*(((char *)s) + j));
+        printf("\tThe length of Actual:%d\n",len);
+        return ReturnFalseString();
+    }
+
+    //In-Place Change
+    strncpy_s(s,len + 1,strNative,lenstrNative);
+
+    SetLastError(1090);
+    //Return
+    return ReturnString();
+}
+
+extern "C" DLL_EXPORT HRESULT WINAPI Marshal_InOut4(LPSTR s, int lcid, LPSTR * retVal)
+{
+    //Check the Input
+    size_t len = strlen(s);
+    if((len != lenstrManaged)||(memcmp(s,strManaged,len)!=0))
+    {
+        printf("Error in Function MarshalPointer_InOut\n");
+
+        //Expected
+        printf("Expected:");
+        for(int i = 0; i< lenstrManaged;++i)
+            putchar(*(((char *)strManaged)+i));
+        printf("\tThe length of Expected:%d\n",lenstrManaged);
+
+        //Actual
+        printf("Actual:");
+        for( int j = 0; j < len; ++j)
+            putchar(*(((char *)s) + j));
+        printf("\tThe length of Actual:%d\n",len);
+
+        size_t lenstrFalseReturn = strlen(ReturnFalseString());
+        *retVal = (LPSTR)CoTaskMemAlloc(sizeof(char)*(lenstrFalseReturn+1));
+        memset(*retVal,'\0',lenstrFalseReturn+1);
+        strncpy_s(*retVal,lenstrFalseReturn,ReturnFalseString(),lenstrFalseReturn);
+
+        return S_FALSE;
+    }
+
+    //Allocate New
+    strncpy_s(s,len + 1,strNative,lenstrNative);
+
+    //Set the error code.
+    SetLastError(1090);
+
+    size_t lenstrReturn = strlen(strReturn);
+    *retVal = (LPSTR)(CoTaskMemAlloc(sizeof(char)*(lenstrReturn+1)));
+    memset(*retVal,'\0',lenstrReturn+1);
+    strncpy_s(*retVal,lenstrReturn + 1,strReturn,lenstrReturn);
+
+    return S_OK;
+}
+
+
+
+
+
+
+
+
+
+
---
+++ tests/src/Interop/PInvoke/Attributes/LCID/LCIDTest.cs
@@ -0,0 +1,131 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+//The testcase focus test the BStr with embed null string
+using System.Runtime.InteropServices;
+using System;
+using System.Reflection;
+using System.Text;
+using CoreFXTestLibrary;
+
+class LCIDTest
+{
+    [DllImport(@"LCIDNative.dll", EntryPoint = "Marshal_InOut1")]
+    [LCIDConversionAttribute(0)]
+    [return: MarshalAs(UnmanagedType.LPStr)]
+    private static extern StringBuilder MarshalStrB_InOut1([In, Out][MarshalAs(UnmanagedType.LPStr)]StringBuilder s);
+
+    [DllImport(@"LCIDNative.dll", EntryPoint = "Marshal_InOut2")]
+    [LCIDConversionAttribute(1)]
+    [return: MarshalAs(UnmanagedType.LPStr)]
+    private static extern StringBuilder MarshalStrB_InOut2([In, Out][MarshalAs(UnmanagedType.LPStr)]StringBuilder s);
+
+    [DllImport(@"LCIDNative.dll", EntryPoint = "Marshal_InOut2", SetLastError = true)]
+    [LCIDConversionAttribute(1)]
+    [return: MarshalAs(UnmanagedType.LPStr)]
+    private static extern StringBuilder MarshalStrB_InOut3([In, Out][MarshalAs(UnmanagedType.LPStr)]StringBuilder s);
+
+    [DllImport(@"LCIDNative.dll", EntryPoint = "Marshal_InOut4", PreserveSig = false, SetLastError = true)]
+    [LCIDConversionAttribute(1)]
+    [return: MarshalAs(UnmanagedType.LPStr)]
+    private static extern StringBuilder MarshalStrB_InOut4([In, Out][MarshalAs(UnmanagedType.LPStr)]StringBuilder s);
+
+    //LCID as first argument
+    static void Scenario1()
+    {
+        string strManaged = "Managed";
+        string strRet = "a";
+        StringBuilder expectedStrRet = new StringBuilder("a", 1);
+        string strNative = " Native";
+        StringBuilder strBNative = new StringBuilder(" Native", 7);
+
+        StringBuilder strPara1 = new StringBuilder(strManaged, strManaged.Length);
+        StringBuilder strRet1 = MarshalStrB_InOut1(strPara1);
+
+        Assert.AreEqual(expectedStrRet.ToString(), strRet1.ToString(), "Method MarshalStrB_InOut1[Managed Side],The Return string is wrong");
+        Assert.AreEqual(strBNative.ToString(), strPara1.ToString(), "Method MarshalStrB_InOut1[Managed Side],The Passed string is wrong");
+    }
+
+    //LCID as last argument
+    static void Scenario2()
+    {
+        string strManaged = "Managed";
+        string strRet = "a";
+        StringBuilder expectedStrRet = new StringBuilder("a", 1);
+        string strNative = " Native";
+        StringBuilder strBNative = new StringBuilder(" Native", 7);
+
+        StringBuilder strPara2 = new StringBuilder(strManaged, strManaged.Length);
+        StringBuilder strRet2 = MarshalStrB_InOut2(strPara2);
+
+        Assert.AreEqual(expectedStrRet.ToString(), strRet2.ToString(), "Method MarshalStrB_InOut2[Managed Side],The Return string is wrong");
+        Assert.AreEqual(strBNative.ToString(), strPara2.ToString(), "Method MarshalStrB_InOut2[Managed Side],The Passed string is wrong");
+
+        //Verify that error value is set.
+        int result = Marshal.GetLastWin32Error();
+        Assert.AreEqual(0, result, "MarshalStrB_InOut2: GetLasterror returned wrong error code");
+    }
+
+    //SetLastError =true
+    static void Scearnio3()
+    {
+        string strManaged = "Managed";
+        string strRet = "a";
+        StringBuilder expectedStrRet = new StringBuilder("a", 1);
+        string strNative = " Native";
+        StringBuilder strBNative = new StringBuilder(" Native", 7);
+
+        StringBuilder strPara3 = new StringBuilder(strManaged, strManaged.Length);
+        StringBuilder strRet3 = MarshalStrB_InOut3(strPara3);
+
+        Assert.AreEqual(expectedStrRet.ToString(), strRet3.ToString(), "Method MarshalStrB_InOut3[Managed Side],The Return string is wrong");
+        Assert.AreEqual(strBNative.ToString(), strPara3.ToString(), "Method MarshalStrB_InOut3[Managed Side],The Passed string is wrong");
+
+        //Verify that error value is set
+        int result = Marshal.GetLastWin32Error();
+        Assert.AreEqual(1090, result, "MarshalStrB_InOut3 : GetLasterror returned wrong error code");
+    }
+
+    //PreserveSig = false, SetLastError = true
+    static void Scenario4()
+    {
+        string strManaged = "Managed";
+        string strRet = "a";
+        StringBuilder expectedStrRet = new StringBuilder("a", 1);
+        string strNative = " Native";
+        StringBuilder strBNative = new StringBuilder(" Native", 7);
+
+        StringBuilder strPara4 = new StringBuilder(strManaged, strManaged.Length);
+        StringBuilder strRet4 = MarshalStrB_InOut4(strPara4);
+
+        Assert.AreEqual(expectedStrRet.ToString(), strRet4.ToString(), "Method MarshalStrB_InOut4[Managed Side],The Return string is wrong");
+        Assert.AreEqual(strBNative.ToString(), strPara4.ToString(), "Method MarshalStrB_InOut4[Managed Side],The Passed string is wrong");
+
+        //Verify that error value is set
+        int result = Marshal.GetLastWin32Error();
+        Assert.AreEqual(1090, result, "MarshalStrB_InOut4 : GetLasterror returned wrong error code");
+    }
+
+    public static int Main(string[] args)
+    {
+        try
+        {
+            //LCID as first argument
+            Scenario1();
+            //LCID as last argument
+            Scenario2();
+            //SetLastError =true
+            Scearnio3();
+            //PreserveSig = false, SetLastError = true
+            Scenario4();
+
+            return 100;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Test Failure: {e}");
+            return 101;
+        }
+    }
+}
