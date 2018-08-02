3b6824ffbc2b709b8412809c8d392bcc71d5cc98
Anipik <anirudhagnihotry098@gmail.com>
2018-08-02 15:10:04 -0700

    Build Corefx change

--- src/System.Private.CoreLib/src/System/Reflection/RuntimeLocalVariableInfo.cs
+++ src/System.Private.CoreLib/src/System/Reflection/RuntimeLocalVariableInfo.cs
@@ -8,33 +8,14 @@ namespace System.Reflection
 {
     internal class RuntimeLocalVariableInfo : LocalVariableInfo
     {
-        #region Private Data Members
         private RuntimeType _type;
         private int _localIndex;
         private bool _isPinned;
-        #endregion
 
-        #region Constructor
         protected RuntimeLocalVariableInfo() { }
-        #endregion
 
-        #region Object Overrides
-        public override string ToString()
-        {
-            string toString = LocalType.ToString() + " (" + LocalIndex + ")";
-
-            if (IsPinned)
-                toString += " (pinned)";
-
-            return toString;
-        }
-        #endregion
-
-        #region Public Members
-        public virtual Type LocalType { get { Debug.Assert(_type != null, "type must be set!"); return _type; } }
-        public virtual int LocalIndex => _localIndex;
-        public virtual bool IsPinned => _isPinned;
-        #endregion
+        public override Type LocalType { get { Debug.Assert(_type != null, "type must be set!"); return _type; } }
+        public override int LocalIndex => _localIndex;
+        public override bool IsPinned => _isPinned;
     }
 }
-
