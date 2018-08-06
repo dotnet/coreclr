0be8dca563a1912a9357a42c8f0523fe6d0fa034
Michelle McDaniel <adiaaida@gmail.com>
2018-08-06 13:36:58 -0700

    Add arch to throughput trigger

--- perf.groovy
+++ perf.groovy
@@ -410,7 +410,7 @@ parallel(
 
         if (isPR) {
             TriggerBuilder builder = TriggerBuilder.triggerOnPullRequest()
-            builder.setGithubContext("Linux ${arch} Perf Test Flow")
+            builder.setGithubContext("Linux Perf Test Flow")
             builder.triggerOnlyOnComment()
             builder.setCustomTriggerPhrase("(?i).*test\\W+linux\\W+perf\\W+flow.*")
             builder.triggerForBranch(branch)
@@ -607,7 +607,7 @@ def static getFullThroughputJobName(def project, def os, def arch, def isPR) {
 
         if (isPR) {
             TriggerBuilder builder = TriggerBuilder.triggerOnPullRequest()
-            builder.setGithubContext("Linux Throughput Perf Test Flow")
+            builder.setGithubContext("Linux ${arch} Throughput Perf Test Flow")
             builder.triggerOnlyOnComment()
             builder.setCustomTriggerPhrase("(?i).*test\\W+linux\\W+throughput\\W+flow.*")
             builder.triggerForBranch(branch)
