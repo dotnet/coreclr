4517bd6ff3c2aa1549ed3654cb45c1bcc6baebbe
Santiago Fernandez Madero <safern@microsoft.com>
2018-08-09 15:48:54 -0700

    Add TypeForwardedFrom to DayOfWeek for serialization

--- src/System.Private.CoreLib/shared/System/DayOfWeek.cs
+++ src/System.Private.CoreLib/shared/System/DayOfWeek.cs
@@ -13,6 +13,7 @@
 
 namespace System
 {
+    [System.Runtime.CompilerServices.TypeForwardedFrom("mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089")]
     public enum DayOfWeek
     {
         Sunday = 0,
