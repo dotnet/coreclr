329565fa64409b918957d9b15bb931564b6b30d2
José Rivero <jorive@microsoft.com>
2018-08-14 10:14:45 -0700

    Add an empty console application to measure startup of the runtime.

---
+++ tests/src/performance/Scenario/JitBench/Benchmarks/CscEmptyMainBenchmark.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JitBench
+{
+    class CscEmptyMainBenchmark : CscBenchmark
+    {
+        public CscEmptyMainBenchmark() : base("Csc_Empty_Main")
+        {
+        }
+
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+        protected override async Task SetupSourceToCompile(string intermediateOutputDir, string runtimeDirPath, bool useExistingSetup, ITestOutputHelper output)
+#pragma warning restore CS1998
+        {
+            string emptyMainDir = Path.Combine(intermediateOutputDir, "emptyMainSource");
+            const string emptyMainFileName = "Program.cs";
+            string emptyMainPath = Path.Combine(emptyMainDir, emptyMainFileName);
+            string systemPrivateCoreLibPath = Path.Combine(runtimeDirPath, "System.Private.CoreLib.dll");
+            string systemRuntimePath = Path.Combine(runtimeDirPath, "System.Runtime.dll");
+            string systemConsolePath = Path.Combine(runtimeDirPath, "System.Console.dll");
+            CommandLineArguments = $"{emptyMainFileName} /nostdlib /r:" + systemPrivateCoreLibPath + " /r:" + systemRuntimePath + " /r:" + systemConsolePath;
+            WorkingDirPath = emptyMainDir;
+            if(useExistingSetup)
+            {
+                return;
+            }
+
+            FileTasks.DeleteDirectory(emptyMainDir, output);
+            FileTasks.CreateDirectory(emptyMainDir, output);
+            File.WriteAllLines(emptyMainPath, new string[]
+            {
+                "using System;",
+                "public static class Program",
+                "{",
+                "    public static int Main(string[] args) => 0;",
+                "}"
+            });
+        }
+    }
+}
