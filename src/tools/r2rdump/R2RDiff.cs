b54d5768d13f0ab7ed1c72c74ac402db8ee57fe6
Tomas Rylek <trylek@microsoft.com>
2018-08-17 21:58:05 +0200

    Add comments to the new methods in R2RDiff

--- src/tools/r2rdump/R2RDiff.cs
+++ src/tools/r2rdump/R2RDiff.cs
@@ -11,12 +11,32 @@ using System.Text;
 
 namespace R2RDump
 {
+    /// <summary>
+    /// Helper class for diffing a pair of R2R images.
+    /// </summary>
     class R2RDiff
     {
+        /// <summary>
+        /// Left R2R image for the diff.
+        /// </summary>
         private readonly R2RReader _leftFile;
+       
+        /// <summary>
+        /// Right R2R image for the diff.
+        /// </summary>
         private readonly R2RReader _rightFile;
+
+        /// <summary>
+        /// Text writer to receive diff output.
+        /// </summary>
         private readonly TextWriter _writer;
 
+        /// <summary>
+        /// Store the left and right file and output writer.
+        /// </summary>
+        /// <param name="leftFile">Left R2R file</param>
+        /// <param name="rightFile">Right R2R file</param>
+        /// <param name="writer">Output writer to receive the diff</param>
         public R2RDiff(R2RReader leftFile, R2RReader rightFile, TextWriter writer)
         {
             _leftFile = leftFile;
@@ -24,6 +44,9 @@ namespace R2RDump
             _writer = writer;
         }
 
+        /// <summary>
+        /// Public API runs all available diff algorithms in sequence.
+        /// </summary>
         public void Run()
         {
             DiffTitle();
@@ -32,6 +55,9 @@ namespace R2RDump
             DiffR2RMethods();
         }
 
+        /// <summary>
+        /// Diff title shows the names of the files being compared and their lengths.
+        /// </summary>
         private void DiffTitle()
         {
             _writer.WriteLine($@"Left file:  {_leftFile.Filename} ({_leftFile.Image.Length} B)");
@@ -39,21 +65,36 @@ namespace R2RDump
             _writer.WriteLine();
         }
 
+        /// <summary>
+        /// Diff raw PE sections.
+        /// </summary>
         private void DiffPESections()
         {
             ShowDiff(GetPESectionMap(_leftFile), GetPESectionMap(_rightFile), "PE sections");
         }
 
+        /// <summary>
+        /// Diff R2R header sections.
+        /// </summary>
         private void DiffR2RSections()
         {
             ShowDiff(GetR2RSectionMap(_leftFile), GetR2RSectionMap(_rightFile), "R2R sections");
         }
 
+        /// <summary>
+        /// Diff the R2R method maps.
+        /// </summary>
         private void DiffR2RMethods()
         {
             ShowDiff(GetR2RMethodMap(_leftFile), GetR2RMethodMap(_rightFile), "R2R methods");
         }
 
+        /// <summary>
+        /// Show a difference summary between the sets of "left objects" and "right objects".
+        /// </summary>
+        /// <param name="leftObjects">Dictionary of left object sizes keyed by their names</param>
+        /// <param name="rightObjects">Dictionary of right object sizes keyed by their names</param>
+        /// <param name="diffName">Logical name of the diffing operation to display in the header line</param>
         private void ShowDiff(Dictionary<string, int> leftObjects, Dictionary<string, int> rightObjects, string diffName)
         {
             HashSet<string> allKeys = new HashSet<string>(leftObjects.Keys);
@@ -110,6 +151,11 @@ namespace R2RDump
             _writer.WriteLine();
         }
 
+        /// <summary>
+        /// Read the PE file section map for a given R2R image.
+        /// </summary>
+        /// <param name="reader">R2R image to scan</param>
+        /// <returns></returns>
         private Dictionary<string, int> GetPESectionMap(R2RReader reader)
         {
             Dictionary<string, int> sectionMap = new Dictionary<string, int>();
@@ -122,6 +168,11 @@ namespace R2RDump
             return sectionMap;
         }
 
+        /// <summary>
+        /// Read the R2R header section map for a given R2R image.
+        /// </summary>
+        /// <param name="reader">R2R image to scan</param>
+        /// <returns></returns>
         private Dictionary<string, int> GetR2RSectionMap(R2RReader reader)
         {
             Dictionary<string, int> sectionMap = new Dictionary<string, int>();
@@ -135,6 +186,11 @@ namespace R2RDump
             return sectionMap;
         }
 
+        /// <summary>
+        /// Read the R2R method map for a given R2R image.
+        /// </summary>
+        /// <param name="reader">R2R image to scan</param>
+        /// <returns></returns>
         private Dictionary<string, int> GetR2RMethodMap(R2RReader reader)
         {
             Dictionary<string, int> methodMap = new Dictionary<string, int>();
