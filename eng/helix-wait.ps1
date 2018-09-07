fd423900873e4b02d42771dd1a1edd4f268dd733
Jon Fortescue <jofortes@microsoft.com>
2018-09-07 05:44:38 -0700

    Test out some stuff

--- eng/helix-wait.ps1
+++ eng/helix-wait.ps1
@@ -1,10 +1,8 @@
 # Script for checking on Helix job periodically
 
 $finished = $false
-$timeout = $false
-$startTime = Get-Date
 
-while (-Not $finished -And -Not $timeout) {
+while (-Not $finished) {
     $workItems = Invoke-WebRequest -Headers @{"Accept"="application/json"} -Uri "https://helix.dot.net/api/2018-03-14/jobs/$env:HelixJobId/details?access_token=$env:HelixAccessToken" -UseBasicParsing |
     ConvertFrom-Json | Select -ExpandProperty "WorkItems"
     Write-Output "Waiting: $($workItems.Waiting); Running: $($workItems.Running)"
@@ -12,19 +10,7 @@ while (-Not $finished -And -Not $timeout) {
         $finished = $true
     }
 
-    $elapsedTime = New-Timespan $startTime $(Get-Date)
-    Write-Output "Elapsed Time: $elapsedTime"
-    if ($elapsedTime -gt (New-TimeSpan -Minutes 30)) {
-        $timeout = $true
-    }
-
     Start-Sleep -s 10
-}
-
-if ($timeout) {
-    Write-Host "##vso[task.logissue type=error;]Timed out while waiting for Helix job to complete"
-    Write-Host "##vso[task.complete result=Failed;]TEIMOUT"
-    exit 1
 }
 
 $aggregateStatus = Invoke-WebRequest -Headers @{"Accept"="application/json"} -Uri "https://helix.dot.net/api/2018-03-14/aggregate/workitems?groupBy=job.build&filter.name=$env:HelixJobId&access_token=$env:HelixAccessToken" -UseBasicParsing |
