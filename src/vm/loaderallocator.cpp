14426a1c939f417ca25313faa85b44de6c7b9ad6
fadimounir <fadim@microsoft.com>
2018-09-19 15:45:08 -0700

    Implementation of R2R vtable call thunks. These thunks will fetch the target code pointer from the vtable of the input thisPtr, and jump to that address. This is especially helpful with generics, since we can avoid a generic dictionary lookup cost for a simple vtable call. Overall, these thunks cause the CPU to have less branch mispredictions, and give a small performance boost to vtable calls.
    These stubs are under VirtualCallStubManager so that the managed debugger can handle stepping through them.

--- src/vm/loaderallocator.cpp
+++ src/vm/loaderallocator.cpp
@@ -1032,7 +1032,7 @@ void LoaderAllocator::ActivateManagedTracking()
 #define COLLECTIBLE_HIGH_FREQUENCY_HEAP_SIZE       (3 * GetOsPageSize())
 #define COLLECTIBLE_STUB_HEAP_SIZE                 GetOsPageSize()
 #define COLLECTIBLE_CODEHEAP_SIZE                  (7 * GetOsPageSize())
-#define COLLECTIBLE_VIRTUALSTUBDISPATCH_HEAP_SPACE (5 * GetOsPageSize())
+#define COLLECTIBLE_VIRTUALSTUBDISPATCH_HEAP_SPACE (6 * GetOsPageSize())
 
 void LoaderAllocator::Init(BaseDomain *pDomain, BYTE *pExecutableHeapMemory)
 {
