4a197ee6e2a8eaf3e72fe79386a49316146712b5
Jeremy Koritzinsky <jekoritz@microsoft.com>
2018-10-19 15:03:36 -0700

    Change error_t in types.h as well

--- tests/src/Interop/common/types.h
+++ tests/src/Interop/common/types.h
@@ -17,7 +17,7 @@ typedef char* LPSTR;
 typedef const char* LPCSTR;
 typedef void* FARPROC;
 typedef void* HMODULE;
-typedef unsigned error_t;
+typedef int error_t;
 typedef void* LPVOID;
 typedef unsigned char BYTE;
 typedef WCHAR OLECHAR;
