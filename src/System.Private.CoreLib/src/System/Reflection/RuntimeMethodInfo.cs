c44688d2fe5cf719ea3dc8828039ee7d2185d5f4
John Salem <josalem@microsoft.com>
2018-10-24 17:04:55 -0700

    Add IsCollectible to MemberInfo and MethodInfo

--- src/System.Private.CoreLib/src/System/Reflection/RuntimeMethodInfo.cs
+++ src/System.Private.CoreLib/src/System/Reflection/RuntimeMethodInfo.cs
@@ -529,6 +529,8 @@ namespace System.Reflection
             }
         }
 
+        public override bool IsCollectible => RuntimeMethodHandle.GetIsCollectible(new RuntimeMethodHandleInternal(m_handle));
+
         public override MethodInfo GetBaseDefinition()
         {
             if (!IsVirtual || IsStatic || m_declaringType == null || m_declaringType.IsInterface)
