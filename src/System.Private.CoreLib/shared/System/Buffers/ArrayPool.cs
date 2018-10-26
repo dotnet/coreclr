05d28ddc547cbf09b504a267bb802f3a7ba96535
Ben Adams <thundercat@illyriad.co.uk>
2018-10-26 19:33:54 +0100

    Fix property

--- src/System.Private.CoreLib/shared/System/Buffers/ArrayPool.cs
+++ src/System.Private.CoreLib/shared/System/Buffers/ArrayPool.cs
@@ -37,7 +37,7 @@ namespace System.Buffers
         /// optimized for very fast access speeds, at the expense of more memory consumption.
         /// The shared pool instance is created lazily on first access.
         /// </remarks>
-        public static ArrayPool<T> Shared { get; } = s_shared;
+        public static ArrayPool<T> Shared => s_shared;
 
         /// <summary>
         /// Creates a new <see cref="ArrayPool{T}"/> instance using default configuration options.
