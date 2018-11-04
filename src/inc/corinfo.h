0d675dd58a6104196e56fee881e5e32782faed8b
Tanner Gooding <tagoo@outlook.com>
2018-11-04 06:11:14 -0800

    Updating the importer to throw a NotImplementedException if it finds a mustExpand hwintrinsic that it can't expand

--- src/inc/corinfo.h
+++ src/inc/corinfo.h
@@ -642,6 +642,7 @@ enum CorInfoHelpFunc
 
     CORINFO_HELP_THROW_ARGUMENTEXCEPTION,           // throw ArgumentException
     CORINFO_HELP_THROW_ARGUMENTOUTOFRANGEEXCEPTION, // throw ArgumentOutOfRangeException
+    CORINFO_HELP_THROW_NOT_IMPLEMENTED,             // throw NotImplementedException
     CORINFO_HELP_THROW_PLATFORM_NOT_SUPPORTED,      // throw PlatformNotSupportedException
     CORINFO_HELP_THROW_TYPE_NOT_SUPPORTED,          // throw TypeNotSupportedException
 
