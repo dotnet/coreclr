c3829db67b6bb6053088e762d0043a1eafc0c60a
Egor Chesakov <Egor.Chesakov@microsoft.com>
2018-11-13 10:26:02 -0800

    Update comment regarding building xunit with specified version in tests/runtest.py

--- tests/runtest.py
+++ tests/runtest.py
@@ -791,7 +791,7 @@ def run_tests(host_os,
     #  1) git clone https://github.com/echesakovMSFT/xunit.git --branch UseConcurrentDictionaryInDependencyContextAssemblyCache --single-branch
     #  2) cd xunit
     #  3) git submodule update --init
-    #  4) powershell .\build.ps1
+    #  4) powershell .\build.ps1 -target packages -buildAssemblyVersion 2.4.1 -buildSemanticVersion 2.4.1-coreclr
     #
     # Then file "xunit\src\xunit.console\bin\Release\netcoreapp2.0\xunit.console.dll" was archived and uploaded to the clrjit blob storage.
     #
