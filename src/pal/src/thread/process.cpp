ef5aadaf30b941e5227db91b747af924f451ff9a
Oded Hanson <odhanson@microsoft.com>
2018-11-21 13:52:40 +0200

    Fixed compilation switch from __APPLE to __APPLE__

--- src/pal/src/thread/process.cpp
+++ src/pal/src/thread/process.cpp
@@ -2299,7 +2299,7 @@ PAL_GetTransportPipeName(
     // to 0. We expect that anyone else making the pipe name will also fail and thus will
     // also try to use 0 as the value.
     _ASSERTE(ret == TRUE || disambiguationKey == 0);
-#ifdef __APPLE
+#ifdef __APPLE__
     if (nullptr != applicationGroupId)
     {
         // Verify the length of the application group ID
