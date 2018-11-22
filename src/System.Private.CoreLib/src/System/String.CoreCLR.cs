e0168c7e4b5b9f993769c679943908c12b89fc5f
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2018-11-22 12:38:46 +0100

    Mark the String.Empty field as Intrinsic
    This is required so that the CPAOT compiler can recognize this field as a RyuJIT intrinsic.

--- src/System.Private.CoreLib/src/System/String.CoreCLR.cs
+++ src/System.Private.CoreLib/src/System/String.CoreCLR.cs
@@ -29,7 +29,7 @@ namespace System
         // We need to call the String constructor so that the compiler doesn't mark this as a literal.
         // Marking this as a literal would mean that it doesn't show up as a field which we can access 
         // from native.
-        public static readonly string Empty;
+        [Intrinsic] public static readonly string Empty;
 
         // Gets the character at a specified position.
         //
