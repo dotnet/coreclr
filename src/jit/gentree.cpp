5397e17b685c0d577bf776a21f8846879cfa6df5
Michal Strehovský <michals@microsoft.com>
2018-12-02 22:11:10 +0100

    CR feedback

--- src/jit/gentree.cpp
+++ src/jit/gentree.cpp
@@ -15162,7 +15162,7 @@ Compiler::TypeProducerKind Compiler::gtGetTypeProducerKind(GenTree* tree)
         bool                 isNonNull = false;
         CORINFO_CLASS_HANDLE clsHnd    = gtGetClassHandle(tree, &isExact, &isNonNull);
 
-        if (clsHnd != (CORINFO_CLASS_HANDLE) nullptr &&
+        if (clsHnd != NO_CLASS_HANDLE &&
             clsHnd == info.compCompHnd->getBuiltinClass(CLASSID_RUNTIME_TYPE))
         {
             return TPK_Other;
@@ -16374,7 +16374,7 @@ CORINFO_CLASS_HANDLE Compiler::gtGetClassHandle(GenTree* tree, bool* pIsExact, b
             if (intrinsic->gtIntrinsicId == CORINFO_INTRINSIC_Object_GetType)
             {
                 CORINFO_CLASS_HANDLE runtimeType = info.compCompHnd->getBuiltinClass(CLASSID_RUNTIME_TYPE);
-                assert(runtimeType != (CORINFO_CLASS_HANDLE) nullptr);
+                assert(runtimeType != NO_CLASS_HANDLE);
 
                 objClass    = runtimeType;
                 *pIsExact   = false;
@@ -16531,7 +16531,7 @@ CORINFO_CLASS_HANDLE Compiler::gtGetHelperCallClassHandle(GenTreeCall* call, boo
             const bool           helperResultNonNull = (helper == CORINFO_HELP_TYPEHANDLE_TO_RUNTIMETYPE);
             CORINFO_CLASS_HANDLE runtimeType         = info.compCompHnd->getBuiltinClass(CLASSID_RUNTIME_TYPE);
 
-            assert(runtimeType != (CORINFO_CLASS_HANDLE) nullptr);
+            assert(runtimeType != NO_CLASS_HANDLE);
 
             objClass    = runtimeType;
             *pIsNonNull = helperResultNonNull;
