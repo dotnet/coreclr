720d2a4589959ddee8a52f9ecfad4b9401186bfe
Morgan Brown <morganb@microsoft.com>
2018-12-11 15:55:47 -0800

    Fix signing command

---
+++ .dotnet/sdk/2.1.401/FSharp/RunFsc.cmd
@@ -0,0 +1,6 @@
+@echo off
+
+REM Copyright (c) .NET Foundation and contributors. All rights reserved.
+REM Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+"%~dp0..\..\..\dotnet" "%~dp0fsc.exe" %*
