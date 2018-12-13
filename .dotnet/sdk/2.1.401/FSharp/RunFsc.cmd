0571ef255a59eae28143c94ee88ad5cc983b8bb8
Morgan Brown <morganb@microsoft.com>
2018-12-12 17:49:02 -0800

    Remove accidental commits

--- .dotnet/sdk/2.1.401/FSharp/RunFsc.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-
-REM Copyright (c) .NET Foundation and contributors. All rights reserved.
-REM Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-"%~dp0..\..\..\dotnet" "%~dp0fsc.exe" %*
