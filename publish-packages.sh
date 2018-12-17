aeda0f87cd13af4b07e7e8c32233274d2d6351df
Sven Boemer <sbomer@gmail.com>
2018-12-17 15:18:56 -0800

    Remove run.sh from (clean/sync/(build/publish)-packages).sh

--- publish-packages.sh
+++ publish-packages.sh
@@ -22,7 +22,62 @@ usage()
 
 working_tree_root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-$working_tree_root/run.sh publish-packages -BuildOS $*
+buildArgs=
+unprocessedBuildArgs=
+
+# TODO: get rid of argument processing entirely once we remove the
+# uses of -Arg=Value style in buildpipeline.
+while :; do
+    if [ $# -le 0 ]; then
+        break
+    fi
+    case "$1" in
+        -\?|-h|--help)
+            usage
+            exit 1
+            ;;
+        -AzureAccount=*)
+            __Account=$(echo $1| cut -d'=' -f 2)
+            buildArgs="$buildArgs /p:CloudDropAccountName=$__Account"
+            ;;
+        -AzureToken=*)
+            __Token=$(echo $1| cut -d'=' -f 2)
+            buildArgs="$buildArgs /p:CloudDropAccessToken=$__Token"
+            ;;
+        -BuildType=*)
+            __Type=$(echo $1| cut -d'=' f 2)
+            buildArgs="$buildArgs /p:__BuildType=$__Type"
+            ;;
+        -BuildArch=*)
+            __Arch=$(echo $1| cut -d'=' -f 2)
+            buildArgs="$buildArgs /p:__BuildArch=$__Arch"
+            ;;
+        -Container=*)
+            __Container=$(echo $1| cut -d'=' -f 2)
+            buildArgs="$buildArgs /p:ContainerName=$__Container"
+            ;;
+        -distroRid=*)
+            __Rid=$(echo $1| cut -d'=' -f 2)
+            buildArgs="$buildArgs /p:__DistroRid=$__Rid"
+            ;;
+        -PublishPackages)
+            buildArgs="$buildArgs /p:__PublishPackages=true"
+            ;;
+        -PublishSymbols)
+            buildArgs="$buildArgs /p:__PublishSymbols=true"
+            ;;
+        -PublishTestNativeBins)
+            buildArgs="$buildArgs /p:PublishTestNativeBins=true"
+            ;;
+        --)
+            ;;
+        *)
+            unprocessedBuildArgs="$unprocessedBuildArgs $1"
+    esac
+    shift
+done
+
+$working_tree_root/dotnet.sh msbuild /nologo /verbosity:minimal /clp:Summary /p:__BuildOS=${OSName} ./src/publish.proj /flp:v=detailed\;LogFile=publish-packages.log /clp:v=detailed $buildArgrs $unprocessedBuildArgs
 if [ $? -ne 0 ]
 then
     echo "ERROR: An error occurred while publishing packages; see $working_tree_root/publish-packages.log for more details. There may have been networking problems, so please try again in a few minutes."
