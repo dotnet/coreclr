b6bd0428623cf0892880f097eddcbc4611b84777
Sven Boemer <sbomer@gmail.com>
2018-12-18 12:32:54 -0800

    Remove run.cmd from (clean/sync/publish-packages).cmd

--- publish-packages.cmd
+++ publish-packages.cmd
@@ -1,11 +1,43 @@
 @if not defined _echo @echo off
 setlocal EnableDelayedExpansion
 
+
+set "__args=%*"
+set processedArgs=
+set unprocessedArgs=
+set __MSBuildArgs=
+
+:Arg_Loop
+if "%1" == "" goto ArgsDone
 if /I [%1] == [-?] goto Usage
 if /I [%1] == [-help] goto Usage
 
-echo %~dp0run.cmd publish-packages %*
-call %~dp0run.cmd publish-packages %*
+REM CMD eats "=" on the argument list.
+if /i "%1" == "-AzureAccount"     (set processedArgs=!processedArgs! %1=%2&set __MSBuildArgs=!__MSBuildArgs! /p:CloudDropAccountName=%2&shift&shift&goto Arg_Loop)
+if /i "%1" == "-AzureToken"       (set processedArgs=!processedArgs! %1=%2&set __MSBuildArgs=!__MSBuildArgs! /p:CloudDropAccessToken=%2&shift&shift&goto Arg_Loop)
+if /i "%1" == "-BuildArch"        (set processedArgs=!processedArgs! %1=%2&set __MSBuildArgs=!__MSBuildArgs! /p:__BuildArch=%2&shift&shift&goto Arg_Loop)
+if /i "%1" == "-BuildType"        (set processedArgs=!processedArgs! %1=%2&set __MSBuildArgs=!__MSBuildArgs! /p:__BuildType=%2&shift&shift&goto Arg_Loop)
+if /i "%1" == "-Container"        (set processedArgs=!processedArgs! %1=%2&set __MSBuildArgs=!__MSBuildArgs! /p:ContainerName=%2&shift&shift&goto Arg_Loop)
+
+if /i "%1" == "-PublishPackages"  (set processedArgs=!processedArgs! %1&set __MSBuildArgs=!__MSBuildArgs! /p:__PublishPackages=true&shift&goto Arg_Loop)
+if /i "%1" == "-PublishSymbols"   (set processedArgs=!processedArgs! %1&set __MSBuildArgs=!__MSBuildArgs! /p:__PublishSymbols=true&shift&goto Arg_Loop)
+
+if /i "%1" == "--"                (set processedArgs=!processedArgs! %1&shift)
+
+REM handle any unprocessed arguments, assumed to go only after the processed arguments above
+if [!processedArgs!]==[] (
+   set unprocessedArgs=%__args%
+) else (
+   set unprocessedArgs=%__args%
+   for %%t in (!processedArgs!) do (
+   REM strip out already-processed arguments from unprocessedArgs
+   set unprocessedArgs=!unprocessedArgs:*%%t=!
+   )
+)
+
+:ArgsDone
+
+call %~dp0msbuild.cmd /nologo /verbosity:minimal /clp:Summary /nodeReuse:false /p:__BuildOS=Windows_NT .\src\publish.proj /flp:v=detailed;LogFile=publish-packages.log /clp:v=detailed %__MSBuildArgs% %unprocessedArgs%
 @exit /b %ERRORLEVEL%
 
 :Usage
--- sync.cmd
+++ sync.cmd
@@ -1,12 +1,48 @@
 @if not defined _echo @echo off
 setlocal EnableDelayedExpansion
 
+set "__args=%*"
+set processedArgs=
+set unprocessedArgs=
+set __MSBuildArgs=
+
+REM If no processed arguments are specified, -p is implied.
+if "%1" == ""      (set __MSBuildArgs=!__MSBuildArgs! .\build.proj /p:RestoreDuringBuild=true /t:Sync)
+if /i [%1] == [--] (set __MSBuildArgs=!__MSBuildArgs! .\build.proj /p:RestoreDuringBuild=true /t:Sync)
+
+:Arg_Loop
+if "%1" == "" goto ArgsDone
 if /I [%1] == [-?] goto Usage
 if /I [%1] == [-help] goto Usage
 
-@if [%1]==[] set __args=-p
+if /i [%1] == [-p]               (set processedArgs=!processedArgs! %1&set __MSBuildArgs=!__MSBuildArgs! .\build.proj /p:RestoreDuringBuild=true /t:Sync&shift&goto Arg_Loop)
+if /i [%1] == [-ab]              (set processedArgs=!processedArgs! %1&set __MSBuildArgs=!__MSBuildArgs! .\src\syncAzure.proj&shift&goto Arg_Loop)
 
- @call %~dp0run.cmd sync %__args% %*
+REM CMD eats "=" on the argument list.
+if /i "%1" == "-AzureAccount"    (set processedArgs=!processedArgs! %1=%2&set __MSBuildArgs=!__MSBuildArgs! /p:CloudDropAccountName=%2&shift&shift&goto Arg_Loop)
+if /i "%1" == "-AzureToken"      (set processedArgs=!processedArgs! %1=%2&set __MSBuildArgs=!__MSBuildArgs! /p:CloudDropAccessToken=%2&shift&shift&goto Arg_Loop)
+if /i "%1" == "-BuildMajor"      (set processedArgs=!processedArgs! %1=%2&set __MSBuildArgs=!__MSBuildArgs! /p:BuildNumberMajor=%2&shift&shift&goto Arg_Loop)
+if /i "%1" == "-BuildMinor"      (set processedArgs=!processedArgs! %1=%2&set __MSBuildArgs=!__MSBuildArgs! /p:BuildNumberMinor=%2&shift&shift&goto Arg_Loop)
+if /i "%1" == "-Container"       (set processedArgs=!processedArgs! %1=%2&set __MSBuildArgs=!__MSBuildArgs! /p:ContainerName=%2&shift&shift&goto Arg_Loop)
+if /i "%1" == "-BlobNamePrefix"  (set processedArgs=!processedArgs! %1=%2&set __MSBuildArgs=!__MSBuildArgs! /p:__BlobNamePrefix=%2&shift&shift&goto Arg_Loop)
+if /i "%1" == "-RuntimeId"       (set processedArgs=!processedArgs! %1=%2&set __MSBuildArgs=!__MSBuildArgs! /p:RuntimeId=%2&shift&shift&goto Arg_Loop)
+if /i "%1" == "--"               (set processedArgs=!processedArgs! %1&shift)
+
+REM handle any unprocessed arguments, assumed to go only after the processed arguments above
+if [!processedArgs!]==[] (
+   set unprocessedArgs=%__args%
+) else (
+   set unprocessedArgs=%__args%
+   for %%t in (!processedArgs!) do (
+   REM strip out already-processed arguments from unprocessedArgs
+   set unprocessedArgs=!unprocessedArgs:*%%t=!
+   )
+)
+
+:ArgsDone
+
+
+@call %~dp0msbuild.cmd /nologo /verbosity:minimal /clp:Summary /nodeReuse:false /flp:v=detailed;LogFile=sync.log %__MSBuildArgs% %unprocessedArgs%
 @exit /b %ERRORLEVEL%
 
 :Usage
