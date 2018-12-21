64a85d93a9c2b42cd921250fe9599a075ee8e561
Tanner Gooding <tagoo@outlook.com>
2018-12-20 16:51:13 -0800

    Fixing `SIMDIntrinsicBitwiseAndNot` to swap the operands for XARCH

--- src/jit/simd.cpp
+++ src/jit/simd.cpp
@@ -2882,6 +2882,18 @@ GenTree* Compiler::impSIMDIntrinsic(OPCODE                opcode,
             op2 = impSIMDPopStack(simdType);
             op1 = impSIMDPopStack(simdType, instMethod);
 
+#ifdef _TARGET_XARCH_
+            if (simdIntrinsicID == SIMDIntrinsicBitwiseAndNot)
+            {
+                // XARCH implements SIMDIntrinsicBitwiseAndNot as ~op1 & op2, while the
+                // software implementation does op1 & ~op2, so we need to swap the operands
+
+                GenTree* tmp = op2;
+                op2          = op1;
+                op1          = tmp;
+            }
+#endif // _TARGET_XARCH_
+
             simdTree = gtNewSIMDNode(simdType, op1, op2, simdIntrinsicID, baseType, size);
             retVal   = simdTree;
         }
