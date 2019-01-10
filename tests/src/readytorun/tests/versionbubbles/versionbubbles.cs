1a423c95d4cac4349bc3ad4f0f0a7389ab8f3d6f
Andon Andonov <andon.andonov@microsoft.com>
2019-01-10 10:57:32 -0800

    Streamline Version Bubble test

--- tests/src/readytorun/tests/versionbubbles/versionbubbles.cs
+++ tests/src/readytorun/tests/versionbubbles/versionbubbles.cs
@@ -9,28 +9,23 @@ public class Program
 
     public static int RunTest()
     {
+
         Helper helper = new Helper();
         string lastMethodName = String.Empty;
-        try
-        {
-            lastMethodName = helper.GetLastMethodName();
-        }
-        catch (System.MissingMethodException e)
-        {
-            if((System.Environment.GetEnvironmentVariable("LargeVersionBubble") != null))
-            {
-                // Cross-Assembly inlining is only allowed in multi-module version bubbles
-                Console.WriteLine("FAIL");
-                return 101;
-            }
-            else
-            {
-                // The missing method is expected in the default crossgen case (i.e. no large version bubble)
-                Console.WriteLine("PASS");
-                return 100;
-            }
-        }
 
+        lastMethodName = helper.GetLastMethodName();
+
+        if((System.Environment.GetEnvironmentVariable("LargeVersionBubble") == null))
+        {
+            // Cross-Assembly inlining is only allowed in multi-module version bubbles
+            Console.WriteLine("Large Version Bubble is disabled.");
+            Console.WriteLine("PASS");
+            return 100;
+        }
+        
+        Console.WriteLine("Large Version Bubble is enabled.");
+        // Helper returns the name of the method in the last stack frame
+        // Check to see if the method has been inlined cross-module
         if (lastMethodName != "GetLastMethodName")
         {
             // method in helper.cs has been inlined
