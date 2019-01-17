f55d39071677a23fb811bfbb2707a95ca6c57606
Tanner Gooding <tagoo@outlook.com>
2019-01-17 10:30:29 -0800

    Fixing the double/float formatters to handle +0.0 and -0.0

--- src/System.Private.CoreLib/shared/System/Number.Formatting.cs
+++ src/System.Private.CoreLib/shared/System/Number.Formatting.cs
@@ -404,7 +404,7 @@ namespace System
 
                 return number.IsNegative ? info.NegativeInfinitySymbol : info.PositiveInfinitySymbol;
             }
-            else if ((value != 0.0) && !Grisu3.RunDouble(value, digits, ref number))
+            else if ((value == 0.0) || !Grisu3.RunDouble(value, digits, ref number))
             {
                 Dragon4Double(value, digits, ref number);
             }
@@ -468,7 +468,7 @@ namespace System
 
                 return number.IsNegative ? info.NegativeInfinitySymbol : info.PositiveInfinitySymbol;
             }
-            else if ((value != 0.0f) && !Grisu3.RunSingle(value, digits, ref number))
+            else if ((value == 0.0f) || !Grisu3.RunSingle(value, digits, ref number))
             {
                 Dragon4Single(value, digits, ref number);
             }
