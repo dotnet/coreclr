0c4b8c3e3fa9e253f61dd14fa7d9d0fb9bc54c70
Marek Safar <marek.safar@gmail.com>
2019-01-28 10:18:12 +0100

    Ignore bestFit/throwOnUnmappableChar on Unix

--- src/System.Private.CoreLib/shared/System/Runtime/InteropServices/Marshal.Unix.cs
+++ src/System.Private.CoreLib/shared/System/Runtime/InteropServices/Marshal.Unix.cs
@@ -10,15 +10,16 @@ namespace System.Runtime.InteropServices
     {
         private static bool IsWin32Atom(IntPtr ptr) => false;
 
-        internal static unsafe int StringToAnsiString(string s, byte* buffer, int wideCharLen)
+        internal static unsafe int StringToAnsiString(string s, byte* buffer, int wideCharLen, bool bestFit = false, bool throwOnUnmappableChar = false)
         {
             int convertedBytes;
 
             fixed (char* pChar = s)
             {
                 convertedBytes = Encoding.UTF8.GetBytes(pChar, s.Length, buffer, wideCharLen);
-                buffer[convertedBytes] = 0;
             }
+
+            buffer[convertedBytes] = 0;
 
             return convertedBytes;
         }
