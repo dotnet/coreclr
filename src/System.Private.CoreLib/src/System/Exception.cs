9b23bbd8c1bb78e8a5b0575effbf9583276aec18
Aaron Robinson <arobins@microsoft.com>
2019-01-30 14:18:36 -0800

    Stop collecting source line information during a stacktrace for the Exception.Source property.

--- src/System.Private.CoreLib/src/System/Exception.cs
+++ src/System.Private.CoreLib/src/System/Exception.cs
@@ -337,7 +337,7 @@ namespace System
             {
                 if (_source == null)
                 {
-                    StackTrace st = new StackTrace(this, true);
+                    StackTrace st = new StackTrace(this, false);
                     if (st.FrameCount > 0)
                     {
                         StackFrame sf = st.GetFrame(0);
