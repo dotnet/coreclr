39290973b37aa8113a11b72b595991df5b90c2a9
Marek Safar <marek.safar@gmail.com>
2019-02-05 00:00:35 +0100

    Move WaitHelperNative to WaitHandle

--- src/System.Private.CoreLib/src/System/Threading/SynchronizationContext.CoreCLR.cs
+++ src/System.Private.CoreLib/src/System/Threading/SynchronizationContext.CoreCLR.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Runtime.CompilerServices;
-
 namespace System.Threading
 {
     public partial class SynchronizationContext
@@ -12,11 +10,5 @@ namespace System.Threading
         {
             return syncContext.Wait(waitHandles, waitAll, millisecondsTimeout);
         }
-
-        // Static helper to which the above method can delegate to in order to get the default
-        // COM behavior.
-        [MethodImplAttribute(MethodImplOptions.InternalCall)]
-        private static extern int WaitHelperNative(IntPtr[] waitHandles, bool waitAll, int millisecondsTimeout);
-
     }
 }
