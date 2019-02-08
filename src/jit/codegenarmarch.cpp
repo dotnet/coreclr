824f6eb89630f2f1ad49bc72bc6773dad06f140a
jashook <jashoo@microsoft.com>
2019-02-08 13:37:02 -0800

    Remove incorrect assert

--- src/jit/codegenarmarch.cpp
+++ src/jit/codegenarmarch.cpp
@@ -2717,8 +2717,6 @@ void CodeGen::genJmpMethod(GenTree* jmp)
         // In case of a jmp call to a vararg method ensure only integer registers are passed.
         if (compiler->info.compIsVarArgs)
         {
-            assert((genRegMask(argReg) & RBM_ARG_REGS) != RBM_NONE);
-
             fixedIntArgMask |= genRegMask(argReg);
 
             if (compiler->lvaIsMultiregStruct(varDsc, compiler->info.compIsVarArgs))
