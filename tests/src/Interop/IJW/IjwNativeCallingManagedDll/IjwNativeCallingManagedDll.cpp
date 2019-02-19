0eca5bc4c2e233a908d08837fd6d095d4c853b0a
Jeremy Koritzinsky <jekoritz@microsoft.com>
2019-02-19 14:33:00 -0800

    Add test for InMemoryAssemblyLoader.LoadInMemoryAssembly.

--- tests/src/Interop/IJW/IjwNativeCallingManagedDll/IjwNativeCallingManagedDll.cpp
+++ tests/src/Interop/IJW/IjwNativeCallingManagedDll/IjwNativeCallingManagedDll.cpp
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
+#include "platformdefines.h"
 
 #pragma managed
 int ManagedCallee()
@@ -12,6 +13,11 @@ int ManagedCallee()
 int NativeFunction()
 {
     return ManagedCallee();
+}
+
+extern "C" DLL_EXPORT int STDMETHODCALLTYPE NativeEntryPoint()
+{
+    return NativeFunction();
 }
 
 #pragma managed
---
+++ tests/src/Interop/IJW/LoadIjwFromModuleHandle/LoadIjwFromModuleHandle.cs
@@ -0,0 +1,57 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Internal.Runtime.InteropServices;
+using TestLibrary;
+
+namespace NativeCallingManaged
+{
+    class NativeCallingManaged
+    {
+        unsafe static int Main(string[] args)
+        {
+            try
+            {
+
+                HostPolicyMock.Initialize(Environment.CurrentDirectory, null);
+
+                // Load our fake mscoree to prevent desktop from loading.
+                NativeLibrary.Load(Path.Combine(Environment.CurrentDirectory, "mscoree.dll"));
+
+                string ijwModulePath = Path.Combine(Environment.CurrentDirectory, "IjwNativeCallingManagedDll.dll");
+                IntPtr ijwNativeHandle = NativeLibrary.Load(ijwModulePath);
+
+                using (HostPolicyMock.Mock_corehost_resolve_component_dependencies(
+                    0,
+                    ijwModulePath,
+                    string.Empty,
+                    string.Empty))
+                fixed (char* path = ijwModulePath)
+                {
+                    InMemoryAssemblyLoader.LoadInMemoryAssembly(ijwNativeHandle, (IntPtr)path);
+                }
+                
+                NativeEntryPointDelegate nativeEntryPoint = Marshal.GetDelegateForFunctionPointer<NativeEntryPointDelegate>(NativeLibrary.GetExport(ijwNativeHandle, "NativeEntryPoint"));
+
+                Assert.AreEqual(100, nativeEntryPoint());
+                
+                return 100;
+            }
+            catch (Exception ex)
+            {
+                Internal.Console.WriteLine(ex.ToString());
+
+                return 101;
+            }
+        }
+
+        [UnmanagedFunctionPointer(CallingConvention.Winapi)]
+        delegate int NativeEntryPointDelegate();
+
+    }
+}
