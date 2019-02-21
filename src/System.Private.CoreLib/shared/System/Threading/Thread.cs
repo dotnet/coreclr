8a249d5f5f5d12ac549beabc85149a2923ba8347
Filip Navara <filip.navara@gmail.com>
2019-02-21 23:16:22 +0100

    Port back CoreRT changes needed for shared code

--- src/System.Private.CoreLib/shared/System/Threading/Thread.cs
+++ src/System.Private.CoreLib/shared/System/Threading/Thread.cs
@@ -18,6 +18,7 @@ namespace System.Threading
         private static AsyncLocal<IPrincipal> s_asyncLocalPrincipal;
 
         public Thread(ThreadStart start)
+            : this()
         {
             if (start == null)
             {
@@ -28,6 +29,7 @@ namespace System.Threading
         }
 
         public Thread(ThreadStart start, int maxStackSize)
+            : this()
         {
             if (start == null)
             {
@@ -42,6 +44,7 @@ namespace System.Threading
         }
 
         public Thread(ParameterizedThreadStart start)
+            : this()
         {
             if (start == null)
             {
@@ -52,6 +55,7 @@ namespace System.Threading
         }
 
         public Thread(ParameterizedThreadStart start, int maxStackSize)
+            : this()
         {
             if (start == null)
             {
