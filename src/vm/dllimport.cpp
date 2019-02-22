bd5c462a9fcb20e379446df59d29ed001ed3ebad
Jeremy Koritzinsky <jekoritz@microsoft.com>
2019-02-22 15:00:59 -0800

    First pass of adding support for copy-constructors when marshalling.

--- src/vm/dllimport.cpp
+++ src/vm/dllimport.cpp
@@ -4139,6 +4139,10 @@ static void CreateNDirectStubWorker(StubState*         pss,
                     COMPlusThrow(kMarshalDirectiveException, IDS_EE_NDIRECT_BADNATL_THISCALL);
             }
 
+            if (info.GetMarshalType() == MarshalInfo::MARSHAL_TYPE_BLITTABLEVALUECLASSWITHCOPYCTOR)
+            {
+                fHasCopyCtorArgs = true;
+            }
 
             argidx++;
         }
