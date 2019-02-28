4372d924d3da950a8dd6c8e7a00eb0c69024b362
Steve MacLean <Steve.MacLean@microsoft.com>
2019-02-28 18:43:45 -0500

    Remove spurious setup_vs_tools.cmd calls

--- dotnet.cmd
+++ dotnet.cmd
@@ -3,11 +3,6 @@ setlocal
 
 set "__ProjectDir=%~dp0"
 
-call "%__ProjectDir%"\setup_vs_tools.cmd
-
-REM setup_vs_tools.cmd will correctly echo error message.
-if NOT '%ERRORLEVEL%' == '0' exit /b 1
-
 :: Clear the 'Platform' env variable for this session, as it's a per-project setting within the build, and
 :: misleading value (such as 'MCD' in HP PCs) may lead to build breakage (issue: #69).
 set Platform=
