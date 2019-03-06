965b00243e5037e98dfca288a1762303741bea0d
Grant Dickinson <grant@k2.com>
2019-03-05 18:54:28 -0800

    Make existing implementations consistent with new

--- src/System.Private.CoreLib/shared/System/MemoryExtensions.Trim.cs
+++ src/System.Private.CoreLib/shared/System/MemoryExtensions.Trim.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Diagnostics;
+using System.Runtime.CompilerServices;
 
 namespace System
 {
@@ -503,64 +504,23 @@ namespace System
         {
             Span<char> span = memory.Span;
 
-            int start = 0;
-            for (; start < span.Length; start++)
-            {
-                if (!char.IsWhiteSpace(span[start]))
-                {
-                    break;
-                }
-            }
+            int start = ClampStart(span);
+            int length = ClampEnd(span, start);
 
-            int end = span.Length - 1;
-            for (; end >= start; end--)
-            {
-                if (!char.IsWhiteSpace(span[end]))
-                {
-                    break;
-                }
-            }
-
-            return memory.Slice(start, end - start + 1);
+            return memory.Slice(start, length);
         }
 
         /// <summary>
         /// Removes all leading white-space characters from the memory.
         /// </summary>
         public static Memory<char> TrimStart(this Memory<char> memory)
-        {
-            Span<char> span = memory.Span;
-
-            int start = 0;
-            for (; start < span.Length; start++)
-            {
-                if (!char.IsWhiteSpace(span[start]))
-                {
-                    break;
-                }
-            }
-
-            return memory.Slice(start);
-        }
+            => memory.Slice(ClampStart(memory.Span));
 
         /// <summary>
         /// Removes all trailing white-space characters from the memory.
         /// </summary>
         public static Memory<char> TrimEnd(this Memory<char> memory)
-        {
-            Span<char> span = memory.Span;
-
-            int end = span.Length - 1;
-            for (; end >= 0; end--)
-            {
-                if (!char.IsWhiteSpace(span[end]))
-                {
-                    break;
-                }
-            }
-
-            return memory.Slice(0, end + 1);
-        }
+            => memory.Slice(0, ClampEnd(memory.Span, 0));
 
         /// <summary>
         /// Removes all leading and trailing white-space characters from the memory.
@@ -569,124 +529,46 @@ namespace System
         {
             ReadOnlySpan<char> span = memory.Span;
 
-            int start = 0;
-            for (; start < span.Length; start++)
-            {
-                if (!char.IsWhiteSpace(span[start]))
-                {
-                    break;
-                }
-            }
+            int start = ClampStart(span);
+            int length = ClampEnd(span, start);
 
-            int end = span.Length - 1;
-            for (; end >= start; end--)
-            {
-                if (!char.IsWhiteSpace(span[end]))
-                {
-                    break;
-                }
-            }
-
-            return memory.Slice(start, end - start + 1);
+            return memory.Slice(start, length);
         }
 
         /// <summary>
         /// Removes all leading white-space characters from the memory.
         /// </summary>
         public static ReadOnlyMemory<char> TrimStart(this ReadOnlyMemory<char> memory)
-        {
-            ReadOnlySpan<char> span = memory.Span;
-
-            int start = 0;
-            for (; start < span.Length; start++)
-            {
-                if (!char.IsWhiteSpace(span[start]))
-                {
-                    break;
-                }
-            }
-
-            return memory.Slice(start);
-        }
+            => memory.Slice(ClampStart(memory.Span));
 
         /// <summary>
         /// Removes all trailing white-space characters from the memory.
         /// </summary>
         public static ReadOnlyMemory<char> TrimEnd(this ReadOnlyMemory<char> memory)
-        {
-            ReadOnlySpan<char> span = memory.Span;
-
-            int end = span.Length - 1;
-            for (; end >= 0; end--)
-            {
-                if (!char.IsWhiteSpace(span[end]))
-                {
-                    break;
-                }
-            }
-
-            return memory.Slice(0, end + 1);
-        }
+            => memory.Slice(0, ClampEnd(memory.Span, 0));
 
         /// <summary>
         /// Removes all leading and trailing white-space characters from the span.
         /// </summary>
         public static ReadOnlySpan<char> Trim(this ReadOnlySpan<char> span)
         {
-            int start = 0;
-            for (; start < span.Length; start++)
-            {
-                if (!char.IsWhiteSpace(span[start]))
-                {
-                    break;
-                }
-            }
+            int start = ClampStart(span);
+            int length = ClampEnd(span, start);
 
-            int end = span.Length - 1;
-            for (; end >= start; end--)
-            {
-                if (!char.IsWhiteSpace(span[end]))
-                {
-                    break;
-                }
-            }
-
-            return span.Slice(start, end - start + 1);
+            return span.Slice(start, length);
         }
 
         /// <summary>
         /// Removes all leading white-space characters from the span.
         /// </summary>
         public static ReadOnlySpan<char> TrimStart(this ReadOnlySpan<char> span)
-        {
-            int start = 0;
-            for (; start < span.Length; start++)
-            {
-                if (!char.IsWhiteSpace(span[start]))
-                {
-                    break;
-                }
-            }
-
-            return span.Slice(start);
-        }
+            => span.Slice(ClampStart(span));
 
         /// <summary>
         /// Removes all trailing white-space characters from the span.
         /// </summary>
         public static ReadOnlySpan<char> TrimEnd(this ReadOnlySpan<char> span)
-        {
-            int end = span.Length - 1;
-            for (; end >= 0; end--)
-            {
-                if (!char.IsWhiteSpace(span[end]))
-                {
-                    break;
-                }
-            }
-
-            return span.Slice(0, end + 1);
-        }
+            => span.Slice(0, ClampEnd(span, 0));
 
         /// <summary>
         /// Removes all leading and trailing occurrences of a specified character.
@@ -695,25 +577,10 @@ namespace System
         /// <param name="trimChar">The specified character to look for and remove.</param>
         public static ReadOnlySpan<char> Trim(this ReadOnlySpan<char> span, char trimChar)
         {
-            int start = 0;
-            for (; start < span.Length; start++)
-            {
-                if (span[start] != trimChar)
-                {
-                    break;
-                }
-            }
+            int start = ClampStart(span, trimChar);
+            int length = ClampEnd(span, start, trimChar);
 
-            int end = span.Length - 1;
-            for (; end >= start; end--)
-            {
-                if (span[end] != trimChar)
-                {
-                    break;
-                }
-            }
-
-            return span.Slice(start, end - start + 1);
+            return span.Slice(start, length);
         }
 
         /// <summary>
@@ -722,18 +589,7 @@ namespace System
         /// <param name="span">The source span from which the character is removed.</param>
         /// <param name="trimChar">The specified character to look for and remove.</param>
         public static ReadOnlySpan<char> TrimStart(this ReadOnlySpan<char> span, char trimChar)
-        {
-            int start = 0;
-            for (; start < span.Length; start++)
-            {
-                if (span[start] != trimChar)
-                {
-                    break;
-                }
-            }
-
-            return span.Slice(start);
-        }
+            => span.Slice(ClampStart(span, trimChar));
 
         /// <summary>
         /// Removes all trailing occurrences of a specified character.
@@ -741,18 +597,7 @@ namespace System
         /// <param name="span">The source span from which the character is removed.</param>
         /// <param name="trimChar">The specified character to look for and remove.</param>
         public static ReadOnlySpan<char> TrimEnd(this ReadOnlySpan<char> span, char trimChar)
-        {
-            int end = span.Length - 1;
-            for (; end >= 0; end--)
-            {
-                if (span[end] != trimChar)
-                {
-                    break;
-                }
-            }
-
-            return span.Slice(0, end + 1);
-        }
+            => span.Slice(0, ClampEnd(span, 0, trimChar));
 
         /// <summary>
         /// Removes all leading and trailing occurrences of a set of characters specified
@@ -763,7 +608,15 @@ namespace System
         /// <remarks>If <paramref name="trimChars"/> is empty, white-space characters are removed instead.</remarks>
         public static ReadOnlySpan<char> Trim(this ReadOnlySpan<char> span, ReadOnlySpan<char> trimChars)
         {
-            return span.TrimStart(trimChars).TrimEnd(trimChars);
+            if (trimChars.IsEmpty)
+            {
+                return span.Trim();
+            }
+
+            int start = ClampStart(span, trimChars);
+            int length = ClampEnd(span, start, trimChars);
+
+            return span.Slice(start, length);
         }
 
         /// <summary>
@@ -780,23 +633,7 @@ namespace System
                 return span.TrimStart();
             }
 
-            int start = 0;
-            for (; start < span.Length; start++)
-            {
-                for (int i = 0; i < trimChars.Length; i++)
-                {
-                    if (span[start] == trimChars[i])
-                    {
-                        goto Next;
-                    }
-                }
-
-                break;
-            Next:
-                ;
-            }
-
-            return span.Slice(start);
+            return span.Slice(ClampStart(span, trimChars));
         }
 
         /// <summary>
@@ -813,23 +650,7 @@ namespace System
                 return span.TrimEnd();
             }
 
-            int end = span.Length - 1;
-            for (; end >= 0; end--)
-            {
-                for (int i = 0; i < trimChars.Length; i++)
-                {
-                    if (span[end] == trimChars[i])
-                    {
-                        goto Next;
-                    }
-                }
-
-                break;
-            Next:
-                ;
-            }
-
-            return span.Slice(0, end + 1);
+            return span.Slice(0, ClampEnd(span, 0, trimChars));
         }
 
         /// <summary>
@@ -837,7 +658,33 @@ namespace System
         /// </summary>
         public static Span<char> Trim(this Span<char> span)
         {
+            int start = ClampStart(span);
+            int length = ClampEnd(span, start);
+
+            return span.Slice(start, length);
+        }
+
+        /// <summary>
+        /// Removes all leading white-space characters from the span.
+        /// </summary>
+        public static Span<char> TrimStart(this Span<char> span)
+            => span.Slice(ClampStart(span));
+
+        /// <summary>
+        /// Removes all trailing white-space characters from the span.
+        /// </summary>
+        public static Span<char> TrimEnd(this Span<char> span)
+            => span.Slice(0, ClampEnd(span, 0));
+
+        /// <summary>
+        /// Delimits all leading occurrences of whitespace.
+        /// </summary>
+        /// <param name="span">The source span from which the element is removed.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static int ClampStart(ReadOnlySpan<char> span)
+        {
             int start = 0;
+
             for (; start < span.Length; start++)
             {
                 if (!char.IsWhiteSpace(span[start]))
@@ -846,7 +693,22 @@ namespace System
                 }
             }
 
+            return start;
+        }
+
+        /// <summary>
+        /// Delimits all trailing occurrences of whitespace.
+        /// </summary>
+        /// <param name="span">The source span from which the element is removed.</param>
+        /// <param name="start">The start index from which to being searching.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static int ClampEnd(ReadOnlySpan<char> span, int start)
+        {
+            // Initially, start==len==0. If ClampStart trims all, start==len
+            Debug.Assert((uint)start <= span.Length);
+
             int end = span.Length - 1;
+
             for (; end >= start; end--)
             {
                 if (!char.IsWhiteSpace(span[end]))
@@ -855,41 +717,97 @@ namespace System
                 }
             }
 
-            return span.Slice(start, end - start + 1);
+            return end - start + 1;
         }
 
         /// <summary>
-        /// Removes all leading white-space characters from the span.
+        /// Delimits all leading occurrences of a specified element.
         /// </summary>
-        public static Span<char> TrimStart(this Span<char> span)
+        /// <param name="span">The source span from which the element is removed.</param>
+        /// <param name="trimChar">The specified element to look for and remove.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static int ClampStart(ReadOnlySpan<char> span, char trimChar)
+        {
+            int start = 0;
+
+            for (; start < span.Length; start++)
+            {
+                if (span[start] != trimChar)
+                {
+                    break;
+                }
+            }
+
+            return start;
+        }
+
+        /// <summary>
+        /// Delimits all trailing occurrences of a specified element.
+        /// </summary>
+        /// <param name="span">The source span from which the element is removed.</param>
+        /// <param name="start">The start index from which to being searching.</param>
+        /// <param name="trimChar">The specified element to look for and remove.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static int ClampEnd(ReadOnlySpan<char> span, int start, char trimChar)
+        {
+            // Initially, start==len==0. If ClampStart trims all, start==len
+            Debug.Assert((uint)start <= span.Length);
+
+            int end = span.Length - 1;
+
+            for (; end >= start; end--)
+            {
+                if (span[end] != trimChar)
+                {
+                    break;
+                }
+            }
+
+            return end - start + 1;
+        }
+
+        /// <summary>
+        /// Delimits all leading occurrences of a specified element.
+        /// </summary>
+        /// <param name="span">The source span from which the element is removed.</param>
+        /// <param name="trimChars">The span which contains the set of elements to remove.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static int ClampStart(ReadOnlySpan<char> span, ReadOnlySpan<char> trimChars)
         {
             int start = 0;
             for (; start < span.Length; start++)
             {
-                if (!char.IsWhiteSpace(span[start]))
+                if (!trimChars.Contains(span[start]))
                 {
                     break;
                 }
             }
 
-            return span.Slice(start);
+            return start;
         }
 
         /// <summary>
-        /// Removes all trailing white-space characters from the span.
+        /// Delimits all trailing occurrences of a specified element.
         /// </summary>
-        public static Span<char> TrimEnd(this Span<char> span)
+        /// <param name="span">The source span from which the element is removed.</param>
+        /// <param name="start">The start index from which to being searching.</param>
+        /// <param name="trimChars">The span which contains the set of elements to remove.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static int ClampEnd(ReadOnlySpan<char> span, int start, ReadOnlySpan<char> trimChars)
         {
+            // Initially, start==len==0. If ClampStart trims all, start==len
+            Debug.Assert((uint)start <= span.Length);
+
             int end = span.Length - 1;
-            for (; end >= 0; end--)
+            for (; end >= start; end--)
             {
-                if (!char.IsWhiteSpace(span[end]))
+                if (!trimChars.Contains(span[end]))
                 {
                     break;
                 }
             }
 
-            return span.Slice(0, end + 1);
+            return end - start + 1;
         }
     }
 }
