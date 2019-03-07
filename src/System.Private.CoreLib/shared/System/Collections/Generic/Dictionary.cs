3099f47d1dcf1f83ffe279086ea1b9e1ed9b5c63
Marco Rossignoli <marco.rossignoli@gmail.com>
2019-03-07 10:31:12 +0100

    change struct layout

--- src/System.Private.CoreLib/shared/System/Collections/Generic/Dictionary.cs
+++ src/System.Private.CoreLib/shared/System/Collections/Generic/Dictionary.cs
@@ -37,11 +37,11 @@ namespace System.Collections.Generic
     {
         private struct Entry
         {
-            public uint hashCode;
             // 0-based index of next entry in chain: -1 means end of chain
             // also encodes whether this entry _itself_ is part of the free list by changing sign and subtracting 3,
             // so -2 means end of free list, -3 means index 0 but on free list, -4 means index 1 but on free list, etc.
             public int next;
+            public uint hashCode;
             public TKey key;           // Key of entry
             public TValue value;         // Value of entry
         }
