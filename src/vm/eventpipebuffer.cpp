dotnet/runtime

commit 8009ba276895edc9decf49c8a2470a1e6bbe2997

Avoid StackWalk in EventPipeEventInstance constructor

src/vm/eventpipebuffer.cpp
@@ -83,6 +83,7 @@ bool EventPipeBuffer::WriteEvent(Thread *pThread, EventPipeSession &session, Eve
             payload.GetSize(),
             (pThread == NULL) ? NULL : pActivityId,
             pRelatedActivityId);
+        pInstance->EnsureStack(session); // TODO: Perform the stackwalk before the constructor
 
         // Copy the stack if a separate stack trace was provided.
         if(pStack != NULL)

src/vm/eventpipe.cpp
@@ -798,6 +798,7 @@ void EventPipe::WriteEventInternal(EventPipeEvent &event, EventPipeEventPayload 
                 payload.GetSize(),
                 pActivityId,
                 pRelatedActivityId);
+            instance.EnsureStack(*s_pSession);
 
             if(s_pFile != NULL)
             {

src/vm/eventpipeconfiguration.cpp
@@ -505,6 +505,7 @@ EventPipeEventInstance* EventPipeConfiguration::BuildEventMetadataEvent(EventPip
         instancePayloadSize,
         NULL /* pActivityId */,
         NULL /* pRelatedActivityId */);
+    _ASSERTE(!m_pMetadataEvent->NeedStack());
 
     // Set the timestamp to match the source event, because the metadata event
     // will be emitted right before the source event.

src/vm/eventpipeeventinstance.cpp
@@ -34,7 +34,7 @@ EventPipeEventInstance::EventPipeEventInstance(
 #endif // _DEBUG
     m_pEvent = &event;
     m_threadID = threadID;
-    if(pActivityId != NULL)
+    if (pActivityId != NULL)
     {
         m_activityId = *pActivityId;
     }
@@ -42,7 +42,7 @@ EventPipeEventInstance::EventPipeEventInstance(
     {
         m_activityId = {0};
     }
-    if(pRelatedActivityId != NULL)
+    if (pRelatedActivityId != NULL)
     {
         m_relatedActivityId = *pRelatedActivityId;
     }
@@ -55,15 +55,17 @@ EventPipeEventInstance::EventPipeEventInstance(
     m_dataLength = length;
     QueryPerformanceCounter(&m_timeStamp);
     _ASSERTE(m_timeStamp.QuadPart > 0);
-
-    if(event.NeedStack() && !session.RundownEnabled())
-    {
-        EventPipe::WalkManagedStackForCurrentThread(m_stackContents);
-    }
-
 #ifdef _DEBUG
     EnsureConsistency();
 #endif // _DEBUG
+}
+
+void EventPipeEventInstance::EnsureStack(const EventPipeSession &session)
+{
+    if (m_pEvent->NeedStack() && !session.RundownEnabled())
+    {
+        EventPipe::WalkManagedStackForCurrentThread(m_stackContents);
+    }
 }
 
 unsigned int EventPipeEventInstance::GetAlignedTotalSize() const

src/vm/eventpipeeventinstance.h
@@ -23,6 +23,8 @@ public:
 
     EventPipeEventInstance(EventPipeSession &session, EventPipeEvent &event, DWORD threadID, BYTE *pData, unsigned int length, LPCGUID pActivityId, LPCGUID pRelatedActivityId);
 
+    void EnsureStack(const EventPipeSession &session);
+
     StackContents* GetStack()
     {
         LIMITED_METHOD_CONTRACT;

dotnet/coreclr

commit 1cc5e3204276d511a1fb5d5c1bb9ec6803141076

Fix issue 23151 (Cleanup TODO introduced by PR #23148)

src/vm/eventpipebuffer.cpp
@@ -75,6 +75,15 @@ bool EventPipeBuffer::WriteEvent(Thread *pThread, EventPipeSession &session, Eve
 
         // Placement-new the EventPipeEventInstance.
         // if pthread is NULL, it's likely we are running in something like a GC thread which is not a Thread object, so it can't have an activity ID set anyway
+
+        StackContents s;
+        memset((void*)&s, 0, sizeof(s));
+        if (event.NeedStack() && !session.RundownEnabled() && pStack == NULL)
+        {
+            EventPipe::WalkManagedStackForCurrentThread(s);
+            pStack = &s;
+        }
+
         EventPipeEventInstance *pInstance = new (m_pCurrent) EventPipeEventInstance(
             session,
             event,
@@ -83,10 +92,9 @@ bool EventPipeBuffer::WriteEvent(Thread *pThread, EventPipeSession &session, Eve
             payload.GetSize(),
             (pThread == NULL) ? NULL : pActivityId,
             pRelatedActivityId);
-        pInstance->EnsureStack(session); // TODO: Perform the stackwalk before the constructor
 
         // Copy the stack if a separate stack trace was provided.
-        if(pStack != NULL)
+        if (pStack != NULL)
         {
             StackContents *pInstanceStack = pInstance->GetStack();
             pStack->CopyTo(pInstanceStack);