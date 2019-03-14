a1aa0857e2a6544ddb2a8e24d372decfd1cf2f37
EgorBo <egorbo@gmail.com>
2019-03-15 02:08:49 +0300

    address nit

--- src/System.Private.CoreLib/src/System/TypeLoadException.CoreCLR.cs
+++ src/System.Private.CoreLib/src/System/TypeLoadException.CoreCLR.cs
@@ -2,15 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-/*=============================================================================
-**
-**
-**
-** Purpose: The exception class for type loading failures.
-**
-**
-=============================================================================*/
-
 using System.Runtime.InteropServices;
 using System.Runtime.CompilerServices;
 
