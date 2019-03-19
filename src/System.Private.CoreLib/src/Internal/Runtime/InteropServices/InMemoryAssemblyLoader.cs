12a49cbd8cf5ef1dd5a90020995c2c490b4c2954
Jeremy Koritzinsky <jekoritz@microsoft.com>
2019-03-19 11:59:46 -0700

    PR feedback.

--- src/System.Private.CoreLib/src/Internal/Runtime/InteropServices/InMemoryAssemblyLoader.cs
+++ src/System.Private.CoreLib/src/Internal/Runtime/InteropServices/InMemoryAssemblyLoader.cs
@@ -19,12 +19,14 @@ namespace Internal.Runtime.InteropServices
         /// <summary>
         /// Loads into an isolated AssemblyLoadContext an assembly that has already been loaded into memory by the OS loader as a native module.
         /// </summary>
-        /// <param name="handle">The native handle for the assembly.</param>
+        /// <param name="moduleHandle">The native module handle for the assembly.</param>
         /// <param name="assemblyPath">The path to the assembly (as a pointer to a UTF-16 C string).</param>
-        public static unsafe void LoadInMemoryAssembly(IntPtr handle, IntPtr assemblyPath)
+        public static unsafe void LoadInMemoryAssembly(IntPtr moduleHandle, IntPtr assemblyPath)
         {
+            // We don't cache the ALCs here since each IJW assembly will call this method at most once
+            // (the load process rewrites the stubs that call here to call the actual methods they're supposed to)
             AssemblyLoadContext context = new IsolatedComponentLoadContext(Marshal.PtrToStringUni(assemblyPath));
-            context.LoadFromInMemoryModule(handle);
+            context.LoadFromInMemoryModule(moduleHandle);
         }
     }
 }
