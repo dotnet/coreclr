a5d099206bc984e834428b479e02b2ddf921c82c
Carol Eidt <carol.eidt@microsoft.com>
2019-03-21 12:01:42 -0700

    Add checks for ISA support

--- tests/src/JIT/Regression/JitBlue/GitHub_19550/GitHub_19550.cs
+++ tests/src/JIT/Regression/JitBlue/GitHub_19550/GitHub_19550.cs
@@ -37,43 +37,46 @@ public class Program
     {
         int returnVal = 100;
 
-        fixed (float* p = &s.f0)
+        if (Sse2.IsSupported)
         {
-            // We need an address aligned on 16 bytes, so we need to add a *float* offset to get there.
-            int alignmentOffset = (0x10 - ((int)p & 0xc)) >> 2;
-            try
+            fixed (float* p = &s.f0)
             {
-                // This is the aligned case.
-                // We're going to store a scalar at an offset of 2 from the aligned location.
-                // As it happens, we know that the struct has been initialized to all zeros,
-                // and the vector passed in was all ones, so now we have a one at offset 2.
-                Sse2.StoreScalar(p + alignmentOffset + 2, Sse2.Subtract(v, Sse2.LoadAlignedVector128(p + offset + alignmentOffset + 4)));
-
-                // Now do a non-temporal load from the aligned location.
-                // That should give us {0, 0, 1, 0}.
-                Vector128<float> v2 = Sse41.LoadAlignedVector128NonTemporal((byte*)(p + alignmentOffset)).AsSingle();
-                if (!v2.Equals(Vector128.Create(0.0F, 0.0F, 1.0F, 0.0F)))
+                // We need an address aligned on 16 bytes, so we need to add a *float* offset to get there.
+                int alignmentOffset = (0x10 - ((int)p & 0xc)) >> 2;
+                try
                 {
-                    Console.WriteLine("Aligned case FAILED: v2 = " + v2);
+                    // This is the aligned case.
+                    // We're going to store a scalar at an offset of 2 from the aligned location.
+                    // As it happens, we know that the struct has been initialized to all zeros,
+                    // and the vector passed in was all ones, so now we have a one at offset 2.
+                    Sse2.StoreScalar(p + alignmentOffset + 2, Sse2.Subtract(v, Sse2.LoadAlignedVector128(p + offset + alignmentOffset + 4)));
+
+                    // Now do a non-temporal load from the aligned location.
+                    // That should give us {0, 0, 1, 0}.
+                    Vector128<float> v2 = Sse41.LoadAlignedVector128NonTemporal((byte*)(p + alignmentOffset)).AsSingle();
+                    if (!v2.Equals(Vector128.Create(0.0F, 0.0F, 1.0F, 0.0F)))
+                    {
+                        Console.WriteLine("Aligned case FAILED: v2 = " + v2);
+                        returnVal = -1;
+                    }
+
+                    // This is the unaligned case. The value we're loading to subtract is one element earlier than what we just stored.
+                    // So we're doing { 1, 1, 1, 1 } - { 0, 1, 0, 0 } = { 1, 0, 1, 1 }
+                    Sse2.Store(p + alignmentOffset + 1, Sse2.Subtract(v, Sse2.LoadVector128(p + offset + alignmentOffset + 1)));
+                    // Now do an unaligned load from that location.
+                    v2 = Sse2.LoadVector128(p + alignmentOffset + 1);
+                    if (!v2.Equals(Vector128.Create(1.0F, 0.0F, 1.0F, 1.0F)))
+                    {
+                        Console.WriteLine("Unaligned case FAILED: v2 = " + v2);
+                        returnVal = -1;
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unexpected exception: " + e.Message);
                     returnVal = -1;
                 }
-
-                // This is the unaligned case. The value we're loading to subtract is one element earlier than what we just stored.
-                // So we're doing { 1, 1, 1, 1 } - { 0, 1, 0, 0 } = { 1, 0, 1, 1 }
-                Sse2.Store(p + alignmentOffset + 1, Sse2.Subtract(v, Sse2.LoadVector128(p + offset + alignmentOffset + 1)));
-                // Now do an unaligned load from that location.
-                v2 = Sse2.LoadVector128(p + alignmentOffset + 1);
-                if (!v2.Equals(Vector128.Create(1.0F, 0.0F, 1.0F, 1.0F)))
-                {
-                    Console.WriteLine("Unaligned case FAILED: v2 = " + v2);
-                    returnVal = -1;
-                }
-
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Unexpected exception: " + e.Message);
-                returnVal = -1;
             }
         }
         return returnVal;
@@ -83,24 +86,27 @@ public class Program
     static unsafe int Test256(ref S s, Vector256<float> v, int offset)
     {
         int returnVal = 100;
-        // offset must be a multiple of the vector size in floats.
-        offset &= ~3;
-        fixed (float* p = &s.f0)
+        if (Avx.IsSupported)
         {
-            try
+            // offset must be a multiple of the vector size in floats.
+            offset &= ~3;
+            fixed (float* p = &s.f0)
             {
-                Avx.Store(p + 1, Avx.Subtract(v, Avx.LoadVector256(p + offset + 1)));
-                Vector256<float> v2 = Avx.LoadVector256(p + 1);
-                if (!v2.Equals(v))
+                try
                 {
-                    Console.WriteLine("Vector256 case FAILED: v = " + v + ", v2 = " + v2);
+                    Avx.Store(p + 1, Avx.Subtract(v, Avx.LoadVector256(p + offset + 1)));
+                    Vector256<float> v2 = Avx.LoadVector256(p + 1);
+                    if (!v2.Equals(v))
+                    {
+                        Console.WriteLine("Vector256 case FAILED: v = " + v + ", v2 = " + v2);
+                        returnVal = -1;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unexpected exception: " + e.Message);
                     returnVal = -1;
                 }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Unexpected exception: " + e.Message);
-                returnVal = -1;
             }
         }
         return returnVal;
