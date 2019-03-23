196d92f9cfe7299638ebfc3ddf85e9efc39eaf16
Sinan Kaya <sinan.kaya@microsoft.com>
2019-03-23 05:30:13 +0000

    find src/jit -type f -exec sed -i -e 's/gtArgPlace\./AsArgPlace()\./g' {} \;

--- src/jit/importer.cpp
+++ src/jit/importer.cpp
@@ -1662,7 +1662,7 @@ GenTree* Compiler::impNormStructVal(GenTree*             structVal,
             break;
 
         case GT_ARGPLACE:
-            structVal->gtArgPlace.gtArgPlaceClsHnd = structHnd;
+            structVal->AsArgPlace()->gtArgPlaceClsHnd = structHnd;
             break;
 
         case GT_INDEX:
