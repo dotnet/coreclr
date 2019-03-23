86363d97f8b8727b23bb4af7d9b232d0a85de926
Sinan Kaya <sinan.kaya@microsoft.com>
2019-03-23 05:29:25 +0000

    find src/jit -type f -exec sed -i -e 's/gtClsVar\./AsClsVar()\./g' {} \;

--- src/jit/gentree.h
+++ src/jit/gentree.h
@@ -5500,7 +5500,7 @@ public:
     pointers) must be flagged as 'large' in GenTree::InitNodeSize().
  */
 
-/* gtClsVar -- 'static data member' (GT_CLS_VAR) */
+/* AsClsVar() -- 'static data member' (GT_CLS_VAR) */
 
 struct GenTreeClsVar : public GenTree
 {
