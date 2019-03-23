c39f5b405bb690adb84f97a614e3cb7f55f7a29b
Sinan Kaya <sinan.kaya@microsoft.com>
2019-03-23 05:38:02 +0000

    find src/jit -type f -exec sed -i -e 's/gtAllocObj\./AsAllocObj()\./g' {} \;

--- src/jit/gentree.cpp
+++ src/jit/gentree.cpp
@@ -1940,8 +1940,8 @@ AGAIN:
                     break;
                 case GT_ALLOCOBJ:
                     hash = genTreeHashAdd(hash, static_cast<unsigned>(
-                                                    reinterpret_cast<uintptr_t>(tree->gtAllocObj.gtAllocObjClsHnd)));
-                    hash = genTreeHashAdd(hash, tree->gtAllocObj.gtNewHelper);
+                                                    reinterpret_cast<uintptr_t>(tree->AsAllocObj()->gtAllocObjClsHnd)));
+                    hash = genTreeHashAdd(hash, tree->AsAllocObj()->gtNewHelper);
                     break;
                 case GT_RUNTIMELOOKUP:
                     hash =
