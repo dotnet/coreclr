bbd6d18c6a1e685fc709b258e437819019d41c92
Sinan Kaya <sinan.kaya@microsoft.com>
2019-03-23 05:07:35 +0000

    find src/jit -type f -exec sed -i -e 's/gtDblCon\./AsDblCon()\./g' {} \;

--- src/jit/gentree.h
+++ src/jit/gentree.h
@@ -6423,7 +6423,7 @@ inline bool GenTree::OperIsCopyBlkOp()
 
 inline bool GenTree::IsFPZero()
 {
-    if ((gtOper == GT_CNS_DBL) && (gtDblCon.gtDconVal == 0.0))
+    if ((gtOper == GT_CNS_DBL) && (AsDblCon()->gtDconVal == 0.0))
     {
         return true;
     }
@@ -6969,7 +6969,7 @@ inline bool GenTree::IsCnsNonZeroFltOrDbl()
 {
     if (OperGet() == GT_CNS_DBL)
     {
-        double constValue = gtDblCon.gtDconVal;
+        double constValue = AsDblCon()->gtDconVal;
         return *(__int64*)&constValue != 0;
     }
 
