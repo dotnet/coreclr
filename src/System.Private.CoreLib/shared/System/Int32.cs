b75fe35d19389f9216e7c7054320ba2f5e5ad4e7
Krzysztof Wicher <mordotymoja@gmail.com>
2019-03-25 18:53:11 -0700

    remove trailing \n after #nullable to make it easier to remove later

--- src/System.Private.CoreLib/shared/System/Int32.cs
+++ src/System.Private.CoreLib/shared/System/Int32.cs
@@ -3,7 +3,6 @@
 // See the LICENSE file in the project root for more information.
 
 #nullable enable
-
 using System.Globalization;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
