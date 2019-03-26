9264123686e054a7a2a195171325766a3d52cdde
Carol Eidt <carol.eidt@microsoft.com>
2019-03-26 14:26:58 -0700

    Clarify `fgRemoveDeadStore`

--- src/jit/liveness.cpp
+++ src/jit/liveness.cpp
@@ -2160,14 +2160,14 @@ bool Compiler::fgRemoveDeadStore(GenTree**        pTree,
         addrNode = tree;
     }
 
-    // Next, find the assignment.
+    // Next, find the assignment (i.e. if we didn't have a LocalStore)
     if (asgNode == nullptr)
     {
         if (addrNode == nullptr)
         {
             asgNode = nextNode;
         }
-        else if (asgNode == nullptr)
+        else
         {
             // This may be followed by GT_IND/assign or GT_STOREIND.
             if (nextNode == nullptr)
@@ -2180,6 +2180,8 @@ bool Compiler::fgRemoveDeadStore(GenTree**        pTree,
                 assert(nextNode->OperGet() != GT_NULLCHECK);
                 if (nextNode->OperIsStore())
                 {
+                    // This is a store, which takes a location and a value to be stored.
+                    // It's 'rhsNode' is the value to be stored.
                     asgNode = nextNode;
                     if (asgNode->OperIsBlk())
                     {
@@ -2187,11 +2189,13 @@ bool Compiler::fgRemoveDeadStore(GenTree**        pTree,
                     }
                     else
                     {
+                        // This is a non-block store.
                         rhsNode = asgNode->gtGetOp2();
                     }
                 }
                 else
                 {
+                    // This is a non-store indirection, and the assignment will com after it.
                     asgNode = nextNode->gtNext;
                 }
             }
