8ea91925b9ef3676db3c89d3c0c269b7ddd82474
Levi Broderick <levib@microsoft.com>
2019-03-27 10:06:48 -0700

    PR feedback: make bitmasks easier to read - Also, 'count' should be typed as uint for correctness

--- src/System.Private.CoreLib/shared/System/Marvin.cs
+++ src/System.Private.CoreLib/shared/System/Marvin.cs
@@ -22,19 +22,19 @@ namespace System
         /// Compute a Marvin hash and collapse it into a 32-bit hash.
         /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static int ComputeHash32(ReadOnlySpan<byte> data, ulong seed) => ComputeHash32(ref MemoryMarshal.GetReference(data), data.Length, (uint)seed, (uint)(seed >> 32));
+        public static int ComputeHash32(ReadOnlySpan<byte> data, ulong seed) => ComputeHash32(ref MemoryMarshal.GetReference(data), (uint)data.Length, (uint)seed, (uint)(seed >> 32));
 
         /// <summary>
         /// Compute a Marvin hash and collapse it into a 32-bit hash.
         /// </summary>
-        public static int ComputeHash32(ref byte data, int count, uint p0, uint p1)
+        public static int ComputeHash32(ref byte data, uint count, uint p0, uint p1)
         {
             // Control flow of this method generally flows top-to-bottom, trying to
             // minimize the number of branches taken for large (>= 8 bytes, 4 chars) inputs.
             // If small inputs (< 8 bytes, 4 chars) are given, this jumps to a "small inputs"
             // handler at the end of the method.
 
-            if ((uint)count < 8)
+            if (count < 8)
             {
                 goto InputTooSmallToEnterMainLoop;
             }
@@ -42,7 +42,7 @@ namespace System
             // Main loop - read 8 bytes at a time.
             // The block function is unrolled 2x in this loop.
 
-            uint loopCount = (uint)count / 8;
+            uint loopCount = count / 8;
             Debug.Assert(loopCount > 0, "Shouldn't reach this code path for small inputs.");
 
             do
@@ -64,10 +64,10 @@ namespace System
 
                 // Bump the data reference pointer and decrement the loop count.
 
-                // TODO: By decrementing the loop count by 1 every time and comparing against zero,
-                // we encourage the JIT to emit "dec test jnz" instead of "add cmp jae". This provides
-                // a potential for further optimization: JIT could elide the test instruction entirely
-                // since dec sets flags, allowing the x86 architecture to micro-op fuse "dec jnz".
+                // Decrementing by 1 every time and comparing against zero allows the JIT to produce
+                // better codegen compared to a standard 'for' loop with an incrementing counter.
+                // Requires https://github.com/dotnet/coreclr/issues/7566 to be addressed first
+                // before we can realize the full benefits of this.
 
                 data = ref Unsafe.AddByteOffset(ref data, 8);
             } while (--loopCount > 0);
@@ -77,7 +77,7 @@ namespace System
             // 3 bits to tell us how much data remains (0 .. 7 bytes) after the loop above is
             // completed.
 
-            if ((count & 4) == 0)
+            if ((count & 0b_0100) == 0)
             {
                 goto DoFinalPartialRead;
             }
@@ -88,7 +88,7 @@ namespace System
             // 4 .. 7 bytes to begin with and couldn't enter the loop in the first place, we need to
             // consume 4 bytes immediately and send them through one round of the block function.
 
-            Debug.Assert((uint)count >= 4, "Only should've gotten here if the original count was >= 4.");
+            Debug.Assert(count >= 4, "Only should've gotten here if the original count was >= 4.");
 
             p0 += Unsafe.ReadUnaligned<uint>(ref data);
             Block(ref p0, ref p1);
@@ -101,11 +101,11 @@ namespace System
             // original buffer. This necessarily means the data we're about to read will overlap with
             // some data we've already processed, but we can handle that below.
 
-            Debug.Assert((uint)count >= 4, "Only should've gotten here if the original count was >= 4.");
+            Debug.Assert(count >= 4, "Only should've gotten here if the original count was >= 4.");
 
             // Read the last 4 bytes of the buffer.
 
-            uint partialResult = Unsafe.ReadUnaligned<uint>(ref Unsafe.Add(ref Unsafe.AddByteOffset(ref data, (nuint)(uint)count & 7), -4));
+            uint partialResult = Unsafe.ReadUnaligned<uint>(ref Unsafe.Add(ref Unsafe.AddByteOffset(ref data, (nuint)count & 7), -4));
 
             // The 'partialResult' local above contains any data we have yet to read, plus some number
             // of bytes which we've already read from the buffer. An example of this is given below
@@ -113,23 +113,23 @@ namespace System
             // need to consume, and ## are bytes which we want to throw away since we've already
             // consumed them as part of a previous read.
             //
-            //                                                    (partialResult contains)  (we want it to contain)
-            // count mod 4 = 0 -> [ ## ## ## ## |             ] -> 0x########             -> 0x00000080
-            // count mod 4 = 1 -> [ ## ## ## ## | AA          ] -> 0xAA######             -> 0x000080AA
-            // count mod 4 = 2 -> [ ## ## ## ## | AA BB       ] -> 0xBBAA####             -> 0x0080BBAA
-            // count mod 4 = 3 -> [ ## ## ## ## | AA BB CC    ] -> 0xCCBBAA##             -> 0x80CCBBAA
+            //                                                    (partialResult contains)   (we want it to contain)
+            // count mod 4 = 0 -> [ ## ## ## ## |             ] -> 0x####_####             -> 0x0000_0080
+            // count mod 4 = 1 -> [ ## ## ## ## | AA          ] -> 0xAA##_####             -> 0x0000_80AA
+            // count mod 4 = 2 -> [ ## ## ## ## | AA BB       ] -> 0xBBAA_####             -> 0x0080_BBAA
+            // count mod 4 = 3 -> [ ## ## ## ## | AA BB CC    ] -> 0xCCBB_AA##             -> 0x80CC_BBAA
 
             if (BitConverter.IsLittleEndian)
             {
                 partialResult >>= 8; // make some room for the 0x80 byte
-                partialResult |= 0x80000000u; // put the 0x80 byte at the beginning
-                partialResult >>= (~count << 3) & 0x1F; // shift out all previously consumed bytes
+                partialResult |= 0x8000_0000u; // put the 0x80 byte at the beginning
+                partialResult >>= (int)(~count << 3) & 0x1F; // shift out all previously consumed bytes
             }
             else
             {
                 partialResult <<= 8; // make some room for the 0x80 byte
                 partialResult |= 0x80u; // put the 0x80 byte at the end
-                partialResult <<= (~count << 3) & 0x1F; // shift out all previously consumed bytes
+                partialResult <<= (int)(~count << 3) & 0x1F; // shift out all previously consumed bytes
             }
 
         DoFinalRoundsAndReturn:
@@ -148,7 +148,7 @@ namespace System
             // We can't run the main loop, but we might still have 4 or more bytes available to us.
             // If so, jump to the 4 .. 7 bytes logic immediately after the main loop.
 
-            if ((count & 4) != 0)
+            if ((count & 0b_0100) != 0)
             {
                 goto Between4And7BytesRemain;
             }
@@ -167,17 +167,17 @@ namespace System
                 partialResult = 0x80000000u;
             }
 
-            if ((count & 1) != 0)
+            if ((count & 0b_0001) != 0)
             {
                 // If the buffer is 1 or 3 bytes in length, let's read a single byte now
                 // and merge it into our partial result. This will result in partialResult
                 // having one of the two values below, where AA BB CC are the buffer bytes.
                 // 
                 //                  (little-endian / big-endian)
-                // [ AA          ]  -> 0x000080AA / 0xAA800000;
-                // [ AA BB CC    ]  -> 0x000080CC / 0xCC800000;
+                // [ AA          ]  -> 0x0000_80AA / 0xAA80_0000
+                // [ AA BB CC    ]  -> 0x0000_80CC / 0xCC80_0000
 
-                partialResult = Unsafe.AddByteOffset(ref data, (nuint)(uint)count & 2);
+                partialResult = Unsafe.AddByteOffset(ref data, (nuint)count & 2);
 
                 if (BitConverter.IsLittleEndian)
                 {
@@ -190,15 +190,15 @@ namespace System
                 }
             }
 
-            if ((count & 2) != 0)
+            if ((count & 0b_0010) != 0)
             {
                 // If the buffer is 2 or 3 bytes in length, let's read a single ushort now
                 // and merge it into the partial result. This will result in partialResult
                 // having one of the two values below, where AA BB CC are the buffer bytes.
                 // 
                 //                  (little-endian / big-endian)
-                // [ AA BB       ]  -> 0x0080BBAA / 0xAABB8000;
-                // [ AA BB CC    ]  -> 0x80CCBBAA / 0xAABBCC80; (carried over from above)
+                // [ AA BB       ]  -> 0x0080_BBAA / 0xAABB_8000
+                // [ AA BB CC    ]  -> 0x80CC_BBAA / 0xAABB_CC80 (carried over from above)
 
                 if (BitConverter.IsLittleEndian)
                 {
