8bc0aa5e3b0117372c3079e15c99146167cab29d
Jeremy Koritzinsky <jekoritz@microsoft.com>
2019-03-28 13:51:13 -0700

    Add license headers to native files.

--- tests/src/Interop/WinRT/NativeComponent/Component.Contracts.BindingProjectionsTesting.cpp
+++ tests/src/Interop/WinRT/NativeComponent/Component.Contracts.BindingProjectionsTesting.cpp
@@ -1,4 +1,7 @@
-﻿#include "pch.h"
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+#include "pch.h"
 #include "Component.Contracts.BindingProjectionsTesting.h"
 #include "Component.Contracts.BindingViewModel.h"
 #include <winrt/Windows.UI.Xaml.Hosting.h>
