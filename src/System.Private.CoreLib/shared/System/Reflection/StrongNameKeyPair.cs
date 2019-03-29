cb3fa2153c284d1b4b70bcffc4ff59f9ae8764df
simoncropp <simon.cropp@gmail.com>
2019-03-29 17:13:38 +1100

    missing usings
    Co-Authored-By: atykhyy <atykhyy@gmail.com>

--- src/System.Private.CoreLib/shared/System/Reflection/StrongNameKeyPair.cs
+++ src/System.Private.CoreLib/shared/System/Reflection/StrongNameKeyPair.cs
@@ -2,8 +2,11 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using System.IO;
 using System.Runtime.Serialization;
+using System.Security;
+using System.Security.Cryptography;
 
 namespace System.Reflection
 {
