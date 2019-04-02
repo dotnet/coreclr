fdba61307e5227db7ebf554e7b0397cb2d033a3d
Sung Yoon Whang <suwhang@microsoft.com>
2019-04-01 19:20:31 -0700

    Add cpu counter

---
+++ src/System.Private.CoreLib/src/System/Diagnostics/Eventing/RuntimeEventSourceHelper.Unix.cs
@@ -0,0 +1,21 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Buffers;
+using System.IO;
+
+
+namespace System.Diagnostics.Tracing
+{
+    internal sealed class RuntimeEventSourceHelper
+    {
+        internal static long GetProcessTimes()
+        {
+            Interop.Sys.ProcessCpuInformation info;
+            Interop.Sys.GetCpuUtilization(info);
+            return info.lastRecordedKernelTime + lastRecordedUserTime;
+        }
+    }
+}
---
+++ src/System.Private.CoreLib/src/System/Diagnostics/Eventing/RuntimeEventSourceHelper.cs
@@ -0,0 +1,22 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Threading;
+
+
+namespace System.Diagnostics.Tracing
+{
+	internal sealed class RuntimeEventSourceHelper
+	{
+		internal static long GetProcessTimes()
+		{
+			long user;
+			long kernel;
+			
+			Interop.Kernel32.GetProcessTimes(Interop.Kernel32.GetCurrentProcess(), out _, out _, out user, out kernel);
+			return user + kernel;
+		}
+	}
+}
