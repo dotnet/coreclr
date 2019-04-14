3e24f9e9f9cbd57c83c0c58c09f2e9255072b7c4
Jeremy Koritzinsky <jekoritz@microsoft.com>
2019-04-13 21:48:30 -0700

    Opt COM methods out of the new Windows instance-method handling.

--- tests/src/Interop/COM/ServerContracts/Server.Contracts.cs
+++ tests/src/Interop/COM/ServerContracts/Server.Contracts.cs
@@ -60,14 +60,6 @@ namespace Server.Contract
 
         int Add_ManyInts11(int i1, int i2, int i3, int i4, int i5, int i6, int i7, int i8, int i9, int i10, int i11);
         int Add_ManyInts12(int i1, int i2, int i3, int i4, int i5, int i6, int i7, int i8, int i9, int i10, int i11, int i12);
-
-        [PreserveSig]
-        SizeF MakeSize(float width, float height);
-        [PreserveSig]
-        Size MakeSizeSmall(byte width, byte height);
-
-        [PreserveSig]
-        HFA_4 MakeHFA(float x, float y, float z, float w);
     }
 
     [ComVisible(true)]
--- tests/src/Interop/COM/ServerContracts/Server.Contracts.h
+++ tests/src/Interop/COM/ServerContracts/Server.Contracts.h
@@ -163,17 +163,6 @@ INumericTesting : IUnknown
         /*[in]*/ int i11,
         /*[in]*/ int i12,
         /*[out]*/ int * result ) = 0;
-        virtual COM_DECLSPEC_NOTHROW SizeF STDMETHODCALLTYPE MakeSize(
-        /*[in]*/ float width,
-        /*[in]*/ float height) = 0;
-        virtual COM_DECLSPEC_NOTHROW Size STDMETHODCALLTYPE MakeSizeSmall(
-        /*[in]*/ BYTE width,
-        /*[in]*/ BYTE height) = 0;
-        virtual COM_DECLSPEC_NOTHROW HFA_4 STDMETHODCALLTYPE MakeHFA(
-        /*[in]*/ float x,
-        /*[in]*/ float y,
-        /*[in]*/ float z,
-        /*[in]*/ float w) = 0;
 };
 
 struct __declspec(uuid("7731cb31-e063-4cc8-bcd2-d151d6bc8f43"))
