2bd78baffb2ba4d32b91801dc3efa90e41f5e6f9
Krzysztof Wicher <kwicher@microsoft.com>
2019-05-01 11:48:42 -0700

    Nullable: System.Collections.Generic remainder

--- src/System.Private.CoreLib/shared/System/Collections/Generic/IReadOnlyDictionary.cs
+++ src/System.Private.CoreLib/shared/System/Collections/Generic/IReadOnlyDictionary.cs
@@ -2,12 +2,13 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+#nullable enable
 using System;
 
 namespace System.Collections.Generic
 {
     // Provides a read-only view of a generic dictionary.
-    public interface IReadOnlyDictionary<TKey, TValue> : IReadOnlyCollection<KeyValuePair<TKey, TValue>>
+    public interface IReadOnlyDictionary<TKey, TValue> : IReadOnlyCollection<KeyValuePair<TKey, TValue>> // TODO-NULLABLE-GENERIC: TKey should be non-nullable
     {
         bool ContainsKey(TKey key);
         bool TryGetValue(TKey key, out TValue value);
