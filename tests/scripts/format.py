d199951576726d17dcc979b3717548fba6433d7b
Jeremy Koritzinsky <jekoritz@microsoft.com>
2019-05-02 18:11:04 -0700

    Cleanup

--- tests/scripts/format.py
+++ tests/scripts/format.py
@@ -167,13 +167,6 @@ def main(argv):
     proc = subprocess.Popen(["dotnet", "build-server", "shutdown"], env=my_env)
     output,error = proc.communicate()
 
-    # shutdown all spurious dotnet processes using os shell
-    if platform == 'Linux' or platform == 'OSX':
-        subprocess.call(['killall', '-SIGTERM', '-qw', dotnet])
-    elif platform == 'Windows_NT':
-        utilpath = os.path.join(coreclr, 'tests\\scripts\\kill-all.cmd')
-        subprocess.call([utilpath, dotnet])
-
     if os.path.isdir(jitUtilsPath):
         print("Deleting " + jitUtilsPath)
         shutil.rmtree(jitUtilsPath, onerror=del_rw)
