601089e29cbf46f347e52e24b0f540032184e6ad
John Salem <josalem@microsoft.com>
2019-05-03 18:39:36 -0700

    * Add member checkers for Flatten and GetSize * Split Flatten impl to have a default version for simple, fixed-size structs

--- src/debug/inc/diagnosticsipc.h
+++ src/debug/inc/diagnosticsipc.h
@@ -16,42 +16,26 @@
 typedef void (*ErrorCallback)(const char *szMessage, uint32_t code);
 
 // "DOTNET_IPC_V1\0\0\0"
-#define DOTNET_IPC_V1_MAGIC { 0x44, 0x4F, 0x54, 0x4E, 0x45, 0x54, 0x5f, 0x49, 0x50, 0x43, 0x5F, 0x56, 0x31, 0x00, 0x00, 0x00 }
+constexpr const char DOTNET_IPC_V1_MAGIC[14] = "DOTNET_IPC_V1";
 
 namespace DiagnosticsIpc
 {
-
     enum IpcVersion : uint8_t
     {
         DOTNET_IPC_V1 = 0x01,
+        // FUTURE
     };
-
-    // "DOTNET_IPC_V1\0\0\0"
-    //static const char DOTNET_IPC_V1[16] = { 0x44, 0x4F, 0x54, 0x4E, 0x45, 0x54, 0x5f, 0x49, 0x50, 0x43, 0x5F, 0x56, 0x31, 0x00, 0x00, 0x00 };
-
-    //const char *IpcVersionString(IpcVersion versionEnum)
-    //{
-    //    switch (versionEnum)
-    //    {
-    //        case IpcVersion::DOTNET_IPC_V1:
-    //            return DOTNET_IPC_V1;
-    //        default:
-    //            ASSERT("Unkown IPC Version");
-    //    }
-    //};
 
     // The header to be associated with every command and response
     // to/from the diagnostics server
     struct IpcHeader
     {
-        char     Magic[16]; // Magic Version number; a 0 terminated char array
-        uint16_t Size;      // The size of the incoming packet, size = header + payload size
-        uint16_t Command;   // top two bytes are the command_set; bottom two bytes are the command
-        uint16_t Reserved;  // reserved for future use
+        char     Magic[14];  // Magic Version number; a 0 terminated char array
+        uint16_t Size;       // The size of the incoming packet, size = header + payload size
+        uint8_t  CommandSet; // The scope of the Command.
+        uint8_t  Command;    // The command being sent
+        uint16_t Reserved;   // reserved for future use
     };
-
-    // TODO: use a shared pointer
-    typedef void *pIpcPacketData;
 
     // Abstract class for all Payload types to implement.
     // This allows payload classes to be not-flat for convenience,
@@ -75,60 +59,71 @@ namespace DiagnosticsIpc
 
     static NullIpcPayload EmptyPayload;
 
+    template <bool B, class T = void>
+    using enable_if_t = typename std::enable_if<B, T>::type;
+
+    // template meta-programming to check for bool(Flatten)(void*) member function
+    template <typename T>
+    struct HasFlatten
+    {
+        using yes = uint16_t;
+        using no  = uint8_t;
+        template <typename U, U u> struct Has;
+        template <typename U> static constexpr yes& test(Has<bool (U::*)(void*), &U::Flatten>*);
+        template <typename U> static constexpr no& test(...);
+        static constexpr bool value = sizeof(test<T>(int())) == sizeof(yes);
+    };
+
+    // template meta-programming to check for uint16_t(GetSize)() member function
+    template <typename T>
+    struct HasGetSize
+    {
+        using yes = uint16_t;
+        using no  = uint8_t;
+        template <typename U, U u> struct Has;
+        template <typename U> static constexpr yes& test(Has<uint16_t (U::*)(), &U::GetSize>*);
+        template <typename U> static constexpr no& test(...);
+        static constexpr bool value = sizeof(test<T>(int())) == sizeof(yes);
+    };
+
     template <class T>
-    class IpcPacket
+    class IpcMessage
     {
     public:
-        IpcPacket(struct IpcHeader header, const T &payload)
+        // Create an IpcMessage from a header and a payload
+        IpcMessage(struct IpcHeader header, const T &payload)
             : m_Header(header), m_Payload(payload)
         {
-            static_assert(std::is_base_of<IpcPackable, T>::value, "Type parameter must derive from IpcPackable");
+            Flatten(); // TODO: Error checking
+        };
+
+        // Create an IpcMessage from an incoming buffer
+        // TODO: deal with ownership of pointer here? Don't want to needlesly copy data...
+        IpcMessage(BYTE *lpBuffer)
+            : m_pData(lpBuffer)
+        {
+            uint32_t bytesRead;
+            TryParse(m_pData, bytesRead);
         };
 
         // Attempt to populate header and payload from a buffer.
         // Payload is left opaque as a flattened buffer in m_pData
-        bool TryParse(void *lpBuffer, uint32_t &nBytesRead);
+        static bool TryParse(BYTE* lpBuffer, uint32_t& nBytesRead)
+        {
+            // TODO
+        };
 
         // Given a buffer, attempt to parse out a given payload type
         template <typename U>
-        bool TryParsePayload(void *lpBuffer, uint32_t &nBytesRead, const U *&result);
+        bool TryParsePayload(BYTE *lpBuffer, uint32_t &nBytesRead, const U *&result);
 
         // Create a buffer of the correct size filled with
-        // header + payload
+        // header + payload. Correctly handles flattening of
+        // trivial structures, but uses a bool(Flatten)(void*)
+        // and uint16_t(GetSize)() when available.
         bool Flatten()
         {
-            if (IsFlattened())
-                return true;
-
-            S_UINT16 temp_size = S_UINT16(0);
-            temp_size += sizeof(struct IpcHeader) + m_Payload.GetSize();
-            if (temp_size.IsOverflow())
-            {
-                // TODO: what should happen here?
-                return false;
-            }
-
-            m_Size = temp_size.Value;
-
-            BYTE *temp_buffer = new (nothrow) BYTE[m_Size];
-            BYTE *temp_buffer_cursor = temp_buffer;
-
-            if (temp_buffer == NULL)
-            {
-                // TODO: Error
-                return false;
-            }
-
-            m_Header.Size = m_Size;
-
-            memcpy(temp_buffer_cursor, &m_Header, sizeof(struct IpcHeader));
-            temp_buffer_cursor += sizeof(struct IpcHeader);
-
-            m_Payload.Flatten(temp_buffer_cursor);
-
-            m_pData = temp_buffer;
-
-            return true;
+            return FlattenImpl<T>();
         };
 
         BYTE *GetFlatData()
@@ -150,10 +145,102 @@ namespace DiagnosticsIpc
         struct IpcHeader m_Header;
         uint16_t m_Size;
 
+        ~IpcMessage()
+        {
+            // TODO: ensure flattened buffer is dealt with
+            delete m_pData;
+        };
+
         bool IsFlattened() const
         {
             return m_pData != NULL;
         };
+
+        // Handles the case where the payload structure exposes Flatten
+        // and GetSize methods
+        template <typename U>
+        enable_if_t<HasFlatten<U>::value && HasGetSize<U>::value, bool>
+        FlattenImpl()
+        {
+            if (IsFlattened())
+                return true;
+
+            S_UINT16 temp_size = S_UINT16(0);
+            temp_size += sizeof(struct IpcHeader) + m_Payload.GetSize();
+            if (temp_size.IsOverflow())
+            {
+                // TODO: what should happen here?
+                return false;
+            }
+
+            m_Size = temp_size.Value;
+
+            BYTE* temp_buffer = new (nothrow) BYTE[m_Size];
+            BYTE* temp_buffer_cursor = temp_buffer;
+
+            if (temp_buffer == NULL)
+            {
+                // TODO: Error
+                return false;
+            }
+
+            m_Header.Size = m_Size;
+
+            memcpy(temp_buffer_cursor, &m_Header, sizeof(struct IpcHeader));
+            temp_buffer_cursor += sizeof(struct IpcHeader);
+
+            m_Payload.Flatten(temp_buffer_cursor);
+
+            m_pData = temp_buffer;
+
+            return true;
+        }
+
+        // handles the case where we were handed a struct with no Flatten or GetSize method
+        template <typename U>
+        enable_if_t<!HasFlatten<U>::value && !HasGetSize<U>::value, bool>
+        FlattenImpl()
+        {
+            if (IsFlattened())
+                return true;
+
+            S_UINT16 temp_size = S_UINT16(0);
+            temp_size += sizeof(struct IpcHeader) + sizeof(m_Payload);
+            if (temp_size.IsOverflow())
+            {
+                // TODO: what should happen here?
+                return false;
+            }
+
+            m_Size = temp_size.Value;
+
+            BYTE* temp_buffer = new (nothrow) BYTE[m_Size];
+            BYTE* temp_buffer_cursor = temp_buffer;
+
+            if (temp_buffer == NULL)
+            {
+                // TODO: Error
+                return false;
+            }
+
+            m_Header.Size = m_Size;
+
+            memcpy(temp_buffer_cursor, &m_Header, sizeof(struct IpcHeader));
+            temp_buffer_cursor += sizeof(struct IpcHeader);
+
+            memcpy(temp_buffer_cursor, &m_Payload, sizeof(m_Payload));
+
+            m_pData = temp_buffer;
+
+            return true;
+        }
+    };
+
+    template <class T>
+    class IpcCommand : private IpcMessage<T>
+    {
+    public:
+        IpcCommand(uint16_t);
     };
 }
 
