7c7339d15eca90bf58b46a253817df1063ace0be
David Wrighton <davidwr@microsoft.com>
2019-05-07 12:56:23 -0700

    Fixup well known attributes, sort by namespace qualified name

--- src/vm/wellknownattributes.h
+++ src/vm/wellknownattributes.h
@@ -7,32 +7,33 @@
 
 enum class WellKnownAttribute : DWORD
 {
-    DisablePrivateReflectionType,
-    ImportedFromTypeLib,
-    PrimaryInteropAssembly,
-    CoClass,
-    ComEventInterface,
-    IDispatchImpl,
-    NativeCallable,
+    ParamArray,
     DefaultMember,
-    UnmanagedFunctionPointer,
-    ManagedToNativeComInteropStub,
-    ThreadStatic,
+    DisablePrivateReflectionType,
     FixedAddressValueType,
     UnsafeValueType,
-    IsByRefLike,
-    Intrinsic,
-    WinRTMarshalingBehaviorAttribute,
-    DefaultDllImportSearchPaths,
-    LCIDConversion,
-    ComVisible,
-    ComCompatibleVersion,
+    BestFitMapping,
     ClassInterface,
+    CoClass,
+    ComCompatibleVersion,
     ComDefaultInterface,
+    ComEventInterface,
     ComSourceInterfaces,
-    TypeIdentifier,
-    ParamArray,
+    ComVisible,
+    DefaultDllImportSearchPaths,
     Guid,
+    LCIDConversion,
+    IDispatchImpl,
+    ImportedFromTypeLib,
+    Intrinsic,
+    IsByRefLike,
+    PrimaryInteropAssembly,
+    ManagedToNativeComInteropStub,
+    NativeCallable,
+    TypeIdentifier,
+    UnmanagedFunctionPointer,
+    ThreadStatic,
+    WinRTMarshalingBehaviorAttribute,
 
     CountOfWellKnownAttributes
 };
@@ -41,82 +42,60 @@ const char *GetWellKnownAttributeName(WellKnownAttribute attribute)
 {
     switch (attribute)
     {
-        case WellKnownAttribute::DisablePrivateReflectionType:
-            return "System.Runtime.CompilerServices.DisablePrivateReflectionAttribute";
-        case WellKnownAttribute::ImportedFromTypeLib:
-            return "System.Runtime.InteropServices.ImportedFromTypeLibAttribute";
-        case WellKnownAttribute::PrimaryInteropAssembly:
-            return "System.Runtime.InteropServices.PrimaryInteropAssemblyAttribute";
-        case WellKnownAttribute::CoClass:
-            return "System.Runtime.InteropServices.CoClassAttribute";
-        case WellKnownAttribute::ComEventInterface:
-            return "System.Runtime.InteropServices.ComEventInterfaceAttribute";
-        case WellKnownAttribute::IDispatchImpl:
-            return "System.Runtime.InteropServices.IDispatchImplAttribute";
-        case WellKnownAttribute::NativeCallable:
-            return "System.Runtime.InteropServices.NativeCallableAttribute";
+        case WellKnownAttribute::ParamArray:
+            return "System.ParamArrayAttribute";
         case WellKnownAttribute::DefaultMember:
             return "System.Reflection.DefaultMemberAttribute";
-        case WellKnownAttribute::UnmanagedFunctionPointer:
-            return "System.Runtime.InteropServices.UnmanagedFunctionPointerAttribute";
-        case WellKnownAttribute::ManagedToNativeComInteropStub:
-            return "System.Runtime.InteropServices.ManagedToNativeComInteropStubAttribute";
-        case WellKnownAttribute::ThreadStatic:
-            return "System.ThreadStaticAttribute";
+        case WellKnownAttribute::DisablePrivateReflectionType:
+            return "System.Runtime.CompilerServices.DisablePrivateReflectionAttribute";
         case WellKnownAttribute::FixedAddressValueType:
             return "System.Runtime.CompilerServices.FixedAddressValueTypeAttribute";
         case WellKnownAttribute::UnsafeValueType:
             return "System.Runtime.CompilerServices.UnsafeValueTypeAttribute";
-        case WellKnownAttribute::IsByRefLike:
-            return "System.Runtime.CompilerServices.IsByRefLikeAttribute";
-        case WellKnownAttribute::Intrinsic:
-            return "System.Runtime.CompilerServices.IntrinsicAttribute";
-        case WellKnownAttribute::WinRTMarshalingBehaviorAttribute:
-            return "Windows.Foundation.Metadata.MarshalingBehaviorAttribute";
-        case WellKnownAttribute::DefaultDllImportSearchPaths:
-            return "System.Runtime.InteropServices.DefaultDllImportSearchPathsAttribute";
-        case WellKnownAttribute::LCIDConversion:
-            return "System.Runtime.InteropServices.LCIDConversionAttribute";
-        case WellKnownAttribute::ComVisible:
-            return "System.Runtime.InteropServices.ComVisibleAttribute";
-        case WellKnownAttribute::ComCompatibleVersion:
-            return "System.Runtime.InteropServices.ComCompatibleVersionAttribute";
         case WellKnownAttribute::BestFitMapping:
             return "System.Runtime.InteropServices.BestFitMappingAttribute";
         case WellKnownAttribute::ClassInterface:
             return "System.Runtime.InteropServices.ClassInterfaceAttribute";
+        case WellKnownAttribute::CoClass:
+            return "System.Runtime.InteropServices.CoClassAttribute";
+        case WellKnownAttribute::ComCompatibleVersion:
+            return "System.Runtime.InteropServices.ComCompatibleVersionAttribute";
         case WellKnownAttribute::ComDefaultInterface:
             return "System.Runtime.InteropServices.ComDefaultInterfaceAttribute";
+        case WellKnownAttribute::ComEventInterface:
+            return "System.Runtime.InteropServices.ComEventInterfaceAttribute";
         case WellKnownAttribute::ComSourceInterfaces:
             return "System.Runtime.InteropServices.ComSourceInterfacesAttribute";
-        case WellKnownAttribute::TypeIdentifier:
-            return "System.Runtime.InteropServices.TypeIdentifierAttribute";
-        case WellKnownAttribute::ParamArray:
-            return "System.ParamArrayAttribute";
+        case WellKnownAttribute::ComVisible:
+            return "System.Runtime.InteropServices.ComVisibleAttribute";
+        case WellKnownAttribute::DefaultDllImportSearchPaths:
+            return "System.Runtime.InteropServices.DefaultDllImportSearchPathsAttribute";
         case WellKnownAttribute::Guid:
             return "System.Runtime.InteropServices.GuidAttribute";
-        case WellKnownAttribute::
-            return ;
-        case WellKnownAttribute::
-            return ;
-        case WellKnownAttribute::
-            return ;
-        case WellKnownAttribute::
-            return ;
-        case WellKnownAttribute::
-            return ;
-        case WellKnownAttribute::
-            return ;
-        case WellKnownAttribute::
-            return ;
-        case WellKnownAttribute::
-            return ;
-        case WellKnownAttribute::
-            return ;
-        case WellKnownAttribute::
-            return ;
-        case WellKnownAttribute::
-            return ;
+        case WellKnownAttribute::LCIDConversion:
+            return "System.Runtime.InteropServices.LCIDConversionAttribute";
+        case WellKnownAttribute::IDispatchImpl:
+            return "System.Runtime.InteropServices.IDispatchImplAttribute";
+        case WellKnownAttribute::ImportedFromTypeLib:
+            return "System.Runtime.InteropServices.ImportedFromTypeLibAttribute";
+        case WellKnownAttribute::Intrinsic:
+            return "System.Runtime.CompilerServices.IntrinsicAttribute";
+        case WellKnownAttribute::IsByRefLike:
+            return "System.Runtime.CompilerServices.IsByRefLikeAttribute";
+        case WellKnownAttribute::PrimaryInteropAssembly:
+            return "System.Runtime.InteropServices.PrimaryInteropAssemblyAttribute";
+        case WellKnownAttribute::ManagedToNativeComInteropStub:
+            return "System.Runtime.InteropServices.ManagedToNativeComInteropStubAttribute";
+        case WellKnownAttribute::NativeCallable:
+            return "System.Runtime.InteropServices.NativeCallableAttribute";
+        case WellKnownAttribute::TypeIdentifier:
+            return "System.Runtime.InteropServices.TypeIdentifierAttribute";
+        case WellKnownAttribute::UnmanagedFunctionPointer:
+            return "System.Runtime.InteropServices.UnmanagedFunctionPointerAttribute";
+        case WellKnownAttribute::ThreadStatic:
+            return "System.ThreadStaticAttribute";
+        case WellKnownAttribute::WinRTMarshalingBehaviorAttribute:
+            return "Windows.Foundation.Metadata.MarshalingBehaviorAttribute";
     }
     _ASSERTE(false); // Should not be possible
     return nullptr;
