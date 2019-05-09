d9a70b3b9deaf2e495ed63a702752d2faa1912b3
Egor Chesakov <Egor.Chesakov@microsoft.com>
2019-05-09 16:42:43 -0700

    Decorate DllMain with DLLEXPORT in superpmi-shim-collector.cpp superpmi-shim-counter.cpp superpmi-shim-simple.cpp

--- src/ToolBox/superpmi/superpmi-shim-collector/superpmi-shim-collector.cpp
+++ src/ToolBox/superpmi/superpmi-shim-collector/superpmi-shim-collector.cpp
@@ -86,10 +86,10 @@ void SetLogFilePath()
     }
 }
 
-extern "C" BOOL
-#ifndef FEATURE_PAL
-    APIENTRY
-#endif // !FEATURE_PAL
+#ifdef FEATURE_PAL
+DLLEXPORT // For Win32 PAL LoadLibrary emulation
+#endif
+    extern "C" BOOL
     DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
 {
     switch (ul_reason_for_call)
