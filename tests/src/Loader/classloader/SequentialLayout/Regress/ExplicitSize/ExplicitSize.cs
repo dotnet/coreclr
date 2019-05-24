098848c595fd2839714a76e8adea19317bb1e8ea
Jeremy Koritzinsky <jekoritz@microsoft.com>
2019-05-24 13:16:16 -0700

    PR Feedback.

--- tests/src/Loader/classloader/SequentialLayout/Regress/ExplicitSize/ExplicitSize.cs
+++ tests/src/Loader/classloader/SequentialLayout/Regress/ExplicitSize/ExplicitSize.cs
@@ -2,9 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-// The struct has an objref and is of odd size.
-// The GC requires that all valuetypes containing objrefs be sized to a multiple of sizeof(void*) )== 4).
-// Since the size of this struct was 17 we were throwing a TypeLoadException.
 using System;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -19,7 +16,7 @@ unsafe class Program
 
     static int Main()
     {
-        Guid initialGuid = Guid.NewGuid();
+        Guid initialGuid = Guid.Parse("E6218D43-3C16-48BF-9C3C-8076FF5AFCD0");
         GUID g = default;
         Test(initialGuid, &g);
         return Unsafe.As<GUID, Guid>(ref g) == initialGuid ? 100 : 101;
