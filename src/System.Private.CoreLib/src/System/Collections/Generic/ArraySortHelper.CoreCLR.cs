7de0096f37765ab0bce7dff835ecc32adff1b604
nietras <nietras@users.noreply.github.com>
2019-06-02 13:00:48 +0200

    fix nullable
    Co-Authored-By: Jan Kotas <jkotas@microsoft.com>

--- src/System.Private.CoreLib/src/System/Collections/Generic/ArraySortHelper.CoreCLR.cs
+++ src/System.Private.CoreLib/src/System/Collections/Generic/ArraySortHelper.CoreCLR.cs
@@ -12,7 +12,7 @@ namespace System.Collections.Generic
 {
     internal interface IArraySortHelper<TKey>
     {
-        void Sort(Span<TKey> keys, int index, int length, IComparer<TKey> comparer);
+        void Sort(Span<TKey> keys, int index, int length, IComparer<TKey>? comparer);
         int BinarySearch(TKey[] keys, int index, int length, TKey value, IComparer<TKey>? comparer);
     }
 
