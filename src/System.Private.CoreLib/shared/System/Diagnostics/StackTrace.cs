85fd40898dda386409175d36ad537a1c27203990
Dan Moseley <danmose@microsoft.com>
2019-06-09 20:42:53 -0700

    Remove EDI boundary?

--- src/System.Private.CoreLib/shared/System/Diagnostics/StackTrace.cs
+++ src/System.Private.CoreLib/shared/System/Diagnostics/StackTrace.cs
@@ -319,13 +319,6 @@ namespace System.Diagnostics
                             sb.AppendFormat(CultureInfo.InvariantCulture, inFileLineNum, fileName, sf.GetFileLineNumber());
                         }
                     }
-
-                    // Skip EDI boundary for async
-                    if (sf.IsLastFrameFromForeignExceptionStackTrace && !isAsync)
-                    {
-                        sb.Append(Environment.NewLine);
-                        sb.Append(SR.Exception_EndStackTraceFromPreviousThrow);
-                    }
                 }
             }
 
