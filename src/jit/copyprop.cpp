5f8870c1fc4a6deeb3a0bbec2c4b2362ecfa259b
Carol Eidt <carol.eidt@microsoft.com>
2019-06-19 11:46:29 -0700

    Change condition

--- src/jit/copyprop.cpp
+++ src/jit/copyprop.cpp
@@ -180,9 +180,13 @@ void Compiler::optCopyProp(BasicBlock* block, GenTreeStmt* stmt, GenTree* tree, 
             continue;
         }
 
-        // Do not copy propagate if the old lclVar was 'doNotEnregister', but not the new one, in case
-        // this is an address-taken context.
-        if (lvaTable[lclNum].lvDoNotEnregister && !lvaTable[newLclNum].lvDoNotEnregister)
+        // Do not copy propagate if the old and new lclVar have different 'doNotEnregister' settings.
+        // This is primarily to avoid copy propagating to IND(ADDR(LCL_VAR)) where the replacement lclVar
+        // is not marked 'lvDoNotEnregister'.
+        // However, in addition, it may not be profitable to propagate a 'doNotEnregister' lclVar to an
+        // existing use of an enregisterable lclVar.
+
+        if (lvaTable[lclNum].lvDoNotEnregister != lvaTable[newLclNum].lvDoNotEnregister)
         {
             continue;
         }
