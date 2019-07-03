3cbb26b33fe545988eaa71ad71a202938a56b4f9
José Rivero <jorive@microsoft.com>
2019-07-03 12:26:51 -0700

    [WIP]

--- src/vm/diagnosticserver.cpp
+++ src/vm/diagnosticserver.cpp
@@ -20,20 +20,21 @@
 #ifdef FEATURE_PERFTRACING
 
 IpcStream::DiagnosticsIpc *DiagnosticServer::s_pIpc = nullptr;
+Volatile<bool> DiagnosticServer::s_shuttingDown(false);
+HANDLE DiagnosticServer::s_hServerThread = INVALID_HANDLE_VALUE;
 
-static DWORD WINAPI DiagnosticsServerThread(LPVOID lpThreadParameter)
+DWORD WINAPI DiagnosticServer::DiagnosticsServerThread(LPVOID)
 {
     CONTRACTL
     {
         NOTHROW;
         GC_TRIGGERS;
         MODE_PREEMPTIVE;
-        PRECONDITION(lpThreadParameter != nullptr);
+        PRECONDITION(s_pIpc != nullptr);
     }
     CONTRACTL_END;
 
-    auto pIpc = reinterpret_cast<IpcStream::DiagnosticsIpc *>(lpThreadParameter);
-    if (pIpc == nullptr)
+    if (s_pIpc == nullptr)
     {
         STRESS_LOG0(LF_DIAGNOSTICS_PORT, LL_ERROR, "Diagnostics IPC listener was undefined\n");
         return 1;
@@ -45,11 +46,11 @@ static DWORD WINAPI DiagnosticsServerThread(LPVOID lpThreadParameter)
 
     EX_TRY
     {
-        while (true)
+        while (!s_shuttingDown)
         {
             // FIXME: Ideally this would be something like a std::shared_ptr
-            IpcStream *pStream = pIpc->Accept(LoggingCallback);
-            
+            IpcStream *pStream = s_pIpc->Accept(LoggingCallback);
+
             if (pStream == nullptr)
                 continue;
 #ifdef FEATURE_AUTO_TRACE
@@ -140,7 +141,7 @@ bool DiagnosticServer::Initialize()
             auto_trace_launch();
 #endif
             DWORD dwThreadId = 0;
-            HANDLE hThread = ::CreateThread( // TODO: Is it correct to have this "lower" level call here?
+            s_hServerThread = ::CreateThread( // TODO: Is it correct to have this "lower" level call here?
                 nullptr,                     // no security attribute
                 0,                           // default stack size
                 DiagnosticsServerThread,     // thread proc
@@ -148,8 +149,11 @@ bool DiagnosticServer::Initialize()
                 0,                           // not suspended
                 &dwThreadId);                // returns thread ID
 
-            if (hThread == nullptr)
+            if (s_hServerThread == NULL)
             {
+                delete s_pIpc;
+                s_pIpc = nullptr;
+
                 // Failed to create IPC thread.
                 STRESS_LOG1(
                     LF_DIAGNOSTICS_PORT,                                 // facility
@@ -162,9 +166,6 @@ bool DiagnosticServer::Initialize()
 #ifdef FEATURE_AUTO_TRACE
                 auto_trace_wait();
 #endif
-                // FIXME: Maybe hold on to the thread to abort/cleanup at exit?
-                ::CloseHandle(hThread);
-
                 // TODO: Add error handling?
                 fSuccess = true;
             }
@@ -191,6 +192,8 @@ bool DiagnosticServer::Shutdown()
 
     bool fSuccess = false;
 
+    s_shuttingDown = true;
+
     EX_TRY
     {
         if (s_pIpc != nullptr)
@@ -203,7 +206,21 @@ bool DiagnosticServer::Shutdown()
                     code,                                                 // data1
                     szMessage);                                           // data2
             };
-            s_pIpc->Unlink(ErrorCallback);
+            s_pIpc->Close(ErrorCallback); // This will break the accept waiting for client connection.
+
+            if (s_hServerThread != NULL)
+            {
+#ifndef FEATURE_PAL
+                if (::CancelSynchronousIo(s_hServerThread) == 0)
+                    _ASSERTE(!"Failed to mark pending synchronous I/O operations issued by Diagnostics Server Thread as canceled.");
+#endif // FEATURE_PAL
+
+                ::WaitForSingleObject(s_hServerThread, INFINITE);
+                ::CloseHandle(s_hServerThread);
+            }
+
+            delete s_pIpc;
+            s_pIpc = nullptr;
         }
         fSuccess = true;
     }
