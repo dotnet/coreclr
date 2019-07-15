b6e2b0c6edebee745813736815e962da550a5a98
pi1024e <doremylover123@gmail.com>
2019-07-15 16:25:17 -0400

    Fixed

--- src/vm/arm/stubs.cpp
+++ src/vm/arm/stubs.cpp
@@ -530,7 +530,7 @@ void LazyMachState::unwindLazyState(LazyMachState* baseState,
 
     PCODE pvControlPc;
 
-    do
+    for(;;)
     {
 #ifndef FEATURE_PAL
         pvControlPc = Thread::VirtualUnwindCallFrame(&ctx, &nonVolRegPtrs);
@@ -580,7 +580,6 @@ void LazyMachState::unwindLazyState(LazyMachState* baseState,
                 break;
         }
     }
-    while(TRUE);
 
     //
     // Update unwoundState so that HelperMethodFrameRestoreState knows which
