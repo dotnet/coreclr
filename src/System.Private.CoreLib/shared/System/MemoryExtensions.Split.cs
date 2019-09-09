9576d4aa03388318f3aeccc6a7cf6c2404b2a1e0
Grant Dickinson <grant@jesanna.com>
2019-09-09 16:02:19 -0700

    Switch over enumerators

--- src/System.Private.CoreLib/shared/System/MemoryExtensions.Split.cs
+++ src/System.Private.CoreLib/shared/System/MemoryExtensions.Split.cs
@@ -7,34 +7,100 @@ namespace System
     public static partial class MemoryExtensions
     {
         public static SpanSplitEnumerator<char> Split(this ReadOnlySpan<char> span)
-            => new SpanSplitEnumerator<char>(span, ' '); // TODO: split on char.IsWhiteSpace
+            => new SpanSplitEnumerator<char>(new CharSpanSplitTrimEnumerator(span));
 
         public static SpanSplitEnumerator<char> Split(this ReadOnlySpan<char> span, char separator)
-            => new SpanSplitEnumerator<char>(span, separator);
+            => new SpanSplitEnumerator<char>(new CharSpanSplitByCharEnumerator(span, separator));
 
         public static SpanSplitEnumerator<char> Split(this ReadOnlySpan<char> span, string separator)
-            => new SpanSplitEnumerator<char>(span, separator[0]); // TODO: Fix impl
+            => new SpanSplitEnumerator<char>(new CharSpanSplitByStringEnumerator(span, separator));
 
         public ref struct SpanSplitEnumerator<T>
 #nullable disable // to enable use with both T and T? for reference types due to IEquatable<T> being invariant
             where T : IEquatable<T>
 #nullable restore
         {
-            private readonly ReadOnlySpan<T> _span;
-            private readonly T _separator;
+            private readonly CharSpanTrimKind _kind;
+            private readonly CharSpanSplitTrimEnumerator _enumeratorTrim;
+            private readonly CharSpanSplitByCharEnumerator _enumeratorChar;
+            private readonly CharSpanSplitByStringEnumerator _enumeratorString;
+
+            public SpanSplitEnumerator<T> GetEnumerator() => this;
+
+            internal SpanSplitEnumerator(CharSpanSplitTrimEnumerator enumerator)
+            {
+                _kind = CharSpanTrimKind.Trim;
+                _enumeratorTrim = enumerator;
+                _enumeratorChar = default;
+                _enumeratorString = default;
+            }
+
+            internal SpanSplitEnumerator(CharSpanSplitByCharEnumerator enumerator)
+            {
+                _kind = CharSpanTrimKind.Char;
+                _enumeratorTrim = default;
+                _enumeratorChar = enumerator;
+                _enumeratorString = default;
+            }
+
+            internal SpanSplitEnumerator(CharSpanSplitByStringEnumerator enumerator)
+            {
+                _kind = CharSpanTrimKind.String;
+                _enumeratorTrim = default;
+                _enumeratorChar = default;
+                _enumeratorString = enumerator;
+            }
+
+            public bool MoveNext()
+            {
+                switch (_kind)
+                {
+                    default: // TODO: Throw?
+                    case CharSpanTrimKind.Trim: return _enumeratorTrim.MoveNext();
+                    case CharSpanTrimKind.Char: return _enumeratorChar.MoveNext();
+                    case CharSpanTrimKind.String: return _enumeratorString.MoveNext();
+                }
+            }
+
+            public Range Current
+            {
+                get
+                {
+                    switch (_kind)
+                    {
+                        default: // TODO: Throw?
+                        case CharSpanTrimKind.Trim: return _enumeratorTrim.Current;
+                        case CharSpanTrimKind.Char: return _enumeratorChar.Current;
+                        case CharSpanTrimKind.String: return _enumeratorString.Current;
+                    }
+                }
+            }
+
+            private enum CharSpanTrimKind
+            {
+                Trim = 0,
+                Char,
+                String
+            }
+        }
+
+        internal ref struct CharSpanSplitTrimEnumerator
+        {
+            private readonly ReadOnlySpan<char> _span;
             private bool _started;
             private bool _ended;
             private int _start;
             private Range _current;
 
-            /// <summary>
-            /// Implements the IEnumerator pattern.
-            /// </summary>
-            public SpanSplitEnumerator<T> GetEnumerator() => this;
+            internal CharSpanSplitTrimEnumerator(ReadOnlySpan<char> span)
+            {
+                _span = span;
+                _started = false;
+                _ended = false;
+                _start = 0;
+                _current = default;
+            }
 
-            /// <summary>
-            /// Implements the IEnumerator pattern.
-            /// </summary>
             public bool MoveNext()
             {
                 _started = true;
@@ -45,7 +111,73 @@ namespace System
                     return false;
                 }
 
-                ReadOnlySpan<T> slice = _start == 0
+                ReadOnlySpan<char> slice = _start == 0
+                    ? _span
+                    : _span.Slice(_start);
+
+                int end = _start;
+                if (slice.Length > 0)
+                {
+                    int index = slice.IndexOfAny(' ', '\n'); // TODO: Fix
+
+                    if (index == -1)
+                    {
+                        index = slice.Length;
+                    }
+
+                    end += index;
+                }
+
+                _current = new Range(_start, end);
+                _start = end + 1;
+
+                return true;
+            }
+
+            public Range Current
+            {
+                get
+                {
+                    if (!_started || _ended)
+                    {
+                        ThrowHelper.ThrowInvalidOperationException_InvalidOperation_EnumOpCantHappen();
+                    }
+
+                    return _current;
+                }
+            }
+        }
+
+        internal ref struct CharSpanSplitByCharEnumerator
+        {
+            private readonly ReadOnlySpan<char> _span;
+            private readonly char _separator;
+            private bool _started;
+            private bool _ended;
+            private int _start;
+            private Range _current;
+
+            internal CharSpanSplitByCharEnumerator(ReadOnlySpan<char> span, char separator)
+            {
+                _span = span;
+                _separator = separator;
+                _started = false;
+                _ended = false;
+                _start = 0;
+                _current = default;
+            }
+
+            public bool MoveNext()
+            {
+                _started = true;
+
+                if (_start > _span.Length)
+                {
+                    _ended = true;
+                    return false;
+                }
+
+                ReadOnlySpan<char> slice = _start == 0
                     ? _span
                     : _span.Slice(_start);
 
@@ -68,9 +200,6 @@ namespace System
                 return true;
             }
 
-            /// <summary>
-            /// Implements the IEnumerator pattern.
-            /// </summary>
             public Range Current
             {
                 get
@@ -83,8 +212,18 @@ namespace System
                     return _current;
                 }
             }
+        }
 
-            internal SpanSplitEnumerator(ReadOnlySpan<T> span, T separator)
+        internal ref struct CharSpanSplitByStringEnumerator
+        {
+            private readonly ReadOnlySpan<char> _span;
+            private readonly string _separator;
+            private bool _started;
+            private bool _ended;
+            private int _start;
+            private Range _current;
+
+            internal CharSpanSplitByStringEnumerator(ReadOnlySpan<char> span, string separator)
             {
                 _span = span;
                 _separator = separator;
@@ -92,6 +231,52 @@ namespace System
                 _ended = false;
                 _start = 0;
                 _current = default;
+            }
+
+            public bool MoveNext()
+            {
+                _started = true;
+
+                if (_start > _span.Length)
+                {
+                    _ended = true;
+                    return false;
+                }
+
+                ReadOnlySpan<char> slice = _start == 0
+                    ? _span
+                    : _span.Slice(_start);
+
+                int end = _start;
+                if (slice.Length > 0)
+                {
+                    int index = slice.IndexOf(_separator);
+
+                    if (index == -1)
+                    {
+                        index = slice.Length;
+                    }
+
+                    end += index;
+                }
+
+                _current = new Range(_start, end);
+                _start = end + 1;
+
+                return true;
+            }
+
+            public Range Current
+            {
+                get
+                {
+                    if (!_started || _ended)
+                    {
+                        ThrowHelper.ThrowInvalidOperationException_InvalidOperation_EnumOpCantHappen();
+                    }
+
+                    return _current;
+                }
             }
         }
     }
