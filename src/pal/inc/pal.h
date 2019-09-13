8cc95e995b875fb34ad79d5907729d2135f90ba4
Jan Kotas <jkotas@microsoft.com>
2019-09-13 13:03:28 -0700

    Deleting more unused methods from PAL

--- src/pal/inc/pal.h
+++ src/pal/inc/pal.h
@@ -4459,18 +4459,6 @@ PALIMPORT DLLEXPORT void * __cdecl bsearch(const void *, const void *, size_t, s
 
 PALIMPORT time_t __cdecl time(time_t *);
 
-struct tm {
-        int tm_sec;     /* seconds after the minute - [0,59] */
-        int tm_min;     /* minutes after the hour - [0,59] */
-        int tm_hour;    /* hours since midnight - [0,23] */
-        int tm_mday;    /* day of the month - [1,31] */
-        int tm_mon;     /* months since January - [0,11] */
-        int tm_year;    /* years since 1900 */
-        int tm_wday;    /* days since Sunday - [0,6] */
-        int tm_yday;    /* days since January 1 - [0,365] */
-        int tm_isdst;   /* daylight savings time flag */
-        };
-
 #endif // !PAL_STDCPP_COMPAT
 
 PALIMPORT int __cdecl _open_osfhandle(INT_PTR, int);
