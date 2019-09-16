da718b4753feef7eb1cfc441cd2706e718fac4c1
David Wrighton <davidwr@microsoft.com>
2019-09-16 15:22:41 -0700

    IBC fixes - Support IntPtr and UIntPtr element types in IBC data - More defensive coding against structurally incorrect IBC data - Skip compilation of open method definitions - Generate IBC data without crashing when the first logged item is an InstantiatedMethod and not a token - Safely shutdown and emit IBC data from a thread that hasn't been given a Thread object

--- src/vm/ceemain.cpp
+++ src/vm/ceemain.cpp
@@ -1449,8 +1449,18 @@ void STDMETHODCALLTYPE EEShutDownHelper(BOOL fIsDllUnloading)
             {
                 if (g_IBCLogger.InstrEnabled())
                 {
-                    Thread * pThread = GetThread();
-                    ThreadLocalIBCInfo* pInfo = pThread->GetIBCInfo();
+                    Thread * pThread = GetThreadNULLOk();
+                    ThreadLocalIBCInfo* pInfo = NULL;
+
+                    if (pThread != NULL)
+                    {
+                        if (pInfo == NULL) 
+                        { 
+                            CONTRACT_VIOLATION( ThrowsViolation | FaultViolation); 
+                            pInfo = new ThreadLocalIBCInfo(); 
+                            pThread->SetIBCInfo(pInfo); 
+                        } 
+                    }
 
                     // Acquire the Crst lock before creating the IBCLoggingDisabler object.
                     // Only one thread at a time can be processing an IBC logging event.
