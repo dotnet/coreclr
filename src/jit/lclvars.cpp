54e754e857bf492e7be6c27f41df05137e19a969
Carol Eidt <carol.eidt@microsoft.com>
2019-09-20 19:27:33 -0700

    Copy live sets when splitting blocks
    With #26456 we may generate labels for split blocks (even though there's no associated code). Therefore we need to ensure that the live sets are correct.
    Also, cleanup some miscellaneous dumping code.
    
    Fix #26795

--- src/jit/lclvars.cpp
+++ src/jit/lclvars.cpp
@@ -6853,6 +6853,10 @@ void Compiler::lvaDumpEntry(unsigned lclNum, FrameLayoutState curState, size_t r
     {
         printf(" exact");
     }
+    if (varDsc->lvLiveInOutOfHndlr)
+    {
+        printf(" EH-live");
+    }
 #ifndef _TARGET_64BIT_
     if (varDsc->lvStructDoubleAlign)
         printf(" double-align");
