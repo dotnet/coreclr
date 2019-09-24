cb7531e106ae9fc1c138ac50f784bf2b3d9da362
Tomas <trylek@microsoft.com>
2019-09-24 22:15:00 +0200

    Address Sven's PR feedback; test one remaining failure in Pri#1 tests
    I found out that, even my fix for the mock host policy native library
    extension, the AssemblyDependencyResolverTests are still failing. The
    problem is caused by the fact that the file
    
    <pre>
    tests/src/Common/CoreCLRTestLibrary/XPlatformUtils.cs
    </pre>
    
    uses conditional compilation to choose native library "decoration"
    style and that naturally doesn't work when we try to run an OSX-built
    test on Linux. I have changed the source file to use runtime
    OS detection.
    
    I originally tried to use RuntimeInformation but that was causing
    problems in some interop tests explicitly building against
    System.Private.CoreLib so I ended up using Environment.OSVersion
    instead.
    
    This change temporarily bumps up the offending test to Pri#0 so that
    I don't have to run the lengthy outerloop pipeline every time
    I test a change to the scripts. Once I have a passing test, I'm
    going to remove the instrumentation.
    
    Thanks
    
    Tomas

--- tests/src/Common/CoreCLRTestLibrary/XPlatformUtils.cs
+++ tests/src/Common/CoreCLRTestLibrary/XPlatformUtils.cs
@@ -2,25 +2,25 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
+
 namespace TestLibrary
 {
     public class XPlatformUtils
     {
-#if PLATFORM_WINDOWS
-        public const string NativeLibraryPrefix = "";
-        public const string NativeLibrarySuffix = ".dll";
-#else
-        public const string NativeLibraryPrefix = "lib";
-#if PLATFORM_OSX
-        public const string NativeLibrarySuffix = ".dylib";
-#else
-        public const string NativeLibrarySuffix = ".so";
-#endif
-#endif
-
         public static string GetStandardNativeLibraryFileName(string simpleName)
         {
-            return NativeLibraryPrefix + simpleName + NativeLibrarySuffix;
+            switch (Environment.OSVersion.Platform)
+            {
+                case PlatformID.Win32NT:
+                    return simpleName + ".dll";
+
+                case PlatformID.MacOSX:
+                    return "lib" + simpleName + ".dylib";
+
+                default:
+                    return "lib" + simpleName + ".so";
+            }
         }
     }
 }
