ca6dd85e27d6994d53495e4686e839d3c1b3da4a
Jakob Botsch Nielsen <t-janie@microsoft.com>
2019-09-27 05:22:57 +0000

    Add csproj and bash script for crossgen2 ThunkGenerator

--- src/tools/crossgen2/Common/JitInterface/ThunkGenerator/gen.bat
+++ src/tools/crossgen2/Common/JitInterface/ThunkGenerator/gen.bat
@@ -1,3 +1,1 @@
-csc Program.cs
-Program.exe ThunkInput.txt ..\CorInfoBase.cs ..\..\..\jitinterface\jitinterface.h
-del Program.exe
+dotnet run -- ThunkInput.txt ..\CorInfoBase.cs ..\..\..\jitinterface\jitinterface.h
