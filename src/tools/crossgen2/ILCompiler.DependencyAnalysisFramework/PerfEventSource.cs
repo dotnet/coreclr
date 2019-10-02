39d9a75f78e423aa23b805f6aab48d1b58b95ad4
Anubhav Srivastava <t-ansri@microsoft.com>
2019-10-01 17:05:18 -0700

    Change PerfEventSource to be able to use using.

--- src/tools/crossgen2/ILCompiler.DependencyAnalysisFramework/PerfEventSource.cs
+++ src/tools/crossgen2/ILCompiler.DependencyAnalysisFramework/PerfEventSource.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using System.Diagnostics.Tracing;
 
 /// <summary>
@@ -10,22 +11,49 @@ using System.Diagnostics.Tracing;
 namespace ILCompiler.DependencyAnalysisFramework
 {
     // The event IDs here must not collide with the ones used by ReadyToRunPerfEventSource.cs
-    [EventSource(Name = "Microsoft-ILCompiler-Perf")]
-    class PerfEventSource : EventSource
+    struct StartStopEvents : IDisposable
     {
-        [Event(1001, Level = EventLevel.Informational)]
-        public void GraphProcessingStart() { WriteEvent(1001); }
-        [Event(1002, Level = EventLevel.Informational)]
-        public void GraphProcessingStop() { WriteEvent(1002); }
+        private Action _stopAction;
+        public void Dispose()
+        {
+            _stopAction();
+        }
 
-        [Event(1003, Level = EventLevel.Informational)]
-        public void DependencyAnalysisStart() { WriteEvent(1003); }
-        [Event(1004, Level = EventLevel.Informational)]
-        public void DependencyAnalysisStop() { WriteEvent(1004); }
+        private StartStopEvents(Action startAction, Action stopAction)
+        {
+            startAction();
+            _stopAction = stopAction;
+        }
 
-        [Event(1005, Level = EventLevel.Informational)]
-        public void AddedNodeToMarkStack() { WriteEvent(1005); }
+        [EventSource(Name = "Microsoft-ILCompiler-Perf")]
+        public class PerfEventSource : EventSource
+        {
+            public static PerfEventSource Log = new PerfEventSource();
 
-        public static PerfEventSource Log = new PerfEventSource();
+            private PerfEventSource() {}
+
+            public StartStopEvents GraphProcessingEvents()
+            {
+                return new StartStopEvents(GraphProcessingStart, GraphProcessingStop);
+            }
+
+            public StartStopEvents DependencyAnalysisEvents()
+            {
+                return new StartStopEvents(DependencyAnalysisStart, DependencyAnalysisStop);
+            }
+
+            [Event(1001, Level = EventLevel.Informational)]
+            private void GraphProcessingStart() { WriteEvent(1001); }
+            [Event(1002, Level = EventLevel.Informational)]
+            private void GraphProcessingStop() { WriteEvent(1002); }
+
+            [Event(1003, Level = EventLevel.Informational)]
+            private void DependencyAnalysisStart() { WriteEvent(1003); }
+            [Event(1004, Level = EventLevel.Informational)]
+            private void DependencyAnalysisStop() { WriteEvent(1004); }
+
+            [Event(1005, Level = EventLevel.Informational)]
+            public void AddedNodeToMarkStack() { WriteEvent(1005); }
+        }
     }
 }
--- src/tools/crossgen2/ILCompiler.ReadyToRun/Compiler/PerfEventSource.cs
+++ src/tools/crossgen2/ILCompiler.ReadyToRun/Compiler/PerfEventSource.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using System.Diagnostics.Tracing;
 
 /// <summary>
@@ -10,29 +11,76 @@ using System.Diagnostics.Tracing;
 namespace ILCompiler
 {
     // The event IDs here must not collide with the ones used by DependencyAnalysis' PerfEventSource
-    [EventSource(Name = "Microsoft-ILCompiler-Perf")]
-    public class PerfEventSource : EventSource
+    public struct StartStopEvents : IDisposable
     {
-        [Event(1, Level = EventLevel.Informational)]
-        public void LoadingStart() { WriteEvent(1); }
-        [Event(2, Level = EventLevel.Informational)]
-        public void LoadingStop() { WriteEvent(2); }
+        private Action _stopAction;
+        public void Dispose()
+        {
+            _stopAction();
+        }
 
-        [Event(3, Level = EventLevel.Informational)]
-        public void EmittingStart() { WriteEvent(3); }
-        [Event(4, Level = EventLevel.Informational)]
-        public void EmittingStop() { WriteEvent(4); }
+        private StartStopEvents(Action startAction, Action stopAction)
+        {
+            startAction();
+            _stopAction = stopAction;
+        }
 
-        [Event(5, Level = EventLevel.Informational)]
-        public void CompilationStart() { WriteEvent(5); }
-        [Event(6, Level = EventLevel.Informational)]
-        public void CompilationStop() { WriteEvent(6); }
+        [EventSource(Name = "Microsoft-ILCompiler-Perf")]
+        public class PerfEventSource : EventSource
+        {
+            public static PerfEventSource Log = new PerfEventSource();
 
-        [Event(7, Level = EventLevel.Informational)]
-        public void JitStart() { WriteEvent(7); }
-        [Event(8, Level = EventLevel.Informational)]
-        public void JitStop() { WriteEvent(8); }
+            private PerfEventSource() { }
 
-        public static PerfEventSource Log = new PerfEventSource();
+            public StartStopEvents LoadingEvents()
+            {
+                return new StartStopEvents(LoadingStart, LoadingStop);
+            }
+
+            public StartStopEvents EmittingEvents()
+            {
+                return new StartStopEvents(EmittingStart, EmittingStop);
+            }
+
+            public StartStopEvents CompilationEvents()
+            {
+                return new StartStopEvents(CompilationStart, CompilationStop);
+            }
+
+            public StartStopEvents JitEvents()
+            {
+                return new StartStopEvents(JitStart, JitStop);
+            }
+
+            public StartStopEvents JitMethodEvents()
+            {
+                return new StartStopEvents(JitMethodStart, JitMethodStop);
+            }
+
+            [Event(1, Level = EventLevel.Informational)]
+            private void LoadingStart() { WriteEvent(1); }
+            [Event(2, Level = EventLevel.Informational)]
+            private void LoadingStop() { WriteEvent(2); }
+
+            [Event(3, Level = EventLevel.Informational)]
+            private void EmittingStart() { WriteEvent(3); }
+            [Event(4, Level = EventLevel.Informational)]
+            private void EmittingStop() { WriteEvent(4); }
+
+            [Event(5, Level = EventLevel.Informational)]
+            private void CompilationStart() { WriteEvent(5); }
+            [Event(6, Level = EventLevel.Informational)]
+            private void CompilationStop() { WriteEvent(6); }
+
+            [Event(7, Level = EventLevel.Informational)]
+            private void JitStart() { WriteEvent(7); }
+            [Event(8, Level = EventLevel.Informational)]
+            private void JitStop() { WriteEvent(8); }
+
+            [Event(9, Level = EventLevel.Informational)]
+            private void JitMethodStart() { WriteEvent(9); }
+            [Event(10, Level = EventLevel.Informational)]
+            private void JitMethodStop() { WriteEvent(10); }
+        }
     }
 }
