6a28ce48302f350552550f33895f73c231cdb22a
Sung Yoon Whang <suwhang@microsoft.com>
2019-10-17 14:00:46 -0700

    Some changes to comment

--- src/vm/eventpipe.cpp
+++ src/vm/eventpipe.cpp
@@ -45,7 +45,7 @@ extern "C"
 #endif
 void InitProvidersAndEvents();
 
-// This is a wrapper method for LTTng.
+// This is a wrapper method for LTTng. See https://github.com/dotnet/coreclr/pull/27273 for details.
 extern "C" bool XplatEventLoggerIsEnabled()
 {
     return XplatEventLogger::IsEventLoggingEnabled();
