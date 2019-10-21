c626352cba8151c62d5db8d5d21db11afb927e03
Michal Strehovský <michals@microsoft.com>
2019-10-21 16:31:13 +0200

    NativeTypeKind.Invalid -> NativeTypeKind.Default

--- src/tools/crossgen2/Common/TypeSystem/Interop/MarshalAsDescriptor.cs
+++ src/tools/crossgen2/Common/TypeSystem/Interop/MarshalAsDescriptor.cs
@@ -37,7 +37,7 @@ namespace Internal.TypeSystem
         LPStruct = 0x2b,    // This is not  defined in Ecma-335(II.23.4)
         CustomMarshaler = 0x2c,
         LPUTF8Str = 0x30,
-        Invalid = 0x50,      // This is the default value
+        Default = 0x50,      // This is the default value
         Variant = 0x51,
     }
 
