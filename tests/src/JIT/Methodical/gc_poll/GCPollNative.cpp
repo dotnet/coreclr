64e751657dd3a60c84ddcdde8356e3ef73fa46e1
Aaron Robinson <arobins@microsoft.com>
2019-10-28 19:58:53 -0700

    JIT test for insertion of GCPoll

---
+++ tests/src/JIT/Methodical/gc_poll/GCPollNative.cpp
@@ -0,0 +1,36 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+#include <cstdio>
+#include <cstdint>
+#include <atomic>
+
+#if defined(_MSC_VER)
+    #define STDMETHODCALLTYPE __stdcall
+    #define EXPORT(type) extern "C" type __declspec(dllexport)
+#else
+    #define STDMETHODCALLTYPE __attribute__((stdcall))
+    #define EXPORT(type) extern "C" __attribute__((visibility("default"))) type
+#endif
+
+namespace
+{
+    std::atomic<uint64_t> _n = 0;
+
+    template<typename T>
+    T NextUInt(T t)
+    {
+        return (T)((++_n) + t);
+    }
+}
+
+EXPORT(uint32_t) STDMETHODCALLTYPE NextUInt32(uint32_t t)
+{
+    return NextUInt(t);
+}
+
+EXPORT(uint64_t) STDMETHODCALLTYPE NextUInt64(uint64_t t)
+{
+    return NextUInt(t);
+}
