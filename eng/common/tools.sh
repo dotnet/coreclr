e41276e4013a25a8718438e1bc613ee7c3905c2a
Christopher Costa <chcosta@microsoft.com>
2019-10-31 22:30:22 +0000

    Update logic

--- eng/common/tools.sh
+++ eng/common/tools.sh
@@ -272,7 +272,7 @@ function GetNuGetPackageCachePath {
 }
 
 function InitializeNativeTools() {
-  if [[ -z "${DisableNativeToolsetInstalls:-}" ]]; then
+  if [[ -n "${DisableNativeToolsetInstalls:-}" ]]; then
     return
   fi
   if grep -Fq "native-tools" $global_json_file
