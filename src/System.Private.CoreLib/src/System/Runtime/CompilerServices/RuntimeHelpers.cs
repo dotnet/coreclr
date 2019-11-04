dce8a7bd2e8004de3d034e78ab43f1f83c564652
Jan Kotas <jkotas@microsoft.com>
2019-11-04 12:26:57 -0800

    Fix Array.Copy regressions for large multidimensional arrays

--- src/System.Private.CoreLib/src/System/Runtime/CompilerServices/RuntimeHelpers.cs
+++ src/System.Private.CoreLib/src/System/Runtime/CompilerServices/RuntimeHelpers.cs
@@ -223,6 +223,7 @@ namespace System.Runtime.CompilerServices
 
         // Returns true iff the object has a component size;
         // i.e., is variable length like System.String or Array.
+        // Callers are required to keep obj alive
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal static unsafe bool ObjectHasComponentSize(object obj)
         {
