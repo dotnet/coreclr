356d84c5f04fe7561b1d10a15661431e4f9c9b5a
David Wrighton <davidwr@microsoft.com>
2019-11-07 16:28:42 -0800

    Fix handling of callvirt to instance methods on interface types that are not virtual - Use call type to indicate if its non-virtual or not, instead of opcode

--- tests/src/Loader/classloader/DefaultInterfaceMethods/sharedgenerics/non_virtual_calls_to_instance_methods.cs
+++ tests/src/Loader/classloader/DefaultInterfaceMethods/sharedgenerics/non_virtual_calls_to_instance_methods.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 
 namespace Sample
 {
-    public sealed class C1 : I1<string>
+    public sealed class C1 : I1<string>, I2
     {
     }
 
@@ -11,22 +11,76 @@ namespace Sample
     {
         static int Main(string[] args)
         {
-            Test1();
-            Test2();
+            CallOpcodeNonGenericInterface();
+            CallOpcodeNonGenericInterfaceGenericMethod();
+            CallOpcodeGenericInterface();
+            CallOpcodeGenericInterfaceGenericMethod();
+            CallVirtOpcodeNonGenericInterface();
+            CallVirtOpcodeNonGenericInterfaceGenericMethod();
+            CallVirtOpcodeGenericInterface();
+            CallVirtOpcodeGenericInterfaceGenericMethod();
+
             return 100;
         }
 
-        private static void Test1()
+        private static void CallOpcodeNonGenericInterface()
         {
-            if (((I1<string>)new C1()).GetItemType() != typeof(string))
-                throw new Exception("Test1 failed");
+            Console.WriteLine("Testing call opcode for calling DIM on non-generic interface non-generic method");
+            if (((I2)new C1()).GetItemTypeNonGeneric(typeof(string)) != typeof(string))
+                throw new Exception("CallOpcodeGenericInterface failed");
         }
 
-        private static void Test2()
+        private static void CallOpcodeNonGenericInterfaceGenericMethod()
         {
+            Console.WriteLine("Testing call opcode for calling DIM on non-generic interface non-generic method");
+            if (((I2)new C1()).GetItemTypeGeneric<object>() != typeof(object))
+                throw new Exception("CallOpcodeGenericInterface failed");
+        }
+
+        private static void CallOpcodeGenericInterface()
+        {
+            Console.WriteLine("Testing call opcode for calling DIM on generic interface");
+            if (((I1<string>)new C1()).GetItemType() != typeof(string))
+                throw new Exception("CallOpcodeGenericInterface failed");
+        }
+
+        private static void CallOpcodeGenericInterfaceGenericMethod()
+        {
+            Console.WriteLine("Testing call opcode for calling generic method on DIM on generic interface");
+            if (((I1<string>)new C1()).GetItemTypeMethod<object>() != typeof(object))
+                throw new Exception("CallOpcodeGenericInterface failed");
+        }
+
+        private static void CallVirtOpcodeNonGenericInterface()
+        {
+            Console.WriteLine("Testing callvirt opcode for calling DIM on non-generic interface non-generic method");
+            I2 c1 = new C1();
+            if (c1.GetItemTypeNonGeneric(typeof(string)) != typeof(string))
+                throw new Exception("CallOpcodeGenericInterface failed");
+        }
+
+        private static void CallVirtOpcodeNonGenericInterfaceGenericMethod()
+        {
+            Console.WriteLine("Testing callvirt opcode for calling DIM on non-generic interface non-generic method");
+            I2 c1 = new C1();
+            if (c1.GetItemTypeGeneric<object>() != typeof(object))
+                throw new Exception("CallOpcodeGenericInterface failed");
+        }
+
+        private static void CallVirtOpcodeGenericInterface()
+        {
+            Console.WriteLine("Testing callvirt opcode for calling DIM on generic interface");
             I1<string> c1 = new C1();
             if (c1.GetItemType() != typeof(string))
-                throw new Exception("Test1 failed");
+                throw new Exception("CallVirtOpcodeGenericInterface failed");
+        }
+
+        private static void CallVirtOpcodeGenericInterfaceGenericMethod()
+        {
+            Console.WriteLine("Testing callvirt opcode for calling generic method on DIM on generic interface");
+            I1<string> c1 = new C1();
+            if (c1.GetItemTypeMethod<object>() != typeof(object))
+                throw new Exception("CallVirtOpcodeGenericInterfaceGenericMethod failed");
         }
     }
 }
@@ -35,4 +89,14 @@ public interface I1<T>
 {
     [MethodImpl(MethodImplOptions.NoInlining)]
     sealed Type GetItemType() => typeof(T);
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    sealed Type GetItemTypeMethod<U>() => typeof(U);
+}
+
+public interface I2
+{
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    sealed Type GetItemTypeNonGeneric(Type t) => t;
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    sealed Type GetItemTypeGeneric<U>() => typeof(U);
 }
