55f20224fee30a7726badccbee1c6c014071d863
Adeel <adeelbm@outlook.com>
2019-11-10 13:31:21 +0000

    Move __CrossBuild check back to build.sh

--- _build-commons.sh
+++ _build-commons.sh
@@ -423,7 +423,3 @@ if [ "$__ClangMajorVersion" = 0 ] && [ "$__ClangMinorVersion" = 0 ]; then
         __ClangMinorVersion=9
     fi
 fi
-
-if [ "${__BuildArch}" != "${__HostArch}" ]; then
-    __CrossBuild=1
-fi
