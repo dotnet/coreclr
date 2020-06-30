875cdcf50ee5b9b8b6ed54f59c918057a8b6be09
Steve MacLean <Steve.MacLean@microsoft.com>
2020-06-29 21:39:31 -0400

    Use linux dactablerva.h for Windows cross dac builds

--- src/pal/tools/gen-dactable-rva.cmd
+++ src/pal/tools/gen-dactable-rva.cmd
@@ -2,7 +2,7 @@
 echo on
 echo gen-dactable-rva.cmd %1 %2 %3 %4 %5
 
-set coreclrPath=%1
+set linuxArtifacts=%1
 set targetOS=%2
 set targetArch=%3
 set buildType=%4
@@ -17,23 +17,23 @@ rem The offset doesn't change often, but in the 2.1.x history it changed several
 rem this needs to be replaced by an automated mechanism to get the table offset
 
 if "%target%" == "Linux-arm-Release" (
-    echo #define DAC_TABLE_RVA 0x00542644 > %dactablervaPath%
+    copy %linuxArtifacts%/linux-arm/libcoreclr.so.dactablerva.h %dactablervaPath%
     exit 0
 )
 if "%target%" == "Linux-arm64-Release" (
-    echo #define DAC_TABLE_RVA 0x00775f08 > %dactablervaPath%
+    copy %linuxArtifacts%/linux-arm64/libcoreclr.so.dactablerva.h %dactablervaPath%
     exit 0
 )
 if "%target%" == "alpine-arm64-Release" (
-    echo #define DAC_TABLE_RVA 0x0076de88 > %dactablervaPath%
+    copy %linuxArtifacts%/linux-musl-arm64/libcoreclr.so.dactablerva.h %dactablervaPath%
     exit 0
 )
 if "%target%" == "Linux-x64-Release" (
-    echo #define DAC_TABLE_RVA 0x00765688 > %dactablervaPath%
+    copy %linuxArtifacts%/linux-x64/libcoreclr.so.dactablerva.h %dactablervaPath%
     exit 0
 )
 if "%target%" == "alpine-x64-Release" (
-    echo #define DAC_TABLE_RVA 0x0099fb88 > %dactablervaPath%
+    copy %linuxArtifacts%/linux-musl-x64/libcoreclr.so.dactablerva.h %dactablervaPath%
     exit 0
 )
 
